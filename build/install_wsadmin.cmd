c628082aa9a05a3ab36010edefa6f65113bf75c9
Mark Wallace <mark.e.wallace@gmail.com>
2012-12-14 19:31:58 +0000

    Remove unused .cmd files

--- build/install_wsadmin.cmd
+++
@@ -1,3 +0,0 @@
-call mvn install:install-file  -Dfile=../java/prereq/eclipse/plugins/com.ibm.sbt.libs.was/lib/com.ibm.ws.admin.core.jar -DgroupId=com.ibm.ws -DartifactId=com.ibm.ws.admin.core -Dversion=7.0.0 -Dpackaging=jar  
-call mvn install:install-file  -Dfile=../java/prereq/eclipse/plugins/com.ibm.sbt.libs.was/lib/com.ibm.ws.admin.services.jar -DgroupId=com.ibm.ws -DartifactId=com.ibm.ws.admin.services -Dversion=7.0.0 -Dpackaging=jar 			  
-call mvn install:install-file  -Dfile=../java/prereq/eclipse/plugins/com.ibm.sbt.libs.was/lib/com.ibm.ws.runtime.jar -DgroupId=com.ibm.ws -DartifactId=com.ibm.ws.runtime -Dversion=7.0.0 -Dpackaging=jar   

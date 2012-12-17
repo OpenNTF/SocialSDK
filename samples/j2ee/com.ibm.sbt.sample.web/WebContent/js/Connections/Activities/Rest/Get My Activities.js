ca8cc13b4e1a0699bfc84d42997447fc8b09a3eb
Mark Wallace <mark.e.wallace@gmail.com>
2012-12-17 15:34:25 +0000

    GitHub transition - samples\j2ee

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/js/Connections/Activities/Rest/Get My Activities.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/js/Connections/Activities/Rest/Get My Activities.js
@@ -1,21 +1,27 @@
-require(["sbt/connections/core","sbt/xml","sbt/xpath"],function(conn,xml,xpath) {
+require(["sbt/connections/core","sbt/xml","sbt/xpath","sbt/dom"],function(conn,xml,xpath,dom) {
 
 	var ep = sbt.Endpoints['connections'];
 	ep.xhrGet({
 		serviceUrl:	"/activities/service/atom2/activities",
 		handleAs:	"text",
 		content: {
-			email:	"%{sample.email1}"
+			email:	"%{sample.email1}",
+			ps: 5
 		},
-		load: function(response) {
-      		var doc = xml.parse(response);
-			var name = xpath.selectText(doc,"/a:feed/a:entry/a:contributor/a:name",conn.namespaces);
-			var email = xpath.selectText(doc,"/a:feed/a:entry/a:contributor/a:email",conn.namespaces);
-
-      		dojo.byId("content").appendChild(dojo.doc.createTextNode("Name:"+name+", email:"+email));
+		load: function(response) { 
+			var displayStr = "";
+      		var activities = xpath.selectNodes(xml.parse(response), "/a:feed/a:entry", conn.namespaces);      		
+      		for(var count = 0; count < activities.length; count ++){	
+      			var activityName = xpath.selectText(activities[count], "a:title", conn.namespaces);
+      			displayStr += activityName + ((count == activities.length -1) ?"  ":" , ");
+      		}
+      		if (displayStr.length == 0) {
+                displayStr = "You are not a member of any activity.";
+            }
+      		dom.setText("content",displayStr);
 		},
 		error: function(error){
-			dojo.byId("content").appendChild(dojo.doc.createTextNode("Error:"+error));
+			dom.setText("content","Error received. Error Code = " +  error.code + ". Error Message = " + error.message);
 		}
 	});
 });

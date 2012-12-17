fbbc301929def410a30e0bf9a6d41ba83098886a
Mark Wallace <mark.e.wallace@gmail.com>
2012-12-17 20:58:23 +0000

    Remove SNAPSHOT build label

--- build/build.cmd
+++ build/build.cmd
@@ -6,4 +6,4 @@ set JAVA_HOME=%toolsDir%\ibm-java-sdk-60-win-i386
 set M2_HOME=%toolsDir%\apache-maven-3.0.4
 set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%PATH%
 
-mvn clean javadoc:javadoc install -DtoolsDir=%toolsDir% -DbuildLabel=SNAPSHOT
+mvn clean javadoc:javadoc install -DtoolsDir=%toolsDir%

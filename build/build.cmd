2ccfd52f02b386c84fb65b169e9731e46d6c9c96
Carlos Manias <carlmani@ie.ibm.com>
2012-12-18 15:29:22 +0000

    Adding Javascript build scripts (docs, linting, minifying) and Java reporting (PMD, FindBugs)

--- build/build.cmd
+++ build/build.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 echo Set toolsDir environment variable to the tools directory in your machine and REM this line
-set toolsDir=..\..\..\tools
+set toolsDir=..\tools
 set JAVA_HOME=%toolsDir%\ibm-java-sdk-60-win-i386
 set M2_HOME=%toolsDir%\apache-maven-3.0.4
 set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%PATH%

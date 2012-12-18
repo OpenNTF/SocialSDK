512f07db13c5e3f105a3f31ca438575f58275c82
Lorenzo Boccaccia <LORENZOB@ie.ibm.com>
2012-12-18 11:47:17 +0000

    update default handling for label

--- build/build.cmd
+++ build/build.cmd
@@ -6,15 +6,15 @@ if "%1"=="-tools" set toolsDir=%2
 
 if "%3"=="-label" set LABEL=%4
 
-if "%3"=="-tools"  set toolsDir=%4
+if "%3"=="-tools" set toolsDir=%4
 
 if "%toolsDir%" == "" echo Set toolsDir environment variable to the tools directory in your machine and REM this line
 if "%toolsDir%" == "" set toolsDir=..\tools
-
+if "%LABEL%" == ""     set LABEL=SNAPSHOT
 
 set JAVA_HOME=%toolsDir%\ibm-java-sdk-60-win-i386
 set M2_HOME=%toolsDir%\apache-maven-3.0.4
 set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%PATH%
-set LABEL=SNAPSHOT
+
 
 mvn clean javadoc:javadoc install -DtoolsDir=%toolsDir% -DbuildLabel=%LABEL%

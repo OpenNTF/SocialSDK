18b9ef1698e3f1ed2dcf1cec341224d7dfa3a527
Lorenzo Boccaccia <LORENZOB@ie.ibm.com>
2012-12-18 11:04:24 +0000

    checking for parameter befor usage

--- build/build.cmd
+++ build/build.cmd
@@ -1,11 +1,5 @@
 @echo off
 
-
-set JAVA_HOME=%toolsDir%\ibm-java-sdk-60-win-i386
-set M2_HOME=%toolsDir%\apache-maven-3.0.4
-set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%PATH%
-set LABEL=SNAPSHOT
-
 if "%1"=="-label" set LABEL=%2
 
 if "%1"=="-tools" set toolsDir=%2
@@ -16,5 +10,11 @@ if "%3"=="-tools"  set toolsDir=%4
 
 if "%toolsDir%" == "" echo Set toolsDir environment variable to the tools directory in your machine and REM this line
 if "%toolsDir%" == "" set toolsDir=..\tools
+
+
+set JAVA_HOME=%toolsDir%\ibm-java-sdk-60-win-i386
+set M2_HOME=%toolsDir%\apache-maven-3.0.4
+set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%PATH%
+set LABEL=SNAPSHOT
 
 mvn clean javadoc:javadoc install -DtoolsDir=%toolsDir% -DbuildLabel=%LABEL%

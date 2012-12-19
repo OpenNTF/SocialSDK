bdc4ab16372698ef3b17c05af731b9a51ea5ded2
Lorenzo Boccaccia <LORENZOB@ie.ibm.com>
2012-12-19 11:02:37 +0000

    different escaping for spaces

--- build/build.cmd
+++ build/build.cmd
@@ -12,12 +12,12 @@ if "%3"=="-tools" set toolsDir=%4
 
 echo building %LABEL% with tools from %toolsDir%
 
-if "%toolsDir:"=%" == "" echo Set toolsDir environment variable to the tools directory in your machine and REM this line
-if "%toolsDir:"=%" == "" set toolsDir=..\tools
-if "%LABEL:"=%" == ""   set LABEL=SNAPSHOT
+if (%toolsDir%) == () echo Set toolsDir environment variable to the tools directory in your machine and REM this line
+if (%toolsDir%) == () set toolsDir=..\tools
+if (%LABEL%) == ()   set LABEL=SNAPSHOT
 
 set JAVA_HOME=%toolsDir:"=%\ibm-java-sdk-60-win-i386
 set M2_HOME=%toolsDir:"=%\apache-maven-3.0.4
 set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%PATH%
 
-mvn clean javadoc:javadoc install -DtoolsDir=%toolsDir% 
+mvn clean javadoc:javadoc install -DtoolsDir=%toolsDir%  -DbuildLabel=%LABEL%

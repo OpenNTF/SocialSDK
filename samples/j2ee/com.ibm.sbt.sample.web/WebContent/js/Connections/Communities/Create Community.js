282a8bd48de3b5c1aa99a73070c1835bbcaa1ee3
Mark Wallace <mark_wallace@ie.ibm.com>
2013-01-10 17:40:00 +0000

    Add some unique data when you create a Community

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/js/Connections/Communities/Create Community.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/js/Connections/Communities/Create Community.js
@@ -1,16 +1,17 @@
 require(["sbt/connections/CommunityService","sbt/dom"], function(CommunityService,dom) {
-	var communityService = new CommunityService();	
-	var community = communityService.getCommunity({		
-		loadIt : false
-	});	
-	community.setTitle("Test Community Title");
-	community.setContent("Test Community Title");
-	communityService.createCommunity(community, {				
-		load : function(community){			
-			dom.setText("content","Title of new community is " + community.getTitle());
-		},
-		error : function(error){
-			dom.setText("content","Error received. Error Code = " +  error.code + ". Error Message = " + error.message);
-		}
-	});
+    var communityService = new CommunityService();  
+    var community = communityService.getCommunity({     
+        loadIt : false
+    }); 
+    var now = new Date();
+    community.setTitle("Test Community" + now.getTime());
+    community.setContent("Test community created: " + now);
+    communityService.createCommunity(community, {               
+        load : function(community){         
+            dom.setText("content","Title of new community is " + community.getTitle());
+        },
+        error : function(error){
+            dom.setText("content","Error received. Error Code = " +  error.code + ". Error Message = " + error.message);
+        }
+    });
 });

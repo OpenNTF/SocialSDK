31188f517cc64f7a7f04f782789de1acf6ee7da4
priand <priand@us.ibm.com>
2013-01-24 18:41:22 -0500

    Fixed build

--- samples/j2ee/com.ibm.sbt.sample.web/src/demo/SnippetFactoryForJava.java
+++ samples/j2ee/com.ibm.sbt.sample.web/src/demo/SnippetFactoryForJava.java
@@ -63,6 +63,6 @@ public class SnippetFactoryForJava {
 			
 		};
 		Importer imp = new Importer(file,factory,Importer.JSP_EXTENSIONS);
-		return (DemoRootNode)imp.readSnippets(new DemoRootNode());
+		return (DemoRootNode)imp.readSnippets(new DemoRootNode(),null);
 	}
 }

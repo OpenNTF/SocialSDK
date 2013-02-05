b31ef5b5d04630b178a1c6e31f2eb69ff72b3945
Vineet Kanwal <vikanwal@in.ibm.com>
2013-02-05 15:46:02 +0530

    JS Files API for Smartcloud

---
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/js/Smartcloud/Files/Read File Entry.js
@@ -0,0 +1,15 @@
+require([ "sbt/smartcloud/FileService", "sbt/dom" ], function(FileService, dom) {
+	var fileService = new FileService();
+	fileService.getFileEntry({
+		loadIt : true,
+		id : "%{sample.smartcloud.fileEntryId}",
+		load : function(file) {
+			var content = "";
+			content = content + file.getId() + " : " + file.getName();			
+			dom.setText("content", content);
+		},
+		error : function(error) {
+			dom.setText("content", "Error received. Error Code = " + error.code + ". Error Message = " + error.message);
+		}
+	});
+});
--- src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/smartcloud/core.js
+++ src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/smartcloud/core.js
@@ -18,19 +18,24 @@
  * Social Business Toolkit SDK. 
  * Helpers for the core SmartCloud capabilities
  */
-define(['sbt/config'],function(sbt) {
+define([ 'sbt/config' ], function(sbt) {
 
 	return sbt.smartcloud = {
 		// Namespaces used when parsing the SmartCloud Atom feeds
-		namespaces: {
-			o:			"http://ns.opensocial.org/2008/opensocial",
-			app:		"http://www.w3.org/2007/app",
-			thr:		"http://purl.org/syndication/thread/1.0",
-			fh:			"http://purl.org/syndication/history/1.0",
-			snx:		"http://www.ibm.com/xmlns/prod/sn",
-			opensearch:	"http://a9.com/-/spec/opensearch/1.1/",
-			a:			"http://www.w3.org/2005/Atom",
-			h:			"http://www.w3.org/1999/xhtml"
+		namespaces : {
+			o : "http://ns.opensocial.org/2008/opensocial",
+			app : "http://www.w3.org/2007/app",
+			thr : "http://purl.org/syndication/thread/1.0",
+			fh : "http://purl.org/syndication/history/1.0",
+			snx : "http://www.ibm.com/xmlns/prod/sn",
+			opensearch : "http://a9.com/-/spec/opensearch/1.1/",
+			a : "http://www.w3.org/2005/Atom",
+			h : "http://www.w3.org/1999/xhtml",
+			cmis : "http://docs.oasis-open.org/ns/cmis/core/200908/",
+			cmism : "http://docs.oasis-open.org/ns/cmis/messaging/200908/",
+			cmisra : "http://docs.oasis-open.org/ns/cmis/restatom/200908/",
+			lcmis : "http://www.ibm.com/xmlns/prod/sn/cmis",
+			sp_0 : "http://www.w3.org/2005/Atom"
 		},
 	};
 

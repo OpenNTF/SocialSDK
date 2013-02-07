99e1d24172e47c07e1520dbc5134b166acd9afd9
priand <priand@priand>
2013-02-06 21:07:40 -0500

    Updated the playground with the infrastructure to support the API explorer

---
+++ domino/eclipse/plugins/com.ibm.xsp.sbtsdk/src/nsf/playground/snippets/APIImporter.java
@@ -0,0 +1,58 @@
+package nsf.playground.snippets;
+
+import lotus.domino.Database;
+import lotus.domino.Document;
+
+import com.ibm.sbt.playground.assets.AssetNode;
+import com.ibm.sbt.playground.assets.NodeFactory;
+import com.ibm.sbt.playground.assets.apis.APIDescription;
+import com.ibm.sbt.playground.assets.apis.APINodeFactory;
+import com.ibm.sbt.playground.vfs.VFSFile;
+
+/**
+ * Class for importing API Descriptions.
+ * 
+ * @author priand
+ *
+ */
+public class APIImporter extends AssetImporter {
+
+	public static final String TYPE = "api";
+	
+	public APIImporter(Database db) {
+		super(db);
+	}
+	
+	protected String getAssetType() {
+		return TYPE;
+	}
+
+	protected String getAssetView() {
+		return "AllAPIsByImportSource";
+	}
+
+	protected NodeFactory getNodeFactory() {
+		return new APINodeFactory();
+	}
+	
+	protected void saveAsset(ImportSource source, VFSFile root, AssetNode node) throws Exception {
+		APIDescription snippet = (APIDescription)node.load(root);
+		Document doc = getDatabase().createDocument();
+		try {
+			setItemValue(doc,"Form", "APIDescription");
+			setItemValue(doc,"Author", doc.getParentDatabase().getParent().getUserName()); // Should we make this private (reader field)?
+			setItemValue(doc,"Id", node.getUnid());
+			setItemValue(doc,"Category", node.getCategory());
+			setItemValue(doc,"Name", node.getName());
+			setItemValue(doc,"Description", snippet.getDescription());
+			setItemValue(doc,"ImportSource", source.getName());
+			setItemValue(doc,"Tags", snippet.getTags());
+			setItemValue(doc,"Documentation", snippet.getDocumentation());
+			setItemValue(doc,"Json", snippet.getJson());
+			
+			doc.save();
+		} finally {
+			doc.recycle();
+		}
+	}
+}

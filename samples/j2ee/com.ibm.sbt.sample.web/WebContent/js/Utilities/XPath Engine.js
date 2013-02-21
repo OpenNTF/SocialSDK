7b0555e0d24eb3256fd96a5c032218e671d8d2ec
Carlos Manias <carlmani@ie.ibm.com>
2013-02-21 16:46:23 +0000

    Modified samples in /Utilities to use bridge and not dojo

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/js/Utilities/XPath Engine.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/js/Utilities/XPath Engine.js
@@ -1,9 +1,9 @@
-require(["sbt/xml","sbt/xpath"],function(xml,xpath) {
+require(["sbt/dom","sbt/xml","sbt/xpath"],function(dom,xml,xpath) {
 
 	var result = "";
 	var doc = xml.parse("<a><b>I'm B</b><c>I'm C</c></a>");
 	
     result += xpath.selectText(doc,"/a/c");
 
-	dojo.byId("content").appendChild(dojo.doc.createTextNode(result));
+	dom.byId("content").appendChild(dom.createTextNode(result));
 });

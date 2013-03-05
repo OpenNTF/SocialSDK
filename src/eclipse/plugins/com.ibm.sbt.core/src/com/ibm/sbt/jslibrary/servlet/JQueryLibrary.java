56255adb518ec0aca44949a901ffd4e7fd6d8444
Carlos Manias <carlmani@ie.ibm.com>
2013-03-05 13:29:35 +0000

    Externalizing jQuery version consumed by the SDK

--- src/eclipse/plugins/com.ibm.sbt.core/src/com/ibm/sbt/jslibrary/servlet/JQueryLibrary.java
+++ src/eclipse/plugins/com.ibm.sbt.core/src/com/ibm/sbt/jslibrary/servlet/JQueryLibrary.java
@@ -16,7 +16,6 @@
 package com.ibm.sbt.jslibrary.servlet;
 
 import java.util.Map;
-
 import com.ibm.commons.util.StringUtil;
 import com.ibm.commons.util.io.json.JsonObject;
 
@@ -46,9 +45,10 @@ public class JQueryLibrary extends AbstractLibrary {
 	public static final String		PATH_TEXT				= "/sbt/js/libs/requirejsPlugins/text";
 
 	// TODO Do these need to be dynamic
-	private static final String[][]	REGISTER_MODULES		= { { MODULE_BRIDGE, PATH_BRIDGE },
-			{ MODULE_HAS, PATH_HAS }, { MODULE_JQUERY, PATH_JQUERY }, { MODULE_JQUERY_UI, PATH_JQUERY_UI },
-			{ PLUGIN_I18N, PATH_I18N }, { PLUGIN_TEXT, PATH_TEXT } };
+	private static final String[][]	REGISTER_MODULES		= { { MODULE_BRIDGE, PATH_BRIDGE } };
+	/*
+	 * , { MODULE_HAS, PATH_HAS }, { MODULE_JQUERY, PATH_JQUERY }, { MODULE_JQUERY_UI, PATH_JQUERY_UI }, { PLUGIN_I18N, PATH_I18N }, { PLUGIN_TEXT, PATH_TEXT } }
+	 */
 
 	private static final String[][]	REGISTER_EXT_MODULES	= { { MODULE_SBTX, PATH_SBTX } };
 	private static final String[]	REQUIRE_MODULES			= new String[0];
@@ -235,20 +235,20 @@ public class JQueryLibrary extends AbstractLibrary {
 
 		generateRequireModules(sb, indentationLevel, requireModules);
 		indentationLevel -= 1;
-		indent(sb, indentationLevel).append("},\n"); // end paths
+		indent(sb, indentationLevel).append("}\n"); // end paths
 
-		indent(sb, indentationLevel).append("shim: {\n"); // begin shim
-		indentationLevel += 1;
-
-		indent(sb, indentationLevel).append("'jquery/ui': {\n");
-		indentationLevel += 1;
-		indent(sb, indentationLevel).append("deps: ['jquery'],\n"); // begin jquery/ui
-		indent(sb, indentationLevel).append("exports: '$'\n");
-		indentationLevel -= 1;
-		indent(sb, indentationLevel).append("},\n"); // end jquery/ui
-
-		indentationLevel -= 1;
-		indent(sb, indentationLevel).append("}\n"); // end shim
+		// indent(sb, indentationLevel).append("shim: {\n"); // begin shim
+		// indentationLevel += 1;
+		//
+		// indent(sb, indentationLevel).append("'jquery/ui': {\n");
+		// indentationLevel += 1;
+		// indent(sb, indentationLevel).append("deps: ['jquery'],\n"); // begin jquery/ui
+		// indent(sb, indentationLevel).append("exports: '$'\n");
+		// indentationLevel -= 1;
+		// indent(sb, indentationLevel).append("},\n"); // end jquery/ui
+		//
+		// indentationLevel -= 1;
+		// indent(sb, indentationLevel).append("}\n"); // end shim
 
 		indentationLevel -= 1;
 		indent(sb, indentationLevel).append("});\n"); // end requirejs.config

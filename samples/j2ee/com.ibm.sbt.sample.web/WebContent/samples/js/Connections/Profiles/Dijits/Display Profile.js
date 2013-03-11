8c79b1454baa0ab3d8ea5d784d080831e3027a7c
Mark Wallace <mark_wallace@ie.ibm.com>
2013-03-11 14:05:43 +0000

    Fix jsp's for jquery toolkit and remove obsolete dijit samples

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Profiles/Dijits/Display Profile.js
+++
@@ -1,9 +0,0 @@
-require(["dijit/form/Button","sbt/dojo/connections/DisplayProfile"]);
-
-function showProfileOne(){
-	dijit.byId('profile1').setValue("%{sample.id1}");
-}
-
-function showProfileTwo(){
-	dijit.byId('profile2').setValue("%{sample.id2}");
-}

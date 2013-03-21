d5ff4029ffefbea6733b9cc737e9e221ffbf3d03
Mark Wallace <mark_wallace@ie.ibm.com>
2013-03-21 07:26:42 +0000

    Move sample snippet stuff to playground

---
+++ src/eclipse/plugins/com.ibm.sbt.playground/src/com/ibm/sbt/sample/web/util/JavaSnippetNode.java
@@ -0,0 +1,39 @@
+/*
+ * © Copyright IBM Corp. 2012
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+package com.ibm.sbt.sample.web.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.ibm.commons.runtime.util.UrlUtil;
+import com.ibm.sbt.playground.assets.CategoryNode;
+import com.ibm.sbt.playground.assets.javasnippets.JavaSnippetAssetNode;
+
+
+/**
+ * Definition of a Java code snippet.
+ */
+public class JavaSnippetNode extends JavaSnippetAssetNode {
+
+	public JavaSnippetNode(CategoryNode parent, String name) {
+		super(parent,name);
+	}
+
+	// url for JSP's
+	public String getJSPUrl(HttpServletRequest request) {
+		String pathToJSPSample = getUnid(); //getPath()+".jsp";	
+		return UrlUtil.getRequestUrl(request,false)+"?snippet="+pathToJSPSample;
+	}
+}

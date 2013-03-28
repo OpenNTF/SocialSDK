883c652f40a20d24ce658031ca91c403f412c0c6
Mark Wallace <mark_wallace@ie.ibm.com>
2013-03-28 12:05:50 +0000

    Fix some broken samples

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Communities/API/Get My Communities.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Communities/API/Get My Communities.js
@@ -1,22 +1,14 @@
-require(["sbt/connections/CommunityService","sbt/dom"], function(CommunityService,dom) {
-	var communityService = new CommunityService();
-	communityService.getMyCommunities({
-		parameters:{
-			ps:5
-		},
-		load: function(communities){
-		    var displayStr = "";
-		    for(var count = 0; count < communities.length; count ++){
-				var community = communities[count];
-				displayStr += community.getTitle() + ((count == communities.length -1) ?"  ":" , ");
-			}
-            if (displayStr.length == 0) {
-                displayStr = "You are not a member of any communities.";
-            }
-			dom.setText("content",displayStr);	
-		},
-		error: function(error){
-			dom.setText("content","Error received. Error Code = " +  error.code + ". Error Message = " + error.message);
-		}		
-	});
+require([ "sbt/connections/CommunityService", "sbt/dom", "sbt/json" ], function(CommunityService,dom,json) {
+    var communityService = new CommunityService();
+    communityService.getMyCommunities({
+        parameters : {
+            ps : 5
+        },
+        load : function(communities) {
+            dom.setText("json", json.jsonBeanStringify(communities));
+        },
+        error : function(error) {
+            dom.setText("json", json.jsonBeanStringify(error));
+        }
+    });
 });

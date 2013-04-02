1e20a3d95914b633675c2f3b5588f543f73a885e
RajmeetBal <rajmeetbal.in.ibm.com>
2013-04-02 13:13:43 +0530

    Authentication Snippets changes.

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Authentication/SmartCloud OAuth 1.0a.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Authentication/SmartCloud OAuth 1.0a.js
@@ -1,11 +1,11 @@
 require(['sbt/Endpoint',"sbt/dom","sbt/config"], function(Endpoint,dom,config) {
 	var ep = Endpoint.find("smartcloud");
 	if(ep.isAuthenticated){
-		dom.setText("OAuth1LoginStatus","Logged in");
+		dom.setText("OAuth1LoginStatus","You are authenticated");
 		dom.byId("OAuth1Login").style.display = "none";
 		dom.byId("OAuth1Logout").style.display = "inline";
 	}else{
-		dom.setText("OAuth1LoginStatus","Not Logged in");
+		dom.setText("OAuth1LoginStatus","You are not authenticated");
 		dom.byId("OAuth1Login").style.display = "inline";
 		dom.byId("OAuth1Logout").style.display = "none";
 	}
@@ -16,7 +16,7 @@ function login(loginUi) {
 		config.Properties["loginUi"] = loginUi;
 		Endpoint.find("smartcloud").authenticate({
 			success: function(response){
-				dom.setText("OAuth1LoginStatus","Logged in");
+				dom.setText("OAuth1LoginStatus","You are authenticated");
 				dom.byId("OAuth1Login").style.display = "none";
 				dom.byId("OAuth1Logout").style.display = "inline";	
 			},
@@ -31,7 +31,7 @@ function logout() {
 	require(['sbt/Endpoint',"sbt/dom"], function(Endpoint,dom) {
 		Endpoint.find("smartcloud").logout({
 			success: function(response){
-				dom.setText("OAuth1LoginStatus","Not Logged in");
+				dom.setText("OAuth1LoginStatus","You are not authenticated");
 				dom.byId("OAuth1Login").style.display = "inline";
 				dom.byId("OAuth1Logout").style.display = "none";
 			},

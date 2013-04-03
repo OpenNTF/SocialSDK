7502782ebc85c6b37a25c9cc89229ae6b3d0206b
RajmeetBal <rajmeetbal.in.ibm.com>
2013-04-03 17:03:32 +0530

    changed autoAuthenticate property to boolean

--- src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/Endpoint.js
+++ src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/Endpoint.js
@@ -201,8 +201,11 @@ var Endpoint = declare("sbt.Endpoint", null, {
 				var error = data;
 				// check for if authentication is required				
 				if (error.code == 401 || error.code == self.authenticationErrorCode) {
-					var autoAuthenticate =  _args.autoAuthenticate || self.autoAuthenticate || sbt.Properties["autoAuthenticate"] || "true";
-					if(autoAuthenticate == "true"){
+					var autoAuthenticate =  _args.autoAuthenticate || self.autoAuthenticate || sbt.Properties["autoAuthenticate"];
+					if(autoAuthenticate == undefined){
+						autoAuthenticate = true;
+					}
+					if(autoAuthenticate){
 						if(self.authenticator) {
 							options = {
 								dialogLoginPage:self.loginDialogPage,

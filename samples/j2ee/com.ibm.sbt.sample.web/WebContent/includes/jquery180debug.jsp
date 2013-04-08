f8b0aff304c218172e3abd63b4f3801d7bbbc481
Mark Wallace <mark_wallace@ie.ibm.com>
2013-04-08 22:29:50 +0100

    Use fully qualified paths for modules

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/includes/jquery180debug.jsp
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/includes/jquery180debug.jsp
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="/sbt.jquery182/development-bundle/jquery-1.8.0.js"></script>
+<script type="text/javascript" src="/sbt.jquery180/development-bundle/jquery-1.8.0.js"></script>
 <script type="text/javascript" src="/sbt/js/libs/require.js"></script>
 
 <script type="text/javascript">
--- src/eclipse/plugins/com.ibm.sbt.core/src/com/ibm/sbt/jslibrary/servlet/JQueryLibrary.java
+++ src/eclipse/plugins/com.ibm.sbt.core/src/com/ibm/sbt/jslibrary/servlet/JQueryLibrary.java
@@ -16,7 +16,7 @@
 package com.ibm.sbt.jslibrary.servlet;
 
 import java.util.Map;
-import com.ibm.commons.util.StringUtil;
+
 import com.ibm.commons.util.io.json.JsonObject;
 
 /**
@@ -26,18 +26,28 @@ import com.ibm.commons.util.io.json.JsonObject;
  */
 public class JQueryLibrary extends AbstractLibrary {
 
-	public static final String		NAME					= "jquery";							//$NON-NLS-1$
+	public static final String		NAME					= "jquery";					//$NON-NLS-1$
 
-	public static final String		MODULE_BRIDGE			= "sbt/_bridge";
-	public static final String		MODULE_SBTX				= "sbtx";								//$NON-NLS-1$
-	public static final String		PATH_BRIDGE				= "_bridges/jquery";					//$NON-NLS-1$
-	public static final String		PATH_SBTX				= "/sbtx/js/sdk/sbtx";
-	public static final String		PATH_BASEURL			= "/sbt/js/sdk";
+    public static final String      MODULE_SBT              = "sbt";                    //$NON-NLS-1$
+    public static final String      MODULE_BRIDGE           = "sbt/_bridge";            //$NON-NLS-1$
+	public static final String		MODULE_SBTX				= "sbtx";				    //$NON-NLS-1$
+	public static final String      MODULE_SBTX_WIDGET      = "sbtx/widget";            //$NON-NLS-1$
+	
+    public static final String      PATH_BRIDGE             = "_bridges/jquery";        //$NON-NLS-1$
+	public static final String		PATH_SBTX				= "sbtx/js/sdk/sbtx";       //$NON-NLS-1$
+    public static final String      PATH_SBTX_JQUERYUI      = "jqueryui";               //$NON-NLS-1$
 
+    /*
+    public static final String		PATH_BASEURL			= "/sbt/js/sdk";            //$NON-NLS-1$
+    */
+    
 	// TODO Do these need to be dynamic
-	private static final String[][]	REGISTER_MODULES		= { { MODULE_BRIDGE, PATH_BRIDGE } };
+	private static final String[][]	REGISTER_MODULES		= { { MODULE_SBT, PATH_SBT },
+	                                                            { MODULE_BRIDGE, PATH_BRIDGE } };
 
-	private static final String[][]	REGISTER_EXT_MODULES	= { { MODULE_SBTX, PATH_SBTX } };
+	private static final String[][]	REGISTER_EXT_MODULES	= { { MODULE_SBTX, PATH_SBTX }, 
+	                                                            { MODULE_SBTX_WIDGET, PATH_SBTX_JQUERYUI } };
+	
 	private static final String[]	REQUIRE_MODULES			= new String[0];
 
 	private static final String		DEFINE_MODULE			= MODULE_CONFIG;
@@ -122,6 +132,7 @@ public class JQueryLibrary extends AbstractLibrary {
 		return REQUIRE_MODULES;
 	}
 
+	/* TODO not referenced anywhere, can this be deleted?
 	protected String generateRequireJSConfig(int indentationLevel) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("requirejs.config({\n");
@@ -138,6 +149,7 @@ public class JQueryLibrary extends AbstractLibrary {
 		indent(sb, indentationLevel).append("});\n");
 		return sb.toString();
 	}
+	*/
 
 	/*
 	 * We redefine the isExtension boolean parameter. The meaning here is that there are extension modules pending to be added (so we need to append a comma) (non-Javadoc)
@@ -151,14 +163,28 @@ public class JQueryLibrary extends AbstractLibrary {
 			return;
 		}
 		int numModules = registerModules.length;
-		for (int i = 0; i < numModules; i++) {
-			String[] registerModule = registerModules[i];
+		for (int i=0; i<numModules; i++) {
+            String[] registerModule = registerModules[i];
+            /*
 			indent(sb, indentationLevel).append("'").append(registerModule[0]).append("' : '")
 					.append(registerModule[1]).append("'");
 			if (i < numModules - 1 || isExtension) {
 				sb.append(",");
 			}
 			sb.append("\n");
+            */
+            if (i==0) {
+                if (isExtension) {
+                    // delimit from standard module paths
+                    sb.append(","); 
+                }
+            }
+			String moduleUrl = getModuleUrl(request, registerModule[1], isExtension);
+			sb.append("'").append(registerModule[0]).append("':'")
+                .append(moduleUrl).append("'");
+			if (i < numModules - 1) {
+                sb.append(",");
+            }
 		}
 	}
 
@@ -209,20 +235,17 @@ public class JQueryLibrary extends AbstractLibrary {
 		int waitSeconds = 0;
 		indent(sb, indentationLevel).append("enforceDefine: '").append(enforceDefine).append("',\n");
 		indent(sb, indentationLevel).append("waitSeconds: '").append(waitSeconds).append("',\n");
-		indent(sb, indentationLevel).append("baseUrl: '").append(PATH_BASEURL).append("',\n");
-		indent(sb, indentationLevel).append("paths: {\n"); // begin paths
-		indentationLevel += 1;
+		//indent(sb, indentationLevel).append("baseUrl: '").append(PATH_BASEURL).append("',\n");
+		indent(sb, indentationLevel).append("paths: {"); // begin paths
 
 		// register the module paths and required modules
-		boolean hasExtension = StringUtil.isNotEmpty(request.getToolkitExtUrl());
-		generateRegisterModules(sb, indentationLevel, request, registerModules, hasExtension);
+		generateRegisterModules(sb, indentationLevel, request, registerModules, false);
 		if (registerExtModules != null) {
-			generateRegisterModules(sb, indentationLevel, request, registerExtModules, false);
+			generateRegisterModules(sb, indentationLevel, request, registerExtModules, true);
 		}
 
 		generateRequireModules(sb, indentationLevel, requireModules);
-		indentationLevel -= 1;
-		indent(sb, indentationLevel).append("}\n"); // end paths
+		sb.append("}\n"); // end paths
 
 		indentationLevel -= 1;
 		indent(sb, indentationLevel).append("});\n"); // end requirejs.config

9e1f4d973c0c89c1b50b9f7ded590d90f1248616
Mark Wallace <mark_wallace@ie.ibm.com>
2013-04-09 17:23:59 +0100

    Acme Airlines snippets

---
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/AcmeAirlines/Get Approver.js
@@ -0,0 +1,62 @@
+require(["sbt/Endpoint", "sbt/json", "sbt/dom", "sbt/xml", "sbt/xpath", "sbt/connections/core"], 
+    function(Endpoint, json, dom, xml, xpath, connections) {
+
+    var basicReportingChain = "/profiles/atom/reportingChain.do?userid="; 
+    var oauthReportingChain = "/profiles/oauth/atom/reportingChain.do?userid="; 
+    
+    var approverEmailXPath = "/a:feed/a:entry[2]/a:contributor/a:email";
+    var approverUserIdXPath = "/a:feed/a:entry[2]/a:contributor/snx:userid";
+
+    var endpoint = Endpoint.find('connections');
+    
+    var reportingChain = null;
+    
+    var getReportingChain = function(onSuccess, onError) {
+        var path = basicReportingChain;
+        if (endpoint.authType == 'oauth') {
+            path = oauthReportingChain;
+        }
+        path += "%{sample.id1}";
+        
+        endpoint.xhrGet({
+            serviceUrl : path,
+            loginUi : "dialog",
+            load: function(response) {
+                reportingChain = xml.parse(response);
+                if(onSuccess) {
+                    onSuccess(reportingChain);
+                }
+            },
+            error: function(error){
+                reportingChain = null;
+                if(onError) {
+                    onError(error);
+                }
+            }
+      });
+    };
+
+    getApproverEmail = function() {
+        if (!reportingChain) {
+            return null;
+        }
+        return xpath.selectText(reportingChain, approverEmailXPath, connections.namespaces);
+    };
+    
+    getApproverUserId = function() {
+        if (!reportingChain) {
+            return null;
+        }
+        return xpath.selectText(reportingChain, approverUserIdXPath, connections.namespaces);
+    };
+
+    // demonstrate calling the method
+    var onSuccess = function(reportingChain) {
+        var approver = { email : getApproverEmail(), userid : getApproverUserId() };
+        dom.setText("json", json.jsonBeanStringify(approver));
+    };
+    var onError = function(error) {
+        dom.setText("json", json.jsonBeanStringify(error));
+    };
+    getReportingChain(onSuccess, onError);
+});
---
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/AcmeAirlines/Get Person Me.js
@@ -0,0 +1,46 @@
+require(["sbt/Endpoint", "sbt/json", "sbt/dom"], 
+    function(Endpoint, json, dom) {
+
+    var basicPeopleMe = '/connections/opensocial/basic/rest/people/@me/';
+    var oauthPeopleMe = '/connections/opensocial/oauth/rest/people/@me/';
+
+    var endpoint = Endpoint.find('connections');
+    
+    var personObject = null;
+    
+    var getPeopleMe = function(onSuccess, onError) {
+        var path = basicPeopleMe;
+        if (endpoint.authType == 'oauth') {
+            path = oauthPeopleMe;
+        }
+        
+        endpoint.xhrGet({
+            serviceUrl : path,
+            handleAs : "json",
+            loginUi : "popup",
+            preventCache : true,
+            load: function(response) {
+                personObject = response.entry;
+                personObject.id = response.entry.id.replace('urn:lsid:lconn.ibm.com:profiles.person:', '');
+                if(onSuccess) {
+                    onSuccess(personObject);
+                }
+            },
+            error: function(error){
+                personObject = null;
+                if(onError) {
+                    onError(error);
+                }
+            }
+      });
+    };
+
+    // demonstrate calling the method
+    var onSuccess = function(person) {
+        dom.setText("json", json.jsonBeanStringify(person));
+    };
+    var onError = function(error) {
+        dom.setText("json", json.jsonBeanStringify(error));
+    };
+    getPeopleMe(onSuccess, onError);
+});

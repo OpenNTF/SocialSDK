77b27db3207a070321ede25dc1a71a4d117796e4
Mark Wallace <mark_wallace@ie.ibm.com>
2013-04-09 09:04:34 +0100

    Restore deleted snippets, these are used by the unit tests

---
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Profiles/REST/Read Profile XML.js
@@ -0,0 +1,16 @@
+require(["sbt/dom", "sbt/Endpoint", "sbt/config" ], function(dom, Endpoint) {
+    var endpoint = Endpoint.find("connections");
+    endpoint.xhrGet({
+        serviceUrl : "/profiles/atom/profile.do",
+        handleAs : "text",
+        content : {
+            email : "%{sample.id1}"
+        },
+        load : function(response) {
+            dom.byId("content").appendChild(dom.createTextNode(response));
+        },
+        error : function(error) {
+            dom.byId("content").appendChild(dom.createTextNode(error));
+        }
+    });
+});

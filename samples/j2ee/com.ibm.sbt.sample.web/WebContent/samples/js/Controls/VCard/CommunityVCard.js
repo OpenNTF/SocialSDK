dfd3088aeb21958ec444f2f91271a84d90e8d5b1
Francis Moloney <franmolo@ie.ibm.com>
2013-04-10 09:08:16 +0100

    Replace fixed Community VCard uuid with the one from sbt.properties

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Controls/VCard/CommunityVCard.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Controls/VCard/CommunityVCard.js
@@ -1,7 +1,7 @@
 require(["sbt/dom", "sbt/controls/vcard/connections/CommunityVCard"], function(dom, CommunityVCard) {
     var communityCard = new CommunityVCard({
-        name : "testCommunity",
-        uuid : "2c508289-ab76-4dc4-b5e5-bd7c75d66601",
+        name : "",
+        uuid : "%{sample.communityId}",
         selectedWidgetId : ""
     });
 

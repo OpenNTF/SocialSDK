ff8817d303946983a680e6b475349beb9b06dba1
Lorenzo Boccaccia <LORENZOB@ie.ibm.com>
2013-04-22 14:29:36 +0100

    Added mail session factory The MailSessionFactory creates a session factory if none is configured using whatever JavaMail library is available. Obtaining a session via name lookup is still supported and takes precedence

--- src/eclipse/plugins/com.ibm.sbt.core/src/com/ibm/sbt/services/client/email/EnvironmentConfig.java
+++ src/eclipse/plugins/com.ibm.sbt.core/src/com/ibm/sbt/services/client/email/EnvironmentConfig.java
@@ -5,8 +5,14 @@ public enum EnvironmentConfig {
 	INSTANCE;
 	
 	private final String SBT_SESSION_RESOURCE = "java:comp/env/mail/ibmsbt-session";
+
+	private static final String	SBT_SESSION_FACTORY_RESOURCE = "ibmsbt-sessionfactory";
 	
 	public String getSbtSessionResource(){
 		return SBT_SESSION_RESOURCE;
+	}
+	
+	public String getSbtSessionFactoryResource(){
+		return SBT_SESSION_FACTORY_RESOURCE;
 	}
 }

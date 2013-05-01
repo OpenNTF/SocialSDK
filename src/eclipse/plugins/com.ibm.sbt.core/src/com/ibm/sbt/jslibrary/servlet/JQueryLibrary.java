05666990d44f8bf55cd05e2adcae7ab1d3ee5d9b
Carlos Manias <carlmani@ie.ibm.com>
2013-05-01 14:42:10 +0100

    Code cleanup

--- src/eclipse/plugins/com.ibm.sbt.core/src/com/ibm/sbt/jslibrary/servlet/JQueryLibrary.java
+++ src/eclipse/plugins/com.ibm.sbt.core/src/com/ibm/sbt/jslibrary/servlet/JQueryLibrary.java
@@ -22,9 +22,9 @@ package com.ibm.sbt.jslibrary.servlet;
  */
 public class JQueryLibrary extends RequireJSLibrary {
 
-	public static final String		NAME				= "jquery";			//$NON-NLS-1$
+	private static final String		NAME				= "jquery";			//$NON-NLS-1$
 
-	public static final String		PATH_BRIDGE			= "_bridges/jquery";	//$NON-NLS-1$
+	private static final String		PATH_BRIDGE			= "_bridges/jquery";	//$NON-NLS-1$
 
 	// TODO Do these need to be dynamic
 	private static final String[][]	REGISTER_MODULES	= { { MODULE_SBT, PATH_SBT },
@@ -63,32 +63,4 @@ public class JQueryLibrary extends RequireJSLibrary {
 	protected String[][] getRegisterModulesAmd() {
 		return REGISTER_MODULES;
 	}
-
-	/*
-	 * We redefine the isExtension boolean parameter. The meaning here is that there are extension modules pending to be added (so we need to append a comma) (non-Javadoc)
-	 * @see com.ibm.sbt.jslibrary.servlet.AbstractLibrary#generateRegisterModules(java.lang.StringBuilder, int, com.ibm.sbt.jslibrary.servlet.LibraryRequest, java.lang.String[][], boolean)
-	 */
-	/*
-	 * @Override protected void generateRegisterModules(StringBuilder sb, int indentationLevel, LibraryRequest request, String[][] registerModules, boolean isExtension) { if (registerModules == null) { return; } int numModules = registerModules.length; for (int i = 0; i < numModules; i++) { String[]
-	 * registerModule = registerModules[i];}
-	 */
-	/*
-	 * indent(sb, indentationLevel).append("'").append(registerModule[0]).append("' : '") .append(registerModule[1]).append("'"); if (i < numModules - 1 || isExtension) { sb.append(","); } sb.append("\n");
-	 */
-	/*
-	 * if (i == 0) { if (isExtension) { // delimit from standard module paths sb.append(","); } } String moduleUrl = getModuleUrl(request, registerModule[1], isExtension); sb.append("'").append(registerModule[0]).append("':'").append(moduleUrl).append("'"); if (i < numModules - 1) { sb.append(",");
-	 * } } }
-	 */
-
-	/*
-	 * (non-Javadoc)
-	 * @see com.ibm.sbt.jslibrary.servlet.AbstractLibrary#generateModuleBlock(com.ibm.sbt.jslibrary.servlet.LibraryRequest, java.lang.String[][], java.lang.String[], int)
-	 */
-	/*
-	 * @Override protected StringBuilder generateModuleBlock(LibraryRequest request, String[][] registerModules, String[][] registerExtModules, String[] requireModules, int indentationLevel) { StringBuilder sb = new StringBuilder(); indent(sb, indentationLevel).append("requirejs.config({\n"); //
-	 * begin requirejs.config indentationLevel += 1; Boolean enforceDefine = true; int waitSeconds = 0; indent(sb, indentationLevel).append("enforceDefine: '").append(enforceDefine).append("',\n"); indent(sb, indentationLevel).append("waitSeconds: '").append(waitSeconds).append("',\n"); //
-	 * indent(sb, indentationLevel).append("baseUrl: '").append(PATH_BASEURL).append("',\n"); indent(sb, indentationLevel).append("paths: {"); // begin paths // register the module paths and required modules generateRegisterModules(sb, indentationLevel, request, registerModules, false); if
-	 * (registerExtModules != null) { generateRegisterModules(sb, indentationLevel, request, registerExtModules, true); } generateRequireModules(sb, indentationLevel, requireModules); sb.append("}\n"); // end paths indentationLevel -= 1; indent(sb, indentationLevel).append("});\n"); // end
-	 * requirejs.config return sb; }
-	 */
 }

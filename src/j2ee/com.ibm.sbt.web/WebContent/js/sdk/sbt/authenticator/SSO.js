3e47a091c83ac767e254a6f0eaba5edb17000e9f
swatisingh10 <swati.singh@in.ibm.com>
2013-05-17 16:47:05 +0530

    updated SSO  related changes- added copyright and bean definition

--- src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/authenticator/SSO.js
+++ src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/authenticator/SSO.js
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
  * implied. See the License for the specific language governing 
  * permissions and limitations under the License.
- */
+ */ 
 /**
  * Social Business Toolkit SDK.
  * Definition of an authentication mechanism.

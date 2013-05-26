a3356c64ab3e80e3b968b3df951413fabe10316e
Mark Wallace <mark_wallace@ie.ibm.com>
2013-05-26 16:28:51 +0100

    Reinstate Subscriber.js and stop the module using the global sbt object

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/SmartCloud/Profiles/API/GetConnections.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/SmartCloud/Profiles/API/GetConnections.js
@@ -1,24 +1,22 @@
-require(["sbt/dom", "sbt/json", "sbt/smartcloud/ProfileService"], function(dom,json,ProfileService) {
-    try {
-    	var results = "";
-        var profileService = new ProfileService();
-        var promise = profileService.getMyConnections();
-        promise.then(    
-        		function(profiles){
-                	dom.setText("json", json.jsonBeanStringify(getResults(profiles)));
-                },
-                function(error){            	
-                    dom.setText("json", json.jsonBeanStringify(error));
-                }
-            );
-        } catch(error) {
-            dom.setText("json", json.jsonBeanStringify(error));
-        }
-    });
-function getResults(profiles) {
-	var results = []; 
-	for (var i=0;i<profiles.length;i++) {
-		results.push(profiles[i].getData()); 
+require([ "sbt/dom", "sbt/json", "sbt/smartcloud/ProfileService" ], 
+	function(dom, json, ProfileService) {
+	try {
+		var results = "";
+		var profileService = new ProfileService();
+		var promise = profileService.getMyConnections();
+		promise.then(function(profiles) {
+			dom.setText("json", json.jsonBeanStringify(getResults(profiles)));
+		}, function(error) {
+			dom.setText("json", json.jsonBeanStringify(error));
+		});
+	} catch (error) {
+		dom.setText("json", json.jsonBeanStringify(error));
 	}
-    return  results;
+});
+function getResults(profiles) {
+	var results = [];
+	for ( var i = 0; i < profiles.length; i++) {
+		results.push(profiles[i].getData());
+	}
+	return results;
 };

c61c4530ddee873dae415eb2aa1957477a53b1b3
Mark Wallace <mark_wallace@ie.ibm.com>
2013-05-28 16:54:11 +0100

    Change Endpoint public methods to return a promise

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Authentication/SmartCloud OAuth 2.0.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Authentication/SmartCloud OAuth 2.0.js
@@ -14,30 +14,30 @@ require(['sbt/Endpoint',"sbt/dom","sbt/config"], function(Endpoint,dom,config) {
 function login(loginUi) {
 	require(['sbt/Endpoint',"sbt/dom","sbt/config"], function(Endpoint,dom,config) {
 		config.Properties["loginUi"] = loginUi;
-		Endpoint.find("smartcloudOA2").authenticate({
-			load: function(response){
+		Endpoint.find("smartcloudOA2").authenticate().then(
+			function(response){
 				dom.setText("OAuth2LoginStatus","You are authenticated");
 				dom.byId("OAuth2Login").style.display = "none";
 				dom.byId("OAuth2Logout").style.display = "inline";	
 			},
-			cancel: function(){
+			function(){
 				dom.setText("content","You need to Login to Proceed");
 			}
-		});
+		);
 	});
 }
 
 function logout() {
 	require(['sbt/Endpoint',"sbt/dom"], function(Endpoint,dom) {
-		Endpoint.find("smartcloudOA2").logout({
-			load: function(response){
+		Endpoint.find("smartcloudOA2").logout().then(
+			function(response){
 				dom.setText("OAuth2LoginStatus","You are not authenticated");
 				dom.byId("OAuth2Login").style.display = "inline";
 				dom.byId("OAuth2Logout").style.display = "none";
 			},
-			error: function(){
+			function(){
 				dom.setText("content","Failed to Logout");
 			}
-		});
+		);
 	});
 }

2eb64e3af8c52e9b1d253df981e8cd4b9de905a9
Mark Wallace <mark_wallace@ie.ibm.com>
2013-05-28 13:13:37 +0100

    Modify REST based samples to use promises

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Activities/REST/Get My Activities.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Activities/REST/Get My Activities.js
@@ -1,14 +1,19 @@
 require(["sbt/connections/ConnectionsConstants","sbt/xml","sbt/xpath","sbt/dom"],function(conn,xml,xpath,dom) {
-
-	var ep = sbt.Endpoints['connections'];
-	ep.xhrGet({
-		serviceUrl:	"/activities/service/atom2/activities",
-		handleAs:	"text",
-		content: {
-			email:	"%{sample.email1}",
-			ps: 5
-		},
-		load: function(response) { 
+    var endpoint = sbt.Endpoints['connections'];
+    
+    var url = "/activities/service/atom2/activities";
+    
+    var options = { 
+        method : "GET", 
+        handleAs : "text",
+        query : {
+            email : "%{sample.email1}",
+            ps : 5
+        }
+    };
+    
+    endpoint.request(url, options).then(
+    	function(response) {
 			var displayStr = "";
       		var activities = xpath.selectNodes(xml.parse(response), "/a:feed/a:entry", conn.Namespaces);      		
       		for(var count = 0; count < activities.length; count ++){	
@@ -18,10 +23,10 @@ require(["sbt/connections/ConnectionsConstants","sbt/xml","sbt/xpath","sbt/dom"]
       		if (displayStr.length == 0) {
                 displayStr = "You are not a member of any activity.";
             }
-      		dom.setText("content",displayStr);
-		},
-		error: function(error){
-			dom.setText("content","Error received. Error Code = " +  error.code + ". Error Message = " + error.message);
-		}
-	});
+      		dom.setText("content", displayStr);
+        },
+        function(error){
+            dom.setText("content", error);
+        }
+    );
 });

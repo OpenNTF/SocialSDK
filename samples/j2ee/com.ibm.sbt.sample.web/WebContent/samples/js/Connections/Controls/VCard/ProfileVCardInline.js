2af765824c10ee40cd5575134013cfbfa5be3523
Francis Moloney <franmolo@ie.ibm.com>
2013-05-28 10:14:10 +0100

    Move VCards to connections/controls

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Controls/VCard/ProfileVCardInline.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Controls/VCard/ProfileVCardInline.js
@@ -1,4 +1,4 @@
-require(["sbt/dom", "sbt/controls/vcard/connections/ProfileVCardInline"], function(dom, ProfileVCardInline) {
+require(["sbt/dom", "sbt/connections/controls/vcard/ProfileVCardInline"], function(dom, ProfileVCardInline) {
     var profileCard = new ProfileVCardInline({ userName : "%{sample.displayName1}", userId : "%{sample.id1}" });
    
     dom.byId("vcardDiv").appendChild(profileCard.domNode);

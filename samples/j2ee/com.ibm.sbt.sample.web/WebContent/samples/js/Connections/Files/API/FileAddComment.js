c60778fb9b9c3383b30eff9813686417f9950fc9
Mark Wallace <mark_wallace@ie.ibm.com>
2013-05-30 08:43:15 +0100

    File comments grid should display first of my files. Samples that create comments should use a unique comment.

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Files/API/FileAddComment.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Files/API/FileAddComment.js
@@ -1,16 +1,21 @@
 require([ "sbt/connections/FileService", "sbt/dom", "sbt/json" ], function(FileService, dom, json) {
 
 	var fileService = new FileService();
+	
+	var date = new Date();
 
-	fileService.getFile("%{sample.fileId}").then(function(file) {
-		file.addComment("Comment Added from JS Sample").then(function(comment) {
-
-			dom.setText("json", json.jsonBeanStringify(comment));
-
-		}, function(error) {
+	fileService.getFile("%{sample.fileId}").then(
+		function(file) {
+			file.addComment("Comment Added from JS Sample on "+date).then(
+				function(comment) {
+					dom.setText("json", json.jsonBeanStringify(comment));
+				}, function(error) {
+					dom.setText("json", json.jsonBeanStringify(error));
+				}
+			);
+		}, 
+		function(error) {
 			dom.setText("json", json.jsonBeanStringify(error));
-		});
-	}, function(error) {
-		dom.setText("json", json.jsonBeanStringify(error));
-	});
+		}
+	);
 });

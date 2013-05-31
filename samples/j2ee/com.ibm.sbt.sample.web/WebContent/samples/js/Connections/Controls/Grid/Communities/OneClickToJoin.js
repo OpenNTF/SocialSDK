cf4a590b4f30d7e7be0aad0dbb5aa8c7d587c147
Mark Wallace <mark_wallace@ie.ibm.com>
2013-05-31 15:58:19 +0100

    Update to new CommunityService API

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Controls/Grid/Communities/OneClickToJoin.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Controls/Grid/Communities/OneClickToJoin.js
@@ -1,71 +1,56 @@
-require(["sbt/dom", 
+require(["sbt/dom", "sbt/lang",
          "sbt/connections/controls/communities/CommunityGrid",
          "sbt/connections/controls/bootstrap/CommunityRendererMixin",
-         "sbt/lang",
          "sbt/connections/CommunityService"], 
-    function(dom, CommunityGrid, CommunityRendererMixin, lang, CommunityService) {
-	    
-		var communityService = new CommunityService();
-		var communitiesGrid = new CommunityGrid();
-	    lang.mixin(communitiesGrid.renderer, CommunityRendererMixin);
-		
-		//Set the custom template
-		var domNode = dom.byId("communityRow");
-	    var CustomCommunityRow = domNode.text || domNode.textContent;
-		communitiesGrid.renderer.template = CustomCommunityRow;  
-	    
-	    dom.byId("gridDiv").appendChild(communitiesGrid.domNode);       
-	    communitiesGrid.update();
-	    
-	    dom.byId("selectedBtn").onclick = function(evt) {
-		        var communities = communitiesGrid.getSelected();
-		        
-		        if(!communities || !communities.length > 0){
-		        	dom.byId("success").style.display = "none";
-		            dom.byId("error").style.display = "";
-		            dom.setText("error","No Communities Selected");
-		        }
-		        
-		        var userEmail = document.getElementById("emailTextField").value;
-		        if(!userEmail || !userEmail.length > 0){
-		        	dom.byId("success").style.display = "none";
-		            dom.byId("error").style.display = "";
-		            dom.setText("error","No Email Entered");
-		        }
-		       
-		        if(!userEmail.length > 0 && !communities.length > 0){
-		        	dom.byId("success").style.display = "none";
-		            dom.byId("error").style.display = "";
-		            dom.setText("error","No Email Or Community Selected");
-		        }
-		       
-		        if(userEmail && userEmail != "" && communities && communities.length > 0){
-			        for (i in communities) {
-			        	 var selected = communities[i].data.getValue("uid");
-			        	 communityService.getCommunity({
-					            id: selected,
-					            load: function(community) {
-					            	community.addMember({id : userEmail},{       
-				        		        load : function(member){
-				        		            dom.byId("success").style.display = "";
-				        		            dom.byId("error").style.display = "none";
-				        		            dom.setText("success", "Successfully Added User To Communities");
-				        		        },
-				        		        error : function(error){
-				        		        	dom.byId("success").style.display = "none";
-				        		            dom.byId("error").style.display = "";
-				        		            dom.setText("error","Could not add the selecedted user to the selected community");
-				        		            console.error(error);
-				        		        }
-				        		    });
-					            },
-					            error: function(error) {
-						              console.log("Error Loading Communities");
-						              console.error(error);
-						        }
-			        	 });
-		           
-			        }
-		        }		        
-		};
+function(dom, lang, CommunityGrid, CommunityRendererMixin, CommunityService) {
+	var communityService = new CommunityService();
+	var communitiesGrid = new CommunityGrid({ hideSorter : true });
+	lang.mixin(communitiesGrid.renderer, CommunityRendererMixin);
+
+	//Set the custom template
+	var domNode = dom.byId("communityRow");
+	var CustomCommunityRow = domNode.text || domNode.textContent;
+	communitiesGrid.renderer.template = CustomCommunityRow;
+
+	dom.byId("gridDiv").appendChild(communitiesGrid.domNode);
+	communitiesGrid.update();
+
+	dom.byId("addBtn").onclick = function(evt) {
+		var communities = communitiesGrid.getSelected();
+
+		if (!communities || !communities.length > 0) {
+			dom.byId("success").style.display = "none";
+			dom.byId("error").style.display = "";
+			dom.setText("error", "Please select a community.");
+			return;
+		}
+
+		var userEmail = document.getElementById("emailTextField").value;
+		if (!userEmail || !userEmail.length > 0) {
+			dom.byId("success").style.display = "none";
+			dom.byId("error").style.display = "";
+			dom.setText("error", "Please enter a valid email adress.");
+			return;
+		}
+
+		for (i in communities) {
+			var communityUrl = communities[i].data.getValue("uid");
+			var index = communityUrl.indexOf("communityUuid=");
+			var communityUuid = communityUrl.substring(index + 14);
+			
+			communityService.addMember(communityUuid, userEmail).then(
+				function(community) {
+					dom.byId("success").style.display = "";
+					dom.byId("error").style.display = "none";
+					dom.setText("success", "Successfully added " + userEmail + " to community: " + community.getCommunityUuid());
+				},
+				function(error) {
+					dom.byId("success").style.display = "none";
+					dom.byId("error").style.display = "";
+					dom.setText("error", "Could not add the selecedted user to the selected community: "+error.message);
+					console.error(error);
+				}
+			);
+		}
+	};
 });

fb1c4995d1811f278e2558d059d7dde42fceff91
Mark Wallace <mark_wallace@ie.ibm.com>
2013-06-05 22:53:39 +0100

    Remove the dependency on HttpServletRequest

--- samples/j2ee/smartcloud.webapp/src/com/ibm/sbt/smartcloud/Util.java
+++ samples/j2ee/smartcloud.webapp/src/com/ibm/sbt/smartcloud/Util.java
@@ -1,30 +1,19 @@
 package com.ibm.sbt.smartcloud;
 
-import javax.servlet.http.HttpServletRequest;
-
-import com.ibm.commons.util.StringUtil;
 import com.ibm.sbt.services.endpoints.Endpoint;
 import com.ibm.sbt.services.endpoints.EndpointFactory;
 
 public class Util {
 
-    public static String getThemeUrl(HttpServletRequest request) {
-    	Endpoint endpoint = EndpointFactory.getEndpoint(getEndpointName(request));
+    public static String getThemeUrl() {
+    	Endpoint endpoint = EndpointFactory.getEndpoint("smartcloud");
     	return endpoint.getUrl() + "/theming/theme/css/3";
     	
     }
 	
-    public static String getNavBarUrl(HttpServletRequest request) {
-    	Endpoint endpoint = EndpointFactory.getEndpoint(getEndpointName(request));
+    public static String getNavBarUrl() {
+    	Endpoint endpoint = EndpointFactory.getEndpoint("smartcloud");
     	return endpoint.getUrl() + "/manage/navbar/banner/smartcloudExt?oneui=3";
     }
     
-    public static String getEndpointName(HttpServletRequest request) {
-    	String name = request.getParameter("endpoint");
-    	if (StringUtil.isEmpty(name)) {
-    		name = "smartcloud";
-    	}
-    	return name;
-    }
-	
 }

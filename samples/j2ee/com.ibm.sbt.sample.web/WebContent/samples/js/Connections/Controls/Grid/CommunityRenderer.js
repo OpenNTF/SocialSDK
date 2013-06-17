12d38eecfe15802ade486a7cdaf93fbf86ebfa44
David Ryan <daviryan@ie.ibm.com>
2013-06-17 11:33:03 +0100

    Make Paramter js Generic by removing connections specific data such as sort values

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Controls/Grid/CommunityRenderer.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Controls/Grid/CommunityRenderer.js
@@ -28,7 +28,19 @@ require(["sbt/controls/grid/Grid",
 			"contributorName"	:"a:contributor/a:name",
 			"contributorEmail"	:"a:contributor/a:email"				
 		};
-     
+		
+		var sortVals = {
+				date: "modified",
+	            popularity: "count",
+	            name: "title"
+		};
+		
+		var ParamSchema = {	
+			pageNumber: parameter.oneBasedInteger("page"),	
+			pageSize: parameter.oneBasedInteger("ps"),
+			sortBy: parameter.sortField("sortField",sortVals),
+			sortOrder: parameter.booleanSortOrder("asc")			
+		};
 	
         var gridRenderer = new CommunityGridRenderer();
          
@@ -36,7 +48,7 @@ require(["sbt/controls/grid/Grid",
             storeArgs : {
                 url : "/communities/service/atom/communities/all",
                 attributes : xpath_community,
-                paramSchema: parameter.communities.all
+                paramSchema: ParamSchema
             },
             renderer : gridRenderer
         });

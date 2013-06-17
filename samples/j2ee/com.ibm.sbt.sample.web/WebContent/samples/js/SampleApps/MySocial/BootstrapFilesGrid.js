1eb92fda99150c57afe68b26e35a207d0930e72d
David Ryan <daviryan@ie.ibm.com>
2013-06-17 14:08:22 +0100

    Bootstrap samples for communities,files and profiles grids.

---
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/SampleApps/MySocial/BootstrapFilesGrid.js
@@ -0,0 +1,14 @@
+require(["sbt/dom", "sbt/lang", "sbt/connections/controls/files/FileGrid", "sbt/connections/controls/bootstrap/FileRendererMixin"], function(dom,lang, FileGrid,FileRendererMixin ) {
+        var grid = new FileGrid({
+	         type : "library",
+	         pinFile: true
+	    });
+       
+        lang.mixin(grid.renderer, FileRendererMixin);
+        var domNode = dom.byId("fileRow");
+        var CustomFileRow = domNode.text || domNode.textContent;
+        grid.renderer.template = CustomFileRow;
+	    dom.byId("gridDiv").appendChild(grid.domNode);
+		         
+	    grid.update();
+});

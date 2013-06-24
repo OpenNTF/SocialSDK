89b48349078f868dff7070565adb497943be9851
Francis Moloney <franmolo@ie.ibm.com>
2013-06-24 13:11:24 +0100

    Add xpath to the SearchConstants. Change ResultXPath to SearchXPath to be consistent with the other Constants files.

--- src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/connections/SearchConstants.js
+++ src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/connections/SearchConstants.js
@@ -41,7 +41,7 @@ define([ "../lang", "./ConnectionsConstants" ], function(lang,conn) {
         /**
          * XPath expressions to be used when reading a search result
          */
-        ResultXPath : {
+        SearchXPath : {
             // used by getEntityData
             entry : "/a:entry",
             // used by getEntityId
@@ -54,9 +54,38 @@ define([ "../lang", "./ConnectionsConstants" ], function(lang,conn) {
             authorName : "a:author/a:name",
             authorEmail : "a:author/a:email",
             rank : "snx:rank",
-            score : "relevance:score"
+            relevance : "relevance:score",
+            summary : "a:summary",
+            authorState : "a:author/snx:userState",
+            type : "a:category[@scheme='http://www.ibm.com/xmlns/prod/sn/type']/@term",
+            application : "a:category[@scheme='http://www.ibm.com/xmlns/prod/sn/component']/@term",
+            applicationCount : "count(a:category[@scheme='http://www.ibm.com/xmlns/prod/sn/component']/@term)",
+            primaryComponent : "a:category[ibmsc:field[@id='primaryComponent']]/@term",
+            tags : "a:category[not(@scheme)]/@term",
+            commentCount : "snx:rank[@scheme='http://www.ibm.com/xmlns/prod/sn/comment']",
+            resultLink : "a:link[not(@rel)]/@href",
+            bookmarkLink : "ibmsc:field[@id='dogearURL']",
+            eventStartDate : "ibmsc:field[@id='eventStartDate']",
+            authorJobTitle : "a:content/xhtml:div/xhtml:span/xhtml:div[@class='title']",
+            authorJobLocation : "a:content/xhtml:div/xhtml:span/xhtml:div[@class='location']",
+            authorCount : "count(a:contributor)",
+            contributorCount : "count(a:author)",
+            tagCount : "count(a:category[not(@scheme)])",
+            highlightField : "ibmsc:field[@id='highlight']",
+            fileExtension : "ibmsc:field[@id='fileExtension']",
+            memberCount : "snx:membercount",
+            communityUuid : "snx:communityUuid",
+            containerType : "ibmsc:field[@id='container_type']",
+            communityParentLink : "a:link[@rel='http://www.ibm.com/xmlns/prod/sn/container' and @type='text/html']/@href",
+            parentageMetaID : "ibmsc:field[contains(@id, 'ID')]/@id",
+            parentageMetaURL : "ibmsc:field[contains(@id, 'URL')]",
+            parentageMetaURLID : "ibmsc:field[contains(@id, 'URL')]/@id",
+            objectRefDisplayName : "ibmsc:field[@id='FIELD_OBJECT_REF_DISPLAY_NAME']",
+            objectRefUrl : "ibmsc:field[@id='FIELD_OBJECT_REF_URL']",
+            accessControl : "a:category[@scheme='http://www.ibm.com/xmlns/prod/sn/accesscontrolled']/@term",
+            commentsSummary : "ibmsc:field[@id='commentsSummary']"
         },
-                
+        
 		/**
          * Search IBM Connections for public information.
          */

d45cc7a347fa7147e6f39a79d8e2d4e508f5b772
David Ryan <daviryan@ie.ibm.com>
2013-07-01 14:10:14 +0100

    Updates To MySocial Grid Samples to match the new sample theme.

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/SampleApps/MySocial/My Colleagues.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/SampleApps/MySocial/My Colleagues.js
@@ -1,15 +1,26 @@
 require(["sbt/dom", "sbt/lang", "sbt/connections/controls/profiles/ProfileGrid"], function(dom, lang, ProfileGrid) {
     var grid = new ProfileGrid({
         type : "colleagues",
-        email : "%{sample.email1}"
+        email : "%{sample.email1}",
+        hideSorter: true,
+        hidePager: true,
+        pageSize: 6
     });
-
     grid.renderer.template = dom.byId("profileRow").innerHTML;
-    grid.renderer.pagerTemplate = dom.byId("pagerTemplate").innerHTML;
-    grid.renderer.sortTemplate = dom.byId("sortingTemplate").innerHTML;
-    grid.renderer.sortAnchor = dom.byId("sortAnchorTemplate").innerHTML;
+    grid.renderer.renderTable =  function(grid, el, items, data){
+    	var div = this._create("div",{style:"width:300px; height:200px;"}, el );
+    	var ul = this._create("ul", {style:"list-style:none outside none;margin: 0;"},div);
+        return ul;	
+    };
+    
+    grid.profileAction = {
+    		
+        	execute: function(item, opts, event){
+        		window.location.assign(item.fnUrl);
+        	}
+        
+        };
+       
     dom.byId("gridDiv").appendChild(grid.domNode);
-
     grid.update();
-   
 });

ef3af4b8522d4f20baffde7e4e3595fc29909f08
David Ryan <daviryan@ie.ibm.com>
2013-07-02 09:30:38 +0100

    Changed samples to use css classes and not inline styles.

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/SampleApps/MySocial/My Files.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/SampleApps/MySocial/My Files.js
@@ -9,7 +9,14 @@ require(["sbt/dom", "sbt/connections/controls/files/FileGrid"], function(dom, Fi
         grid.renderer.template = dom.byId("fileRow").innerHTML;
         
         grid.renderer.renderTable =  function(grid, el, items, data){
-        	var div = this._create("div",{style:"width:300px; height:200px;"}, el );
+        	var div = this._create("div",{"class":"span3"}, el );
+        	
+        	var headingDiv = this._create("div", {"class":"examples"},div);
+        	var h3 = this._create("h3", {
+        		"class":"h3",
+        		innerHTML: '<a href="">My Files</a>'
+        	}, headingDiv);
+        	
         	var ul = this._create("ul", {style:"list-style:none outside none;margin: 0;"},div);
             return ul;	
         };

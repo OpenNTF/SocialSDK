c725a6cae49fb6ef1b8d9af90b35ffb8a78dd9e1
Francis Moloney <franmolo@ie.ibm.com>
2013-07-10 12:03:24 +0100

    Removed comments which had substitution variables from samples. Substitution variables which don't exist in sbt.properties will now break the tests because we redirect to an input screen.

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Controls/Grid/Profiles/DirectReports.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Controls/Grid/Profiles/DirectReports.js
@@ -1,7 +1,6 @@
 require(["sbt/dom", "sbt/connections/controls/profiles/ProfileGrid"], function(dom, ProfileGrid) {
     var grid = new ProfileGrid({
         type : "peopleManaged",
-        //email : "%{sample.email2}"
         email : "georgebandini@renovations.com"
     });
 

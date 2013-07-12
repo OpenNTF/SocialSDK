f80cc6a6c39710f9015265fdce5f8d8385edb656
David Ryan <daviryan@ie.ibm.com>
2013-07-12 12:43:58 +0100

    Adding additional users to sbt properties

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Controls/Grid/Profiles/AreColleagues.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Controls/Grid/Profiles/AreColleagues.js
@@ -4,7 +4,7 @@ require(["sbt/dom", "sbt/connections/controls/profiles/ColleagueGrid"],
             type: "dynamic",
             rendererArgs: { hideViewAll: true },
             userid: "%{sample.userId1}",
-            targetUserids: [ "%{sample.userId2}",  "%{sample.userId3}", "26356FD1-CDC8-67D2-4825-7A7000256C06", "3657BE03-58AC-4CB5-4825-7A700025E3BA" ]
+            targetUserids: [ "%{sample.userId2}",  "%{sample.userId3}", "%{sample.userId4}", "%{sample.userId5}" ]
         });
         dom.byId("gridDiv").appendChild(grid.domNode);
     }

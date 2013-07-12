f518324b18499ca195269e18ef4cc57a0e1070b6
David Ryan <daviryan@ie.ibm.com>
2013-07-12 10:38:45 +0100

    Changing samples to use Ids, instead of email to work on greenhouse

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Controls/Grid/Profiles/AreColleagues.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Connections/Controls/Grid/Profiles/AreColleagues.js
@@ -3,9 +3,8 @@ require(["sbt/dom", "sbt/connections/controls/profiles/ColleagueGrid"],
         var grid = new ColleagueGrid({
             type: "dynamic",
             rendererArgs: { hideViewAll: true },
-            email: "%{sample.email1}",
-            targetEmails: [ "AndreasBerzat@renovations.com", "JasmineHaj@renovations.com", "TedAmado@renovations.com", 
-                            "LouiseFitzgerald@renovations.com", "EdBlanks@renovations.com", "NancySmith@renovations.com" ]
+            userid: "%{sample.userId1}",
+            targetUserids: [ "%{sample.userId2}",  "%{sample.userId3}", "26356FD1-CDC8-67D2-4825-7A7000256C06", "3657BE03-58AC-4CB5-4825-7A700025E3BA" ]
         });
         dom.byId("gridDiv").appendChild(grid.domNode);
     }

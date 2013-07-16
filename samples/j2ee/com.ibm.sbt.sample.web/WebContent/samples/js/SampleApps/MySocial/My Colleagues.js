c78ff9c157099503e9459f467a33079c92648f77
Mark Wallace <mark_wallace@ie.ibm.com>
2013-07-16 11:27:06 +0100

    Remove use of provider Grid._create method and use dom.create instead

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/SampleApps/MySocial/My Colleagues.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/SampleApps/MySocial/My Colleagues.js
@@ -1,33 +1,39 @@
-require(["sbt/dom", "sbt/lang", "sbt/connections/controls/profiles/ProfileGrid"], function(dom, lang, ProfileGrid) {
-    var grid = new ProfileGrid({
-        type : "colleagues",
-        email : "%{sample.email1}",
-        hideSorter: true,
-        hidePager: true,
-        pageSize: 6
-    });
-    grid.renderer.template = dom.byId("profileRow").innerHTML;
-    grid.renderer.renderTable =  function(grid, el, items, data){
-    	var div = this._create("div",{"class":"span3"}, el );
-    	
-    	var headingDiv = this._create("div", {"class":"examples"},div);
-    	var h3 = this._create("h3", {
-    		"class":"h3",
-    		innerHTML: '<a href="">My Network</a>'
-    	}, headingDiv);
-    	
-    	var ul = this._create("ul", {"class":"network"},div);
-        return ul;	
-    };
-    
-    grid.profileAction = {
-    		
-        	execute: function(item, opts, event){
-        		window.location.assign(item.fnUrl);
-        	}
-        
-        };
-       
-    dom.byId("gridDiv").appendChild(grid.domNode);
-    grid.update();
+require([ "sbt/dom", "sbt/lang", "sbt/connections/controls/profiles/ProfileGrid" ], function(dom, lang, ProfileGrid) {
+	var grid = new ProfileGrid({
+		type : "colleagues",
+		email : "%{sample.email1}",
+		hideSorter : true,
+		hidePager : true,
+		pageSize : 6
+	});
+	grid.renderer.template = dom.byId("profileRow").innerHTML;
+	grid.renderer.renderTable = function(grid, el, items, data) {
+		var div = dom.create("div", {
+			"class" : "span3"
+		}, el);
+
+		var headingDiv = dom.create("div", {
+			"class" : "examples"
+		}, div);
+		
+		var h3 = dom.create("h3", {
+			"class" : "h3",
+			innerHTML : "<a href=''>My Network</a>"
+		}, headingDiv);
+
+		var ul = dom.create("ul", {
+			"class" : "network"
+		}, div);
+
+		return ul;
+	};
+
+	grid.profileAction = {
+		execute : function(item, opts, event) {
+			window.location.assign(item.fnUrl);
+		}
+	};
+
+	dom.byId("gridDiv").appendChild(grid.domNode);
+	grid.update();
 });

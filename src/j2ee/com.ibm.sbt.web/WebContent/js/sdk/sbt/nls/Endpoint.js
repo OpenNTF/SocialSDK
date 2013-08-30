9628725a062effb001781529d5d103c00db30b4c
RajmeetBal <rajmeetbal.in.ibm.com>
2013-08-30 11:29:53 +0530

    Removing all unsupported languages, These will be added as and when we start supporting them

--- src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/nls/Endpoint.js
+++ src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/nls/Endpoint.js
@@ -22,37 +22,6 @@
 define({
   root: ({	 
 	  cannot_find_endpoint:"Unable to find endpoint named {0}, creating it now with an error transport."
-  }),
-  "zh": true,
-  "zh-tw": true,
-  "tr": true,
-  "th": true,
-  "sv": true,
-  "sl": true,
-  "sk": true,
-  "ru": true,
-  "ro": true,
-  "pt": true,
-  "pt-pt": true,
-  "pl": true,
-  "nl": true,
-  "nb": true,
-  "ko": true,
-  "kk": true,
-  "ja": true,
-  "it": true,
-  "hu": true,
-  "hr": true,
-  "he": true,
-  "fr": true,
-  "fi": true,
-  "es": true,
-  "el": true,
-  "de": true,
-  "da": true,
-  "cs": true,
-  "ca": true,
-  "az": true,
-  "ar": true
+  })
   
 });

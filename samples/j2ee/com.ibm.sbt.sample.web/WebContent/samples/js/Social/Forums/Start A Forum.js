f2f3fc594b01ff57fc0c1d2f4ab8027a341be106
Mark Wallace <mark_wallace@ie.ibm.com>
2013-09-02 13:34:28 +0100

    New sample which demonstrates how to start a forum and display the outcome

---
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Forums/Start A Forum.js
@@ -0,0 +1,38 @@
+require(["sbt/dom", 
+         "sbt/connections/controls/forum/ForumGrid",
+         "sbt/connections/ForumService"], 
+    function(dom, ForumGrid, ForumService) {
+		// display My Forums
+		var forumGrid = new ForumGrid({
+	        type : "my"
+	    });
+	    dom.byId("gridDiv").appendChild(forumGrid.domNode);
+	    forumGrid.update();
+	    
+	    // create ForumService
+	    var forumService = new ForumService();
+	    addStartForumBehaviour(forumGrid, forumService, dom);
+	}
+);
+
+function addStartForumBehaviour(forumGrid, forumService, dom) {
+	var actionBtn = dom.byId("actionBtn");
+    actionBtn.onclick = function(evt) {
+    	var title = prompt("Please enter a title for the new form", "");
+        if (title) {
+        	var forum = forumService.newForum(); 
+            forum.setTitle(title);
+            forum.setContent("");
+            var promise = forumService.createForum(forum);
+            promise.then(
+                function(forum) {
+                	forumGrid.refresh();
+                	alert("Forum started successfully!");
+                },
+                function(error) {
+                	alert("Error starting forum: "+error);
+                }
+            );
+        }
+    };
+}

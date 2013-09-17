8730952146cc2e319aaf43a5c4b2d4cddf45bdff
VimalDhupar <vimal.dhupar@in.ibm.com>
2013-09-17 20:54:04 +0530

    Checked in the incorrect version. This Fixes the unit test failure. Executed the Failing test case. Works fine now.

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Profiles SmartCloud/API/GetContactByGUID.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Profiles SmartCloud/API/GetContactByGUID.js
@@ -1,22 +1,18 @@
 require(["sbt/dom", "sbt/json", "sbt/smartcloud/ProfileService"], 
     function(dom,json,ProfileService) {
-	var results = [];  
-    try {
+	try {
         var profileService = new ProfileService();
         var promise = profileService.getContact("%{name=sample.smartcloud.contactGUID|helpSnippetId=Social_Profiles_SmartCloud_Get_Profile}"); 
         promise.then(    
             function(profile){
             	var result = {"Name" : profile.getDisplayName()}; 
-                results.push(result);
-              	dom.setText("json", json.jsonBeanStringify(results));
+              	dom.setText("json", json.jsonBeanStringify(result));
               },
               function(error) {
-              	results.push(error);
-                  dom.setText("json", json.jsonBeanStringify(results));
+                  dom.setText("json", json.jsonBeanStringify(error));
               }
           );
        } catch(error) {
-       	results.push(error);
-          dom.setText("json", json.jsonBeanStringify(results));
+          dom.setText("json", json.jsonBeanStringify(error));
        }
 });

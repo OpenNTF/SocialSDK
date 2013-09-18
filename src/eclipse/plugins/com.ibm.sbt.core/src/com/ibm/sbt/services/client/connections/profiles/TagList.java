0a0ac156d5e9ac157ee9be57119ca195e41ff4c8
swatisingh10 <swati.singh@in.ibm.com>
2013-09-18 14:11:08 +0530

    added copyrights

--- src/eclipse/plugins/com.ibm.sbt.core/src/com/ibm/sbt/services/client/connections/profiles/TagList.java
+++ src/eclipse/plugins/com.ibm.sbt.core/src/com/ibm/sbt/services/client/connections/profiles/TagList.java
@@ -1,3 +1,19 @@
+/*
+ * © Copyright IBM Corp. 2013
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
 package com.ibm.sbt.services.client.connections.profiles;
 
 import java.util.ArrayList;
@@ -10,6 +26,10 @@ import com.ibm.sbt.services.client.Response;
 import com.ibm.sbt.services.client.base.datahandlers.EntityList;
 import com.ibm.sbt.services.client.base.datahandlers.XmlDataHandler;
 
+/**
+* @Represents Profile Tags
+* @author Swati Singh
+*/
 public class TagList extends EntityList<Tag> {
 
 	public TagList(Response requestData, TagFeedHandler feedHandler) {

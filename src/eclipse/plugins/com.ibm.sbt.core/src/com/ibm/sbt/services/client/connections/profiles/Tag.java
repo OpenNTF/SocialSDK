febc2b31648d8e01bac3d0c5cfd2ae7d534d38f6
swatisingh10 <swati.singh@in.ibm.com>
2013-09-18 13:09:35 +0530

    get Tags related changes

---
+++ src/eclipse/plugins/com.ibm.sbt.core/src/com/ibm/sbt/services/client/connections/profiles/Tag.java
@@ -0,0 +1,38 @@
+package com.ibm.sbt.services.client.connections.profiles;
+
+import com.ibm.sbt.services.client.base.BaseEntity;
+import com.ibm.sbt.services.client.base.BaseService;
+import com.ibm.sbt.services.client.base.datahandlers.DataHandler;
+
+/**
+ * tag Entry Class - representing a tag associated with a Profile.
+ *
+ * @author Swati Singh
+ */
+public class Tag extends BaseEntity{
+
+
+	public Tag(BaseService svc, DataHandler<?> handler) {
+		super(svc,handler);
+	}
+
+	public String getTerm() {
+		return getAsString(ProfileXPath.term);
+	}
+
+	public void setTerm(String term) {
+		setAsString(ProfileXPath.term,term);
+	}
+
+	public int getFrequency(){
+		return getAsInt(ProfileXPath.frequency);
+	}
+	public int getIntensity() {
+		return getAsInt(ProfileXPath.intensity);
+	}
+
+	public int getVisibility() {
+		return getAsInt(ProfileXPath.visibility);
+	}
+
+}

0cea51ef48006317ead1e7320c5175698867af95
benjamij <benjamij@ie.ibm.com>
2013-09-30 15:15:22 +0100

    UPDATE: bootstrap samples for bookmarks grid

---
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Bookmarks/Controls/BootstrapAllBookmarks.js
@@ -0,0 +1,15 @@
+require(["sbt/dom", "sbt/connections/controls/bookmarks/BookmarkGrid"], function(dom, BookmarkGrid) {
+    
+	var domNode = dom.byId("bookmarkRow");
+    var CustomBookmarkRow = domNode.text || domNode.textContent;
+	
+	var grid = new BookmarkGrid({
+		type: "any"
+	});
+
+	 grid.renderer.template = CustomBookmarkRow;
+     
+     dom.byId("gridDiv").appendChild(grid.domNode);
+              
+     grid.update();
+});

78b9295615bb5b0a2f69dbc199f6f6b15d60f336
Vineet Kanwal <vikanwal@in.ibm.com>
2013-10-04 19:40:39 +0530

    Added unit test for Activity Service JS

---
+++ test/selenium/com.ibm.sbt.automation.test/src/com/ibm/sbt/test/js/connections/activities/api/CreateActivity.java
@@ -0,0 +1,34 @@
+package com.ibm.sbt.test.js.connections.activities.api;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.ibm.commons.util.io.json.JsonJavaObject;
+import com.ibm.sbt.automation.core.test.connections.BaseActivitiesTest;
+import com.ibm.sbt.automation.core.test.pageobjects.JavaScriptPreviewPage;
+import com.ibm.sbt.services.client.connections.activity.Activity;
+import com.ibm.sbt.services.client.connections.activity.ActivityNode;
+
+public class CreateActivity extends BaseActivitiesTest {
+
+	static final String SNIPPET_ID = "Social_Activities_API_CreateActivity";
+
+	String activityId = null;
+
+	@After
+	public void destroy() {
+		deleteActivity(activityId);
+	}
+
+	@Test
+	public void testCreateActivity() {
+		JavaScriptPreviewPage previewPage = executeSnippet(SNIPPET_ID);
+		JsonJavaObject json = previewPage.getJson();
+		activityId = json.getAsString("getActivityId");
+		Assert.assertNotNull(activityId);
+	}
+}

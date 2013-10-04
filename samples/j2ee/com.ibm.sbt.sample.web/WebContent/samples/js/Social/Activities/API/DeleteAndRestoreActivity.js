ff2b85d7035abf1d1b320a58b3a12c3eb562a3f9
Vineet Kanwal <vikanwal@in.ibm.com>
2013-10-04 19:08:19 +0530

    Activity Service JS APIs

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Activities/API/DeleteAndRestoreActivity.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Activities/API/DeleteAndRestoreActivity.js
@@ -1,12 +1,12 @@
 require([ "sbt/dom", "sbt/json", "sbt/connections/ActivityService" ], function(dom, json, ActivityService) {
 	var activitiesService = new ActivityService();
-	var activityNodeId = "%{name=sample.activityNodeId|helpSnippetId=Social_Activities_API_GetActivityNode}";
-	activitiesService.deleteActivityNode(activityNodeId).then(function(response) {
+	var activityId = "%{name=sample.activityId|helpSnippetId=Social_Activities_API_GetActivity}";
+	activitiesService.deleteActivity(activityId).then(function(response) {
 		return response;
 	}, function(error) {
 		dom.setText("json", json.jsonBeanStringify(error));
 	}).then(function(response) {
-		var promise = activitiesService.restoreActivityNode(activityNodeId);
+		var promise = activitiesService.restoreActivity(activityId);
 		promise.then(function(response) {
 			dom.setText("json", json.jsonBeanStringify(response));
 		}, function(error) {

2a0dd72fdaf210e83f087aba042c6339376c4642
Mark Wallace <mark_wallace@ie.ibm.com>
2013-10-07 15:02:17 +0100

    Working on recommendations API

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Forums/API/GetForumReply.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Forums/API/GetForumReply.js
@@ -2,7 +2,7 @@ require(["sbt/connections/ForumService", "sbt/dom", "sbt/json"],
     function(ForumService, dom, json) {
         var forumService = new ForumService();  
     	var replyUuid = "%{name=ForumService.replyUuid}";	
-        var promise = forumService.getForumTopic(replyUuid);
+        var promise = forumService.getForumReply(replyUuid);
         promise.then(
             function(forumReply) {
                dom.setText("json", json.jsonBeanStringify(forumReply));

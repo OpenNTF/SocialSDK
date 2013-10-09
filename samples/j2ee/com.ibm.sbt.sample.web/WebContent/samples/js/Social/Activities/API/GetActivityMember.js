b5ed32abd20e79734e93014bcdc506b6663c1dd6
Vineet Kanwal <vikanwal@in.ibm.com>
2013-10-09 16:46:07 +0530

    incorporated review comments for ActivityService JS

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Activities/API/GetActivityMember.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Activities/API/GetActivityMember.js
@@ -1,7 +1,7 @@
 require([ "sbt/dom", "sbt/json", "sbt/connections/ActivityService" ], function(dom,json,ActivityService) {
         var activitiesService = new ActivityService();
-        var activityId = "%{name=sample.activityId|helpSnippetId=Social_Activities_API_CreateActivity}";
-        var memberId = "%{name=sample.memberId|helpSnippetId=Social_Activities_API_AddMemberToActivity}";
+        var activityId = "%{name=sample.activityId|helpSnippetId=Social_Activities_API_GetMyActivities}";
+        var memberId = "%{name=sample.memberId|helpSnippetId=Social_Activities_API_GetActivityMembers}";
         var promise = activitiesService.getMember(activityId, memberId);
         promise.then(
             function(member) {

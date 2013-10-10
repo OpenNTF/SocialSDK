81cf58633b7af5769dadf19a5e9dc93ba2c5b7a2
Francis Moloney <franmolo@ie.ibm.com>
2013-10-10 11:36:44 +0100

    Add some more to the events. Update sample to iunclude date filters.

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Communities/API/CommunityEvents.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Communities/API/CommunityEvents.js
@@ -1,25 +1,30 @@
 require([ "sbt/dom", "sbt/json", "sbt/connections/CommunityService" ], function(dom,json,CommunityService) {
-  var communityService = new CommunityService({endpoint: "w3connections"});
-        var promise = communityService.getCommunityEvents();
-        
-        promise.then(
-            function(events) {
-              var detailedEvents = [];
-              var i;
-              for(i=0; i < events.length; i++){
-                events[i].getDetailedEvent().then(function(event){
-                  detailedEvents.push(event);
-                  return i;
-                }).then(function(index){
+  var communityService = new CommunityService();
+  var startDate = new Date();
+  startDate.setDate(startDate.getDate()-10);
+  
+  var promise = communityService.getCommunityEvents("%{name=sample.communityId|label=communityId|helpSnippetId=Social_Communities_Get_My_Communities}", startDate.toISOString());
+  promise.then(
+      function(events) {
+          if(events.length === 0){
+              dom.setText("json", "No results, try varying the startDate and endDate parameters");
+          }
+          var detailedEvents = [];
+          var i;
+          for(i=0; i < events.length; i++){
+              events[i].getDetailedEvent().then( // we need an extra network request to populate all event information, e.g. its content
+                  function(event){
+                      detailedEvents.push(event);
+                      return i;
+              }).then(function(index){
                   if(index === events.length){
-                    dom.setText("json", json.jsonBeanStringify(detailedEvents));
-                  }
-                });
-              }
-            },
-            function(error) {
-                dom.setText("json", json.jsonBeanStringify(error));
-            }
-        );
-    }
-);
+                      dom.setText("json", json.jsonBeanStringify(detailedEvents));
+                 }
+              });
+          }
+      },
+      function(error) {
+          dom.setText("json", json.jsonBeanStringify(error));
+      }
+  );
+});

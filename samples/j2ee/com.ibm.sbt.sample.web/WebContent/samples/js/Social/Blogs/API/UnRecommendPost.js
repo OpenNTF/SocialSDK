f5a28941d7675aee7a10334449278cd667c1a788
RajmeetBal <rajmeetbal.in.ibm.com>
2013-10-15 14:58:07 +0530

    Used promise chaining in new samples

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Blogs/API/UnRecommendPost.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Blogs/API/UnRecommendPost.js
@@ -1,54 +1,45 @@
+
 require(["sbt/connections/BlogService", "sbt/dom", "sbt/json"], 
+		
     function(BlogService, dom, json) {
         var blogService = new BlogService(); 
         var now = new Date();
         var post = blogService.newPost();
         post.setTitle("Post at " + now.getTime());
         post.setContent("Post Content at " + now.getTime());
+        var blogHandle;
+        var blogPostId;
         
     	blogService.getBlogs({ ps: 1 }).then(
             function(blogs){
-                if (blogs.length == 0) {
-                    text = "No Blogs found.";
-                    dom.setText("content", text);
-                } else {
-                        var firstBlogHandle = blogs[0].getHandle();
-				        var promise = blogService.createPost(post, firstBlogHandle);
-				        promise.then(
-			        		function(post) {
-			        			var postId = post.getPostUuid();
-		        				var recPromise = blogService.recommendPost(postId, firstBlogHandle);
-		        				recPromise.then(
-	    			        		function() {
-	    			        			unrecPromise = blogService.unRecommendPost(postId, firstBlogHandle);
-	    		        				unrecPromise.then(
-	    	    			        		function() {
-	    	    			        			var returnOjject = {};
-	    	    			        			returnOjject.status = unrecPromise.response.status;
-	    	    			        			dom.setText("json", json.jsonBeanStringify(returnOjject));
-	    	    			                },
-	    	    			                function(error) {
-	    	    			                    dom.setText("json", json.jsonBeanStringify(error));
-	    	    			                }
-	    	    				        );
-	    			                },
-	    			                function(error) {
-	    			                    dom.setText("json", json.jsonBeanStringify(error));
-	    			                }
-	    				        );
-		        				
-			                },
-			                function(error) {
-			                    dom.setText("json", json.jsonBeanStringify(error));
-			                }
-				        );
+            	return blogs[0].getHandle();
+            }
+    	).then(
+			function(firstBlogHandle){
+				blogHandle = firstBlogHandle;
+				return blogService.createPost(post, blogHandle);
+            }
+    	).then(
+			function(createdPost){
+				blogPostId = createdPost.getPostUuid();
+				return blogService.recommendPost(blogPostId, blogHandle);
+            }
+    	).then(
+			function() {
+				var unRecommendPromise = blogService.unRecommendPost(blogPostId, blogHandle);
+				unRecommendPromise.then(
+				function(){
+	    			var returnOjject = {};
+	    			returnOjject.status = unRecommendPromise.response.status;
+	    			dom.setText("json", json.jsonBeanStringify(returnOjject));
+				},
+                function(error) {
+                    dom.setText("json", json.jsonBeanStringify(error));
                 }
-            },
-            function(error){
-                dom.setText("content", "Error code:" +  error.code + ", message:" + error.message);
-            }   
-    	);
-					        
-					        
-    }
+    			);
+            }
+		);
+	
+	}
+
 );

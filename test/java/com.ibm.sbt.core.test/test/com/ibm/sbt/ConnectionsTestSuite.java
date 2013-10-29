f6d0be2a7d20e0eed7e6e956129f78f98a4dab0b
Lorenzo Boccaccia <LORENZOB@ie.ibm.com>
2013-10-29 20:07:57 +0000

    added file service to test suite, fixed null entity serialization

--- test/java/com.ibm.sbt.core.test/test/com/ibm/sbt/ConnectionsTestSuite.java
+++ test/java/com.ibm.sbt.core.test/test/com/ibm/sbt/ConnectionsTestSuite.java
@@ -5,9 +5,10 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import com.ibm.sbt.services.client.connections.communities.CommunityServiceTest;
+import com.ibm.sbt.services.client.connections.files.FileServiceTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({CommunityServiceTest.class})
+@SuiteClasses({CommunityServiceTest.class, FileServiceTest.class})
 public class ConnectionsTestSuite {
 
 }

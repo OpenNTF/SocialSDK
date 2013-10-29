aba44b8f63f3c33bf59d714c887c95f7554c7a1f
Lorenzo Boccaccia <LORENZOB@ie.ibm.com>
2013-10-29 20:07:57 +0000

    added file service to test suite, fixed null entity serialization

--- test/java/com.ibm.sbt.core.test/test/com/ibm/sbt/SmartCloudTestSuite.java
+++ test/java/com.ibm.sbt.core.test/test/com/ibm/sbt/SmartCloudTestSuite.java
@@ -7,10 +7,11 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import com.ibm.sbt.services.client.connections.communities.CommunityServiceTest;
+import com.ibm.sbt.services.client.connections.files.FileServiceTest;
 import com.ibm.sbt.test.lib.TestEnvironment;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CommunityServiceTest.class })
+@SuiteClasses({ CommunityServiceTest.class, FileServiceTest.class })
 public class SmartCloudTestSuite {
 	@BeforeClass
 	public static void before() {

f960c79d2c20351d022bf2c3b81af130511452af
padraic-edwards <padraic.edwards@ie.ibm.com>
2013-10-30 15:58:40 +0000

    shortcuts to SDK services

--- domino/eclipse/plugins/com.ibm.xsp.sbtsdk.core/src/sbt/ConnectionsProfileService.java
+++ domino/eclipse/plugins/com.ibm.xsp.sbtsdk.core/src/sbt/ConnectionsProfileService.java
@@ -1,11 +1,11 @@
 /*
- * © Copyright IBM Corp. 2010
+ * © Copyright IBM Corp. 2011
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
  * You may obtain a copy of the License at:
  * 
- * http://www.apache.org/licenses/LICENSE-2.0 
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software 
  * distributed under the License is distributed on an "AS IS" BASIS, 
@@ -15,23 +15,20 @@
  */
 package sbt;
 
+import com.ibm.sbt.services.client.connections.profiles.ProfileService;
 import com.ibm.sbt.services.endpoints.Endpoint;
 
+public class ConnectionsProfileService extends ProfileService {
 
-/**
- * Connections service.
- * @author Philippe Riand
- */
-public class ConnectionsService extends com.ibm.sbt.services.client.connections.ConnectionsService {
-	public ConnectionsService() {
+	public ConnectionsProfileService() {
+		super(DEFAULT_ENDPOINT_NAME, DEFAULT_CACHE_SIZE);
 	}
 
-	public ConnectionsService(Endpoint endpoint) {
-		super(endpoint);
+	public ConnectionsProfileService(String endpointName) {
+		super(endpointName, DEFAULT_CACHE_SIZE);
 	}
 
-	public ConnectionsService(String endpointName) {
-		super(endpointName);
-	}   
-    
+	public ConnectionsProfileService(Endpoint endpoint) {
+		super(endpoint, DEFAULT_CACHE_SIZE);
+	}
 }

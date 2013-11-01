6ef7fb16db46fdda5a09aace335cb4dd2c256552
RajmeetBal <rajmeetbal.in.ibm.com>
2013-11-01 19:57:43 +0530

    did the review changes

--- src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/connections/FollowConstants.js
+++ src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/connections/FollowConstants.js
@@ -22,25 +22,43 @@ define([ "../lang", "./ConnectionsConstants" ], function(lang,conn) {
     return lang.mixin(conn, {
     	
     	/**
-         * Profile resource
+         * Activities Source
          * 
-         * @property ProfilesSource
+         * @property ActivitiesSource
          * @type String
          * @for sbt.connections.FollowedResource
          */
-        ProfilesSource : "profiles",
-
-        /**
-         * Profile Resource Type
-         * 
-         * @property ProfilesResourceType
-         * @type String
-         * @for sbt.connections.FollowedResource
-         */
-        ProfilesResourceType : "profile",
+        ActivitiesSource : "activities",
         
         /**
-         * Community resource
+         * Activity Resource Type
+         * 
+         * @property ActivityResourceType
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        ActivityResourceType : "activity",
+        
+        /**
+         * Blogs Source
+         * 
+         * @property BlogsSource
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        BlogsSource : "blogs",
+        
+        /**
+         * Blog Resource Type
+         * 
+         * @property BlogResourceType
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        BlogResourceType : "blog",
+        
+        /**
+         * Communities Source
          * 
          * @property CommunitiesSource
          * @type String
@@ -56,7 +74,178 @@ define([ "../lang", "./ConnectionsConstants" ], function(lang,conn) {
          * @for sbt.connections.FollowedResource
          */
         CommunitiesResourceType : "community",
-    	
+        
+        /**
+         * Files Source
+         * 
+         * @property FilesSource
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        FilesSource : "files",
+
+        /**
+         * File Resource Type
+         * 
+         * @property FileResourceType
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        FileResourceType : "file",
+        
+        /**
+         * FileFolder Resource Type
+         * 
+         * @property FileFolderResourceType
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        FileFolderResourceType : "file_folder",
+        
+        /**
+         * Forums Source
+         * 
+         * @property ForumsSource
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        ForumsSource : "forums",
+
+        /**
+         * Forum Resource Type
+         * 
+         * @property ForumResourceType
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        ForumResourceType : "forum",
+        
+        /**
+         * ForumTopic Resource Type
+         * 
+         * @property ForumTopicResourceType
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        ForumTopicResourceType : "forum_topic",
+        
+        /**
+         * Profile Source
+         * 
+         * @property ProfilesSource
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        ProfilesSource : "profiles",
+
+        /**
+         * Profile Resource Type
+         * 
+         * @property ProfilesResourceType
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        
+        ProfilesResourceType : "profile",
+        
+        /**
+         * Wikis Source
+         * 
+         * @property WikisSource
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        WikisSource : "wikis",
+
+        /**
+         * Wiki Resource Type
+         * 
+         * @property WikiResourceType
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        WikiResourceType : "wiki",
+        
+        /**
+         * WikiPage Resource Type
+         * 
+         * @property WikiPageResourceType
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        WikiPageResourceType : "wiki_page",
+        
+        /**
+         * Tags Source
+         * 
+         * @property TagsSource
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        TagsSource : "tags",
+
+        /**
+         * Tag Resource Type
+         * 
+         * @property TagResourceType
+         * @type String
+         * @for sbt.connections.FollowedResource
+         */
+        TagResourceType : "tag",
+        
+        
+        /**
+		 * Get the followed resources feed
+		 * 
+		 * @method getFollowedResources
+		 * @param {String} source String specifying the resource. Options are:
+		 *
+		 *	activities
+		 *	blogs
+		 *	communities
+		 *	files
+		 *	forums
+		 *	profiles
+		 *	wikis
+		 *	tags
+		 *
+		 * @param {String} resourceType String representing the resource type. Options are:
+		 * 
+		 * If source=activities
+		 * 	   activity
+		 * 
+		 * If source=blogs
+		 *     blog
+		 *     
+		 * If source=communities
+		 *     community
+		 *     
+		 * If source=files
+		 *     file
+		 *     file_folder
+		 *      
+		 * 
+		 * If source=forums
+		 *     forum
+		 *     forum_topic
+		 *      
+		 * 
+		 * If source=profiles
+		 *     profile
+		 *     
+		 * If source=wikis
+		 *     wiki
+		 *     wiki_page
+		 *      
+		 * 
+		 * If source=tags
+		 *     tag
+		 * 
+		 * @param {Object} [args] Object representing various parameters
+         * that can be passed to get a feed of members of a
+         * community. The parameters must be exactly as they are
+         * supported by IBM Connections like ps, sortBy etc.
+		 */
     	FollowedResourceFeedXPath : conn.ConnectionsFeedXPath,
     	
     	FollowedResourceXPath : lang.mixin({

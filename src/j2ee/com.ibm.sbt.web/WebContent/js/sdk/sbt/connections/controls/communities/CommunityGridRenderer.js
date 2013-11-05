45bee73aa3f8baaf4cf0e98f81d6f941a17ef23e
benjamij <benjamij@ie.ibm.com>
2013-11-05 11:55:44 +0000

    BUGFIX: Fixed bug related to display of restricted icon

--- src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/connections/controls/communities/CommunityGridRenderer.js
+++ src/j2ee/com.ibm.sbt.web/WebContent/js/sdk/sbt/connections/controls/communities/CommunityGridRenderer.js
@@ -137,6 +137,24 @@ define(["../../../declare", "../../../stringUtil", "../../../i18n", "../../../la
          },
          
          /**
+          * Displays the restricted image for a community
+          * @method displayRestricted
+          * @param grid The Grid Element
+          * @param item An Object containing all of the data for the current row
+          * @param i The number of the current row
+          * @param items  an object array containing the data for all of the grid rows
+          * @returns {String}
+          */
+         displayRestricted: function(grid, item, i, items) {
+        	 var communityType = item.getValue("communityType");
+             if (communityType == undefined || communityType != "publicInviteOnly") {
+                 return "display: none";
+             } else {
+                 return "";
+             }
+         },
+         
+         /**
           * Get the tags for a community
           * @param grid The Grid Element
           * @param item An Object containing all of the data for the current row

48461a37766d93ebc6023996641eafec5195cff4
benjamij <benjamij@ie.ibm.com>
2013-11-12 13:28:06 +0000

    UPDATE: Renamed samples

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Communities/Controls/PublicCommunitiesGrid.js
+++
@@ -1,9 +0,0 @@
-require(["sbt/dom", "sbt/connections/controls/communities/CommunityGrid"], function(dom, CommunityGrid) {
-    var grid = new CommunityGrid();
-           
-    dom.byId("gridDiv").appendChild(grid.domNode);
-             
-    grid.update();
-});
-
-

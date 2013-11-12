886a94263d9a7d9b9b5cbb14684442d00f0b491f
benjamij <benjamij@ie.ibm.com>
2013-11-12 13:26:03 +0000

    UPDATE: Renamed samples

---
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Bookmarks/Get  Bookmarks  Tags.js
@@ -0,0 +1,33 @@
+require(["sbt/connections/BookmarkService", "sbt/dom"], 
+    function(BookmarkService, dom) {
+	    var createRow = function(tag) {
+	        var table = dom.byId("bookmarksTable");
+	        var tr = document.createElement("tr");
+	        table.appendChild(tr);
+	        var td = document.createElement("td");
+	        td.innerHTML = tag.getTerm();
+	        tr.appendChild(td);
+	        td = document.createElement("td");
+	        td.innerHTML = tag.getFrequency();
+	        tr.appendChild(td);
+	    };
+    
+    	var bookmarkService = new BookmarkService();
+    	bookmarkService.getBookmarksTags({ ps: 5 }).then(
+            function(tags){
+                if (tags.length == 0) {
+                    text = "bookmarks tags returned no results.";
+                    dom.setText("content", text);
+                } else {
+                    for(var i=0; i<tags.length; i++){
+                        var tag = tags[i];
+                        createRow(tag);
+                    }
+                }
+            },
+            function(error){
+                dom.setText("content", "Error code:" +  error.code + ", message:" + error.message);
+            }       
+    	);
+    }
+);

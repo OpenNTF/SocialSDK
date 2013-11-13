6b854130402f400117f908e52fca591c5c762cd6
Mark Wallace <mark_wallace@ie.ibm.com>
2013-11-13 15:49:28 +0000

    Add missing property files and doc.html files

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Toolkit/DemoSnippet.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Toolkit/DemoSnippet.js
@@ -1,5 +1,4 @@
 require([ "sbt/dom", "sbt/json" ], function(dom,json) {
-    
     var results = {
         name : "DemoSnippet",
         communityUuid : "%{name=sample.communityId|helpSnippetId=Social_Communities_Get_My_Communities}"

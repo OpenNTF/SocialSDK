2fb1f32ac061a06ab4dadf77ed6e48fb4d55984c
projsaha <projsaha@in.ibm.com>
2013-11-17 16:42:17 +0530

    test cases for utilities and profiles samples

---
+++ test/selenium/com.ibm.sbt.automation.test/src/com/ibm/sbt/test/js/connections/profiles/api/ProfileCreatePostData.java
@@ -0,0 +1,44 @@
+/*
+ * � Copyright IBM Corp. 2013
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+package com.ibm.sbt.test.js.connections.profiles.api;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.junit.Assert;
+
+import com.ibm.sbt.automation.core.test.pageobjects.JavaScriptPreviewPage;
+
+/**
+ * @author projsaha
+ *  
+ * @date 15 Nov 2013
+ */
+public class ProfileCreatePostData extends BaseProfileEntryTest {
+    
+    static final String SNIPPET_ID = "Social_Profiles_API_ProfileCreatePostData";
+
+    @Test
+    public void testProfileCreatePostData() {
+        JavaScriptPreviewPage previewPage = executeSnippet(SNIPPET_ID);
+        String newPost = previewPage.getWebElement().findElement(By.id("newPost")).getText();
+        String newPut = previewPage.getWebElement().findElement(By.id("newPut")).getText();
+        Assert.assertNotNull(newPost);
+        Assert.assertNotNull(newPut);
+        
+        
+    }
+
+}

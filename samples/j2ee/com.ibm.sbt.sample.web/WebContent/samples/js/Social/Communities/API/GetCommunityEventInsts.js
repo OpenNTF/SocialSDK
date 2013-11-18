fe244b959f6fe04b53f20ea6c0c477b69db01f5c
Francis Moloney <franmolo@ie.ibm.com>
2013-11-18 12:57:35 +0000

    Clean up events implementation, add some samples.

--- samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Communities/API/GetCommunityEventInsts.js
+++ samples/j2ee/com.ibm.sbt.sample.web/WebContent/samples/js/Social/Communities/API/GetCommunityEventInsts.js
@@ -2,15 +2,15 @@ require([ "sbt/dom", "sbt/json", "sbt/connections/CommunityService" ], function(
   
   var communityId = "%{name=CommunityService.communityUuid|label=Community Id|helpSnippetId=Social_Communities_Get_My_Communities}";
   var startDate = "%{name=sample.startDate|label=Start Date}";
-
+  
   var communityService = new CommunityService();
-  var promise = communityService.getCommunityEvents(communityId, startDate);
+  var promise = communityService.getCommunityEventInsts(communityId, startDate);
   promise.then(
-      function(events) {
-          if( events.length === 0) {
+      function(eventInsts) {
+          if( eventInsts.length === 0) {
               dom.setText("json", "There are no events scheduled after: "+startDate);
           }
-          dom.setText("json", json.jsonBeanStringify(events));
+          dom.setText("json", json.jsonBeanStringify(eventInsts));
       },
       function(error) {
           dom.setText("json", json.jsonBeanStringify(error));

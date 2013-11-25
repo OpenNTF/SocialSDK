f820ed7d4436a24e504c697ad69bacd94a91f909
mduarte <mariofduarte@gmail.com>
2013-11-25 14:58:57 +0000

    Created wiki service. Added new xml/atom serialization classes.

---
+++ sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/services/client/base/serializers/AtomEntitySerializer.java
@@ -0,0 +1,94 @@
+/*
+ * � Copyright IBM Corp. 2013
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+package com.ibm.sbt.services.client.base.serializers;
+
+import java.util.List;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import com.ibm.sbt.services.client.base.AtomEntity;
+import com.ibm.sbt.services.client.base.ConnectionsConstants.Namespaces;
+
+/**
+ * @author Mario Duarte
+ *
+ */
+public abstract class AtomEntitySerializer<T extends AtomEntity> extends BaseEntitySerializer<T> {
+
+	public AtomEntitySerializer(T entity) {
+		super(entity);
+	}
+	
+	public Node genericAtomEntry() {
+		Node entry = entry();
+		
+		appendChilds(entry, 
+			title(),
+			id(),
+			published(),
+			updated(),
+			summary(),
+			content()
+		);
+		
+		appendChilds(entry, author());
+		appendChilds(entry, contributor());
+		appendChilds(entry, tags());
+		return entry;
+	}
+
+	protected Node entry() {
+		return rootNode(element(Namespaces.ATOM, "entry"));
+	}
+	
+	protected Element title() {
+		return textElement("title", entity.getTitle(), attribute("type", "text"));
+	}
+	
+	protected Element id() {
+		return textElement("id", entity.getId());
+	}
+	
+	protected Element published() {
+		return textElement("published", DataSerializer.toString(entity.getPublished()));
+	}
+	
+	protected Element updated() {
+		return textElement("updated", DataSerializer.toString(entity.getUpdated()));
+	}
+	
+	protected Element summary() {
+		return textElement("summary", entity.getSummary(), attribute("type", "text"));
+	}
+	
+	protected Element content() {
+		return textElement("content", entity.getContent(), attribute("type", "text"));
+	}
+	
+	protected Element author() {
+		return null;  //FIXME
+	}
+	
+	protected Element contributor() {
+		return null;  //FIXME
+	}
+
+	protected List<Element> tags() {
+		return null;  //FIXME
+	}
+}
---
+++ sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/services/client/connections/wikis/serializers/WikiPageSerializer.java
@@ -0,0 +1,69 @@
+/*
+ * � Copyright IBM Corp. 2013
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+package com.ibm.sbt.services.client.connections.wikis.serializers;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import com.ibm.sbt.services.client.base.ConnectionsConstants.Namespaces;
+import com.ibm.sbt.services.client.base.serializers.AtomEntitySerializer;
+import com.ibm.sbt.services.client.connections.wikis.WikiPage;
+
+/**
+ * @author Mario Duarte
+ *
+ */
+public class WikiPageSerializer extends AtomEntitySerializer<WikiPage> {
+
+	public WikiPageSerializer(WikiPage entity) {
+		super(entity);
+	}
+
+	public void generateCreate() {
+		Node entry = entry();
+		
+		appendChilds(entry,
+				title(),
+				label(),
+				wikiPageCategory(),
+				summary(),
+				content()
+		);
+		
+		appendChilds(entry, tags());
+	}
+
+	public void generateUpdate() {
+		Node entry = genericAtomEntry();
+		
+		appendChilds(entry,
+				label(),
+				wikiPageCategory()
+		);
+	}
+
+	private Element label() {
+		return textElement(Namespaces.TD, "label", entity.getLabel());
+	}
+	
+	private Element wikiPageCategory() {
+		return element("category", 
+				attribute("scheme", "tag:ibm.com,2006:td/type"), 
+				attribute("term", "page"), 
+				attribute("label", "page"));
+	}
+}

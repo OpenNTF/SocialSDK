2e68f38d0c7a28f0a1a9bdadd6a707dd85672a8e
Manish Kataria <mkataria@in.ibm.com>
2013-11-29 19:45:07 +0530

    First batch of changes for serialization for entity objects. Changes were tested for ProfilesService ( XML based service ). Once this gets merged would checkin changes for other service , including JSON based services.

---
+++ sdk/com.ibm.sbt.core.test/src/test/java/com/ibm/sbt/services/client/SerializationUtil.java
@@ -0,0 +1,27 @@
+package com.ibm.sbt.services.client;
+
+import java.io.*;
+
+import com.ibm.commons.util.StringUtil;
+import com.ibm.sbt.services.client.base.BaseEntity;
+import com.ibm.sbt.services.client.connections.profiles.Profile;
+
+public abstract class SerializationUtil {
+	
+	protected String serFile = "serFile.out";
+	
+	public void isSerializable(BaseEntity basentity){
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(serFile)); 
+			oos.writeObject(basentity);
+			oos.close();
+			validateSerializable();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public abstract void validateSerializable();
+
+}

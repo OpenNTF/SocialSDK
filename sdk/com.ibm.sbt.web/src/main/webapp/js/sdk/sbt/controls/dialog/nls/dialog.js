b877ca2807bda460df02135e5a3f2982bdec47c9
Mark Wallace <mark_wallace@ie.ibm.com>
2013-12-02 17:03:03 +0000

    Add new method ForumService.getCommunityForumTopics

--- sdk/com.ibm.sbt.web/src/main/webapp/js/sdk/sbt/controls/dialog/nls/dialog.js
+++ sdk/com.ibm.sbt.web/src/main/webapp/js/sdk/sbt/controls/dialog/nls/dialog.js
@@ -5,37 +5,24 @@
  * you may not use this file except in compliance with the License. 
  * You may obtain a copy of the License at:
  * 
- * http://www.apache.org/licenses/LICENSE-2.0 
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software 
  * distributed under the License is distributed on an "AS IS" BASIS, 
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
  * implied. See the License for the specific language governing 
  * permissions and limitations under the License.
- */
+ */ 
 
 /**
+ * Social Business Toolkit SDK
  * 
+ * Resource bundle for dialog control.
  */
-define([ "../_bridge/declare", "../lang", "dijit/Dialog", "dijit/_TemplatedMixin"], 
-    function(declare, lang, Dialog, _TemplatedMixin) {
-
-    /**
-     * @module sbt.widget._TemplatedDialog
-     */
-    var _TemplatedDialog = declare([ Dialog, _TemplatedMixin ], {        
-    	
-    	nls : null,
-    	
-    	/**
-		 * _TemplatedDialog class constructor function
-		 * 
-		 * @method constructor
-		 * @param args
-		 */
-		constructor: function(args) {	
-		}
-    });
-    
-    return _TemplatedDialog;
+define({
+  root: ({	 
+	  OK : "OK",
+	  Cancel : "Cancel",
+	  Close : "Close"
+  })
 });

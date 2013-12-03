405e5f5995a5ea3de6e5929583be709f258a5ab0
Ryan J Baxter <rbaxter85@apache.org>
2013-12-02 20:18:06 -0500

    Fixed some headers

--- domino/com.ibm.sbt.opensocial.domino/src/com/ibm/sbt/opensocial/domino/servlets/SecurityTokenServlet.java
+++ domino/com.ibm.sbt.opensocial.domino/src/com/ibm/sbt/opensocial/domino/servlets/SecurityTokenServlet.java
@@ -1,17 +1,3 @@
-/* ***************************************************************** */
-/*                                                                   */
-/* IBM Confidential                                                  */
-/*                                                                   */
-/* OCO Source Materials                                              */
-/*                                                                   */
-/* Copyright IBM Corp. 2012                                          */
-/*                                                                   */
-/* The source code for this program is not published or otherwise    */
-/* divested of its trade secrets, irrespective of what has been      */
-/* deposited with the U.S. Copyright Office.                         */
-/*                                                                   */
-/* ***************************************************************** */
-
 package com.ibm.sbt.opensocial.domino.servlets;
 
 import java.io.IOException;

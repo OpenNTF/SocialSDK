754219c68b9c96a1f3dc131af6c2da03ce55174a
Lorenzo Boccaccia <LORENZOB@ie.ibm.com>
2013-12-03 14:51:02 +0000

    found latest changes, new files wheren't showing in the diff

--- samples/j2ee/snippets/com.ibm.sbt.sample.web/src/main/webapp/samples/js/Social/Forums/API/GetCommunityForumTopics.js
+++ samples/j2ee/snippets/com.ibm.sbt.sample.web/src/main/webapp/samples/js/Social/Forums/API/GetCommunityForumTopics.js
@@ -1,6 +1,6 @@
 require([ "sbt/dom", "sbt/json", "sbt/connections/ForumService" ], function(dom,json,ForumService) {
         var forumService = new ForumService();
-        var promise = forumService.getCommunityForumTopics("%{ForumService.communityUuid}");
+        var promise = forumService.getForumTopics("%{ForumService.forumUuid}", "%{ForumService.communityUuid}");
         promise.then(
             function(topics) {
                 dom.setText("json", json.jsonBeanStringify(topics));

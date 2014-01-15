0d3544506a2615c4d8df68683d56526c33d6a883
priand <priand@us.ibm.com>
2014-01-15 11:51:43 -0500

    Playground Gadget import with properties and filtering

--- samples/domino/com.ibm.xsp.sbtsdk.playground/src/nsf/playground/beans/GadgetSnippetBean.java
+++ samples/domino/com.ibm.xsp.sbtsdk.playground/src/nsf/playground/beans/GadgetSnippetBean.java
@@ -4,6 +4,7 @@ import java.net.URLEncoder;
 
 import nsf.playground.environments.PlaygroundEnvironment;
 
+import com.ibm.commons.util.StringUtil;
 import com.ibm.sbt.opensocial.domino.container.ContainerExtPointException;
 import com.ibm.sbt.playground.assets.AssetNode;
 import com.ibm.sbt.playground.assets.CategoryNode;
@@ -28,6 +29,26 @@ public abstract class GadgetSnippetBean extends AssetBean {
 	
 	protected AssetNode createAssetNode(String notesUnid, CategoryNode parent, String name, String category, String assetId) {
 		return new GadgetSnippetAssetNode(parent,name,category,notesUnid,assetId);
+	}
+	
+	// For gadgets, the runtime platform is *not* enough, but it also needs to use one of the OAuth
+	// endpoints. So we have to check that the Endpoint is actually an OAuth endpoint
+	@Override
+	protected boolean runtimeExists(PlaygroundEnvironment env, String name) {
+		if(name.endsWith("OA")) {
+			return isEndpoint(env, name, name.substring(0, name.length()-2));
+		} else if(name.endsWith("OA2")) {
+			return isEndpoint(env, name, name.substring(0, name.length()-3));
+		} else {
+			return super.runtimeExists(env, name);
+		}
+	}
+	protected boolean isEndpoint(PlaygroundEnvironment env, String epName, String name) {
+		if(super.runtimeExists(env, name)) {
+			String p = env.getPropertyValueByName("sbt.endpoint."+name);
+			return StringUtil.equals(p, epName);
+		}
+		return false;
 	}
 
 	/**

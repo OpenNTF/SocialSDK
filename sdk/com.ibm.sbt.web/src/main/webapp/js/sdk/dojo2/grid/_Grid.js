0242855b9073da257a043eb33e9adebb337923e8
David Ryan <daviryan@ie.ibm.com>
2014-01-15 08:25:38 +0000

    Fix for the grid paging so that when a new file is added it displays the correct total number of files. Also removed extra spaces from the NLS String for the paging to match what is used in connections.

--- sdk/com.ibm.sbt.web/src/main/webapp/js/sdk/dojo2/grid/_Grid.js
+++ sdk/com.ibm.sbt.web/src/main/webapp/js/sdk/dojo2/grid/_Grid.js
@@ -50,7 +50,7 @@ define([ "../../declare", "../../store/AtomStore", "dojo/_base/lang", "dojo/_bas
                      start : options.start,
                      end : options.start + items.length,
                      count : items.length,
-                     totalCount : totalCount,
+                     totalCount : items.length,
                      response : results.response
                     };
                     try {

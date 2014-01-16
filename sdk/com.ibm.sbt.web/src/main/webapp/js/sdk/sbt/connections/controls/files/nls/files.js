168c7853a4889936797fea8e79b1835e1f3875a2
David Ryan <daviryan@ie.ibm.com>
2014-01-16 15:40:10 +0000

    Share File Widget Fixes

--- sdk/com.ibm.sbt.web/src/main/webapp/js/sdk/sbt/connections/controls/files/nls/files.js
+++ sdk/com.ibm.sbt.web/src/main/webapp/js/sdk/sbt/connections/controls/files/nls/files.js
@@ -45,7 +45,7 @@ define({
     	labelAsReader: "as Reader",
     	labelAsEditor: "as Editor",
     	labelRole: "Role",
-    	labelPersonName: "Person name",
+    	labelPersonName: "Person name or email",
     	labelCommunityName: "Community name...",
     	
     	titleEnterTags : "Please enter the tags you want to add.",

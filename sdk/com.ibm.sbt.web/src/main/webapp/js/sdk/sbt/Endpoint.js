0016519f6dec1677b9b42d2bf46ba3f178c292a2
benjamij <benjamij@ie.ibm.com>
2014-01-21 09:56:58 +0000

    UPDATE: Fixed endpoint spaces and action url issues

--- sdk/com.ibm.sbt.web/src/main/webapp/js/sdk/sbt/Endpoint.js
+++ sdk/com.ibm.sbt.web/src/main/webapp/js/sdk/sbt/Endpoint.js
@@ -1,6 +1,6 @@
 /**
  * 
- * © Copyright IBM Corp. 2012
+ * (C) Copyright IBM Corp. 2012
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 

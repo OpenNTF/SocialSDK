59e0724a7ce352bdf7760b17438adb80dc5518d0
Francis Moloney <franmolo@ie.ibm.com>
2014-01-21 09:20:13 +0000

    Add nls for unsupported browser. Remove comments.

--- sdk/com.ibm.sbt.web/src/main/webapp/js/sdk/sbt/connections/controls/astream/ActivityStreamWrapper.js
+++ sdk/com.ibm.sbt.web/src/main/webapp/js/sdk/sbt/connections/controls/astream/ActivityStreamWrapper.js
@@ -13,7 +13,11 @@
  * implied. See the License for the specific language governing 
  * permissions and limitations under the License.
  */
-define(["../../../declare", "../../../lang", "../../../url", "../../../config", "../../../util", "../../../connections/controls/WidgetWrapper", "../../../text!../../../connections/controls/astream/templates/ActivityStreamContent.html"], function(declare, lang, Url, config, util, WidgetWrapper, defaultTemplate) {
+define(["../../../declare", "../../../lang", "../../../url", 
+        "../../../config", "../../../util", "../../../connections/controls/WidgetWrapper", 
+        "../../../text!../../../connections/controls/astream/templates/ActivityStreamContent.html",
+        "../../i18n!sbt/connections/controls/nls/WidgetWrapper"], 
+        function(declare, lang, Url, config, util, WidgetWrapper, defaultTemplate, nls) {
 
     /**
      * The wrapper for the ActivityStream.
@@ -143,7 +147,7 @@ define(["../../../declare", "../../../lang", "../../../url", "../../../config", 
             }
             
             if(lang.isIE() < 10){ // Don't load the stream for IE < 10
-                this.defaultTemplate = "";
+                this.defaultTemplate = nls.IE9Unsupported;
             }
         }
         

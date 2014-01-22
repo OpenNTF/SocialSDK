892ae1146d4d7c57317f4d4be5277388986cad6b
David Ryan <daviryan@ie.ibm.com>
2014-01-22 09:33:58 +0000

    Forum View, Also some changes to the forum grid to support displaying topics from a specified forum, a sample to demonstrate this and some changes to the base action classes to support hiding actions.

---
+++ sdk/com.ibm.sbt.web/src/main/webapp/js/sdk/sbt/connections/controls/forums/nls/ForumView.js
@@ -0,0 +1,39 @@
+/*
+ * © Copyright IBM Corp. 2014
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+// NLS_CHARSET=UTF-8
+define({
+    root: {
+    	startTopic: "Start a Topic",
+    	ok: "Ok",
+    	loading: "Loading...",
+    	attachAFile: "Attach a File",
+    	attachments: "Attachments",
+    	forum: "Forum",
+    	cancel: "Cancel",
+    	save: "Save",
+    	addTags: "Add Tags",
+    	required: "Required",
+    	none: "none",
+    	tags: "Tags",
+    	markTopicAsQuestion: "Mark this topic as a question",
+    	editTitle: "Edit Title",
+    	title: "Title",
+    	titleError: "Enter a title before posting.",
+    	topicSuccess: "New topic created successfully.",
+    	topicError: "Error creating new topic."
+    }
+});

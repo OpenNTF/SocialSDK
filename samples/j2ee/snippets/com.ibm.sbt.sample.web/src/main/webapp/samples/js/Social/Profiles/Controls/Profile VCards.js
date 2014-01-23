993bac9397e0166d6073cebfe66d0ceee5bafb1b
Francis Moloney <franmolo@ie.ibm.com>
2014-01-23 17:01:33 +0000

    Update vcard samples

--- samples/j2ee/snippets/com.ibm.sbt.sample.web/src/main/webapp/samples/js/Social/Profiles/Controls/Profile VCards.js
+++ samples/j2ee/snippets/com.ibm.sbt.sample.web/src/main/webapp/samples/js/Social/Profiles/Controls/Profile VCards.js
@@ -1,7 +1,15 @@
 require(["sbt/dom", "sbt/connections/controls/vcard/ProfileVCard"], function(dom, ProfileVCard) {
-    var profileCard1 = new ProfileVCard({ userName : "%{name=sample.displayName1}", userId : "%{name=sample.id1|helpSnippetId=Social_Profiles_Get_Profile}" });
+    var profileCard1 = new ProfileVCard({
+        userName : "%{name=sample.displayName1}", 
+        userId : "%{name=sample.id1|helpSnippetId=Social_Profiles_Get_Profile}",
+        inclCss : true
+    });
     dom.byId("vcardDiv1").appendChild(profileCard1.domNode);
 
-    var profileCard2 = new ProfileVCard({ userName : "%{name=sample.displayName2}", userId : "%{name=sample.email2}" });
+    var profileCard2 = new ProfileVCard({ 
+        userName : "%{name=sample.displayName2}", 
+        userId : "%{name=sample.email2}",
+        inclCss : true
+    });
     dom.byId("vcardDiv2").appendChild(profileCard2.domNode);
 });

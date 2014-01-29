1f8dbc93da0f72566bad0336c6d63bfb62fd163e
benjamij <benjamij@ie.ibm.com>
2014-01-29 08:58:01 +0000

    UPDATE: Sample Controller update

---
+++ php/wordpress-plugin/src/system/libs/js-sdk/js/sdk/sbt/authenticator/OAuth.js
@@ -0,0 +1,85 @@
+/*
+ * © Copyright IBM Corp. 2012
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+/**
+ * Social Business Toolkit SDK.
+ * Definition of the authentication mechanism for OAuth 1.0.
+ */
+define(['../declare','../lang', '../util'], function(declare, lang, util) {
+
+	/**
+	 * OAuth 1.0 authentication.
+	 * 
+	 * This class triggers the authentication for a service.
+	 */
+	return declare(null, {
+		
+		url:			"",
+		loginUi:		"",	// mainWindow, dialog or popup
+	
+		constructor: function(args){
+			lang.mixin(this, args || {});	
+		},
+	
+		/**
+		 * Method that authenticates the current user 
+		 */
+		authenticate: function(options) {
+		    var self = this;
+			require(["sbt/config"], function(config) {
+			    var mode = options.loginUi || config.Properties["loginUi"] || this.loginUi;
+            	var width = config.Properties["login.oauth.width"] || 800;
+            	var height = config.Properties["login.oauth.height"] || 450;
+	            if(mode=="popup") {
+	                return self._authPopup(options, self.url, width, height);
+	            } else if(mode=="dialog") {
+	                return self._authDialog(options, self.url, width, height);
+	            } else {
+	                return self._authMainWindow(options, self.url);
+	            }
+			});
+		},
+		
+		_authMainWindow: function(options, sbtUrl) {
+			var url = sbtUrl + "?oaredirect="+encodeURIComponent(window.location.href);
+			newwindow=window.location.href = url;
+			return true;
+			
+		},
+		
+		_authPopup: function(options, sbtUrl, width, height) {
+		    require(["sbt/config"], function(config){
+		        config.callback = options.callback;
+	            var url = sbtUrl + "?loginUi=popup";
+	            
+	            var windowQueryMap = {
+                    height: height,
+                    width: width
+	            };
+	            var windowQuery = util.createQuery(windowQueryMap, ",");
+	            newwindow = window.open(url,'Authentication',windowQuery);
+	            return true;
+		    });
+		},
+		
+		_authDialog: function(options, sbtUrl, width, height) {
+			require(["sbt/_bridge/ui/OAuthDialog"], function(dialog) {
+				dialog.show(sbtUrl, width, height);
+			});
+			return true;
+		}
+	});
+});
---
+++ php/wordpress-plugin/src/system/libs/js-sdk/js/sdk/sbt/authenticator/templates/login.js
@@ -0,0 +1,92 @@
+/*
+ * © Copyright IBM Corp. 2012
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+function submitOnClick(contentForm) {
+    if (contentForm.username.value == "" || contentForm.password.value == "") {
+        document.getElementById("wrongCredsMessage").style.display = "block";
+        return;
+    }
+    var argsMap = getArgsMap();// get map of query string arguments
+    var actionURL = decodeURIComponent(argsMap.actionURL);
+    var loginUi = decodeURIComponent(argsMap.loginUi);
+    if (loginUi.length == 0) {
+        loginUi = "mainWindow";
+    }
+    if (loginUi == "popup") {
+        contentForm.action = actionURL + "?loginUi=popup&redirectURLToLogin="
+                + encodeURIComponent(document.URL)+"&endpointAlias="+opener.globalEndpointAlias;
+    } else if (loginUi == "mainWindow") {
+        var redirectURL = argsMap.redirectURL;
+        contentForm.action = actionURL
+                + "?loginUi=mainWindow&redirectURLToLogin="
+                + encodeURIComponent(document.URL) + "&redirectURL="
+                + encodeURIComponent(redirectURL);
+    }
+    contentForm.submit();
+}
+
+function cancelOnClick() {
+    var argsMap = getArgsMap();// get map of query string arguments
+    var redirectURL = decodeURIComponent(argsMap.redirectURL);
+    var loginUi = decodeURIComponent(argsMap.loginUi);
+    if (loginUi == "popup") {
+        if(window.cancel){
+            window.cancel();
+            delete window.cancel;
+        }
+        window.close();
+    } else {
+        window.location.href = redirectURL;
+    }
+}
+
+function onLoginPageLoad() {
+    var argsMap = getArgsMap();// get map of query string arguments
+    var showWrongCredsMessage = argsMap.showWrongCredsMessage;
+    if (showWrongCredsMessage == "true") {
+        document.getElementById("wrongCredsMessage").style.display = "block";
+    }
+    if(opener && opener.globalLoginFormStrings){
+        var loginForm = opener.globalLoginFormStrings;
+    	document.getElementById('wrongCredsMessage').appendChild(document.createTextNode(loginForm.wrong_creds_message));
+    	document.getElementById('basicLoginFormUsername').appendChild(document.createTextNode(loginForm.username));
+    	document.getElementById('basicLoginFormPassword').appendChild(document.createTextNode(loginForm.password));
+    	document.getElementById('basicLoginFormOK').value = loginForm.login_ok;
+    	document.getElementById('basicLoginFormCancel').value = loginForm.login_cancel;
+    }else{
+        document.getElementById('wrongCredsMessage').appendChild(document.createTextNode(decodeURIComponent(argsMap.wrong_creds_message)));
+        document.getElementById('basicLoginFormUsername').appendChild(document.createTextNode(decodeURIComponent(argsMap.username)));
+        document.getElementById('basicLoginFormPassword').appendChild(document.createTextNode(decodeURIComponent(argsMap.password)));
+		document.getElementById('basicLoginFormOK').value = decodeURIComponent(argsMap.login_ok);
+    	document.getElementById('basicLoginFormCancel').value = decodeURIComponent(argsMap.login_cancel);
+	}
+}
+
+function getArgsMap() {
+    try {
+        var qString = location.search.substring(1);// getting query string args
+        var qStringParams = qString.split("&");// getting array of all query
+                                                // string arg key value pairs
+        var argsMap = {};
+        var i;
+        for (i = 0; i < qStringParams.length; i++) {
+            var argArray = qStringParams[i].split("=");
+            argsMap[argArray[0]] = argArray[1];
+        }
+        return argsMap;
+    } catch (err) {
+        console.log("Error making agrs map in login.js " + err);
+    }
+}

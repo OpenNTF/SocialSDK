c5bf85240a5830d2f893ca4b4604d25a24173ef0
David Ryan <daviryan@ie.ibm.com>
2014-02-07 08:36:56 +0000

    Add new bootstrap samples and fixes for existing samples

--- samples/j2ee/snippets/com.ibm.sbt.sample.web/src/main/webapp/samples/js/Social/Forums/Controls/Bootstrap My Topics.js
+++ samples/j2ee/snippets/com.ibm.sbt.sample.web/src/main/webapp/samples/js/Social/Forums/Controls/Bootstrap My Topics.js
@@ -1,16 +1,13 @@
 require(["sbt/dom", 
          "sbt/connections/controls/forums/ForumGrid"], 
 function(dom, ForumGrid) {
-	var domNode = dom.byId("myForums");
-    var CustomForumRow = domNode.text || domNode.textContent;
 	
     var grid = new ForumGrid({
          type: "myTopics",
-         hideSorter: true,
- 		 hidePager: true
+         theme: "bootstrap"
     });
     
-    grid.renderer.template = CustomForumRow;
+    
              
     dom.byId("gridDiv").appendChild(grid.domNode);
              

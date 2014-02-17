ad9b2ee8f7ffac9f8f457deb4b5cef491b37d661
benjamij <benjamij@ie.ibm.com>
2014-02-17 13:15:57 +0000

    Initial PHP Core 1.0 commit

---
+++ php/php-core-1.0/system/libs/vendor/guzzle/guzzle/tests/Guzzle/Tests/Mock/MockMulti.php
@@ -0,0 +1,11 @@
+***REMOVED***
+
+namespace Guzzle\Tests\Mock;
+
+class MockMulti extends \Guzzle\Http\Curl\CurlMulti
+{
+    public function getHandle()
+    {
+        return $this->multiHandle;
+    }
+}

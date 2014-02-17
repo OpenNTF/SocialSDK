10093497bffe97009c57f16cb3e6d729c34a870e
benjamij <benjamij@ie.ibm.com>
2014-02-17 13:02:36 +0000

    Initial WP 1.0 commit

---
+++ php/wordpress-plugin-1.0/src/views/wp-settings/newEndpointButton.php
@@ -0,0 +1,1 @@
+<input type="button" onclick="create_new_endpoint();" name="btn_new_endpoint" id="btn_new_endpoint" class="button button-primary" value="***REMOVED*** echo $GLOBALS[LANG]['new_endpoint']; ?>" />&nbsp;

7d6cdf27c31d81ee5830cb751f288027ccac95ca
Mark Wallace <mark_wallace@ie.ibm.com>
2014-02-27 13:36:23 +0000

    Move constants for better reuse

--- sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/jslibrary/servlet/LibraryRequestParams.java
+++ sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/jslibrary/servlet/LibraryRequestParams.java
@@ -18,7 +18,11 @@ package com.ibm.sbt.jslibrary.servlet;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.ibm.commons.runtime.Application;
+import com.ibm.commons.util.PathUtil;
+import com.ibm.commons.util.StringUtil;
 import com.ibm.sbt.jslibrary.SBTEnvironment;
+import com.ibm.sbt.jslibrary.SBTEnvironmentFactory;
 
 /**
  * @author Mark Wallace
@@ -41,6 +45,112 @@ public class LibraryRequestParams {
     private String pathInfo;
     private Map<String, String> parameters = new HashMap<String, String>();
 
+    /**
+     * Servlet parameter which allows the location of the toolkit to be
+     * overridden, the default value is %local_server%/sbt. %local_server% is
+     * dynamically replaced by http://<server>:<port>
+     */
+    static public final String PARAM_TOOLKIT_URL = "toolkitUrl"; //$NON-NLS-1$
+
+    /**
+     * Servlet parameter which allows the location of the toolkit extensions to
+     * be overridden, the default value is %local_server%/sbtx. %local_server%
+     * is dynamically replaced by http://<server>:<port>
+     */
+    static public final String PARAM_TOOLKIT_EXT_URL = "toolkitExtUrl"; //$NON-NLS-1$
+
+    /**
+     * Servlet parameter which allows the location of the javascript libraries to
+     * be overridden, the default value is %local_server%/sbt/js/libs. %local_server%
+     * is dynamically replaced by http://<server>:<port>
+     */
+    static public final String PARAM_JS_LIBRARY_URL = "jsLibraryUrl"; //$NON-NLS-1$
+    
+    /**
+     * Servlet parameter which allows the path to the JavaScript library to be
+     * overridden, the default value is /js/sdk
+     */
+    static public final String PARAM_JAVASCRIPT_PATH = "javaScriptPath"; //$NON-NLS-1$
+
+    /**
+     * Servlet parameter which allows the location of the service servlet to be
+     * overridden, the default value is %local_application%/service
+     */
+    static public final String PARAM_SERVICE_URL = "serviceUrl"; //$NON-NLS-1$
+    
+    /**
+     * Servlet parameter which allows the location of the service servlet to be
+     * overridden, the default value is %local_application%/library
+     */
+    static public final String PARAM_LIBRARY_URL = "libraryUrl"; //$NON-NLS-1$
+
+    /**
+     * Servlet parameter which allows the location of the IFrame content
+     * template to be overridden, the default value is /xhr/IFrameContent.html
+     */
+    static public final String PARAM_IFRAME_PATH = "iframePath"; //$NON-NLS-1$
+
+    /**
+     * Servlet parameter which allows the list of default endpoints to be
+     * overridden, the default value is connections,smartcloud,domino,sametime
+     */
+    static public final String PARAM_ENDPOINTS = "endpoints"; //$NON-NLS-1$
+
+    /**
+     * Servlet parameter which allows the list of default client properties to
+     * be overridden, the default value is a null string
+     */
+    static public final String PARAM_CLIENT_PROPERTIES = "clientProperties"; //$NON-NLS-1$
+
+    /**
+     * Servlet parameter which allows the name of the default environment to be
+     * overridden, the default value is defaultEnvironment
+     */
+    static public final String PARAM_ENVIRONMENT = "environment"; //$NON-NLS-1$
+    
+    //
+    // Default values for library parameters
+    //
+    static public final String DEFAULT_TOOLKIT_URL = "%local_server%/sbt"; //$NON-NLS-1$
+    static public final String DEFAULT_TOOLKIT_EXT_URL = null;
+    static public final String DEFAULT_JS_LIBRARY_URL = "%local_server%/sbt/js/libs"; //$NON-NLS-1$
+    static public final String DEFAULT_JAVASCRIPT_PATH = "/js/sdk"; //$NON-NLS-1$
+    static public final String DEFAULT_SERVICE_URL = "%local_application%/service"; //$NON-NLS-1$
+    static public final String DEFAULT_LIBRARY_URL = "%local_application%/library";
+    static public final String DEFAULT_IFRAME_PATH = "/xhr/IFrameContent.html"; //$NON-NLS-1$
+    static public final String DEFAULT_ENDPOINTS = "connections,smartcloud,domino,sametime"; //$NON-NLS-1$
+    static public final String DEFAULT_CLIENT_PROPERTIES = null; //$NON-NLS-1$
+    static public final String DEFAULT_ENVIRONMENT = "defaultEnvironment"; //$NON-NLS-1$
+    
+    /**
+     * Initialize with the default values
+     */
+    public void initDefaults() {
+        // default parameters
+        Application application = Application.get();
+        String defaultToolkitUrl = getAppParameter(application, LibraryRequestParams.PARAM_TOOLKIT_URL, LibraryRequestParams.DEFAULT_TOOLKIT_URL);
+        String defaultToolkitExtUrl = getAppParameter(application, LibraryRequestParams.PARAM_TOOLKIT_EXT_URL, LibraryRequestParams.DEFAULT_TOOLKIT_EXT_URL);
+        String defaultJsLibraryUrl = getAppParameter(application, LibraryRequestParams.PARAM_JS_LIBRARY_URL, LibraryRequestParams.DEFAULT_JS_LIBRARY_URL);
+        String defaultJavaScriptPath = getAppParameter(application, LibraryRequestParams.PARAM_JAVASCRIPT_PATH, LibraryRequestParams.DEFAULT_JAVASCRIPT_PATH);
+        String defaultServiceUrl = getAppParameter(application, LibraryRequestParams.PARAM_SERVICE_URL, LibraryRequestParams.DEFAULT_SERVICE_URL);
+        String defaultLibraryUrl = getAppParameter(application, LibraryRequestParams.PARAM_LIBRARY_URL, LibraryRequestParams.DEFAULT_LIBRARY_URL);
+        String defaultIFramePath = getAppParameter(application, LibraryRequestParams.PARAM_IFRAME_PATH, LibraryRequestParams.DEFAULT_IFRAME_PATH);
+
+        // load initialisation parameters
+        setToolkitUrl(defaultToolkitUrl);
+        setToolkitJsUrl(PathUtil.concat(getToolkitUrl(), defaultJavaScriptPath, '/'));
+        setToolkitExtUrl(defaultToolkitExtUrl);
+        setToolkitExtJsUrl(PathUtil.concat(getToolkitExtUrl(), defaultJavaScriptPath, '/'));
+        setServiceUrl(defaultServiceUrl);
+        setLibraryUrl(defaultLibraryUrl);
+        setJsLibraryUrl(defaultJsLibraryUrl);
+        setIframeUrl(PathUtil.concat(getToolkitUrl(), defaultIFramePath, '/'));
+            	
+        // load initialization parameters
+    	SBTEnvironment environment = SBTEnvironmentFactory.get("defaultEnvironment");
+    	setEnvironment(environment);
+    }
+    
     /**
      * @return the defaultEnvironment
      */
@@ -246,6 +356,18 @@ public class LibraryRequestParams {
 	 */
 	public void setParameter(String name, String value) {
 		parameters.put(name, value);
+	}
+	
+	/**
+	 * 
+	 * @param application
+	 * @param name
+	 * @param defaultValue
+	 * @return
+	 */
+	protected String getAppParameter(Application application, String name, String defaultValue) {
+		String value = application.getProperty(name);
+		return StringUtil.isEmpty(value) ? defaultValue : value;
 	}
 	
 }

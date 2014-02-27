88b53bb1cce217320ee2eec1e8e7eaadfbc1491c
David Ryan <daviryan@ie.ibm.com>
2014-02-27 08:52:15 +0000

    Added snippet parameters to the tests and fixed a bug in the profile tags grid

--- samples/j2ee/snippets/com.ibm.sbt.sample.web/src/main/webapp/samples/js/Social/Profiles/Controls/Community Members.js
+++ samples/j2ee/snippets/com.ibm.sbt.sample.web/src/main/webapp/samples/js/Social/Profiles/Controls/Community Members.js
@@ -1,12 +1,35 @@
-require(["sbt/dom", "sbt/connections/controls/profiles/ProfileGrid"], function(dom, ProfileGrid) {
-    var grid = new ProfileGrid({
-        type : "communityMembers",
-        communityUuid : "%{name=CommunityService.communityUuid|helpSnippetId=Social_Communities_Get_My_Communities}"
-    });
-
-    dom.byId("gridDiv").appendChild(grid.domNode);
-
-    grid.update();
+require(["sbt/dom", "sbt/connections/controls/profiles/ProfileGrid","sbt/connections/CommunityService"], 
+		function(dom, ProfileGrid, CommunityService){
+		
+			var id = "";
+			var communityService = new CommunityService();
+			var promise = communityService.getPublicCommunities({}); 
+			var self = this;
+			promise.then(
+					function(communities) {
+						if(communities.length < 0){
+							dom.setText("content", "There are no public communities");
+						}else{
+							var id = communities[communities.length-1].getCommunityUuid();
+							var grid = new ProfileGrid({
+						        type : "communityMembers",
+						        communityUuid : id
+						    });
+						
+						    dom.byId("gridDiv").appendChild(grid.domNode);
+						
+						    grid.update();
+						}
+					}, 
+					function(error) {
+						dom.setText("content", error);
+					}
+			);
+			
+		
+				
+			
+			
 });
 
 

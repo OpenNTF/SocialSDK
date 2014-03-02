05b4c32d493e00c2bb68e5c736de420b0425be45
Mark Wallace <mark_wallace@ie.ibm.com>
2014-03-02 08:42:31 +0000

    Update Static JavaScript Update static copy of JavaScript used by sbt.web.resources. Eventually this code will be dynamically generated by the build.

--- osgi/com.ibm.sbt.web.resources/resources/js/sdk/sbt/smartcloud/controls/nls/BaseGridRenderer.js
+++ osgi/com.ibm.sbt.web.resources/resources/js/sdk/sbt/smartcloud/controls/nls/BaseGridRenderer.js
@@ -1,5 +1,5 @@
 /*
- * © Copyright IBM Corp. 2013
+ * ï¿½ Copyright IBM Corp. 2013
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
@@ -17,7 +17,7 @@
 dojo.provide("sbt.smartcloud.controls.nls.BaseGridRenderer");
 
 // NLS_CHARSET=UTF-8
-define({
+define('sbt/smartcloud/controls/nls/BaseGridRenderer',[],{
   root: {
       empty : "Empty",
       loading : "Loading...",

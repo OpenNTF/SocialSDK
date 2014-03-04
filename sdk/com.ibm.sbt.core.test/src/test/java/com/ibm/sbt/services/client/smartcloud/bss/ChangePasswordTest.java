32516d61528a5bf4b3876981d48fe6f5b057632e
Mark Wallace <mark_wallace@ie.ibm.com>
2014-03-04 13:18:22 +0000

    Util Make Absolute Url Add new utility method to make an absolute url using protocol/host/port from the current request

--- sdk/com.ibm.sbt.core.test/src/test/java/com/ibm/sbt/services/client/smartcloud/bss/ChangePasswordTest.java
+++ sdk/com.ibm.sbt.core.test/src/test/java/com/ibm/sbt/services/client/smartcloud/bss/ChangePasswordTest.java
@@ -36,7 +36,8 @@ public class ChangePasswordTest extends BaseBssTest {
     		System.out.println(loginName);
     		
     		UserCredentialJsonBuilder userCredential = new UserCredentialJsonBuilder();
-    		userCredential.setNewPassword("new_passw0rd")
+    		userCredential.setLoginName(loginName)
+    					  .setNewPassword("new_passw0rd")
     					  .setConfirmPassword("new_passw0rd");
     		
     		AuthenticationService authenticationService = getAuthenticationService();

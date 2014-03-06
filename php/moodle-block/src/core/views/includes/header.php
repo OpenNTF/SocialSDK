617fd6547a5d97d1b5a5d47fe21b52bd73f63fc3
benjamij <benjamij@ie.ibm.com>
2014-03-06 12:23:31 +0000

    Moodle work in progress

---
+++ php/moodle-block/src/core/views/includes/header.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Plugin Name: Social Business Toolkit integration plugin
+ * Plugin URI: http://example.com
+ * Description: Plugin providing access to the SBTK javascript api and controls.
+ * Version: 0.1
+ * Author: Lorenzo Boccaccia, Benjamin Jakobus
+ * Author URI: https://github.com/LorenzoBoccaccia
+ * License: Apache 2.0
+ */
+
+$agnostic_deploy_url = str_replace('http://', '//', $deploy_url);
+
+function plugins_url() {
+	return str_replace('http://', '//', BASE_LOCATION);
+}
+?>
+<script type="text/javascript">
+	var djConfig = {
+	baseUrl: "<?php echo $agnostic_deploy_url; ?>",
+	parseOnLoad: true,
+	locale: "en"
+};
+
+</script>
+
+<script src="//ajax.googleapis.com/ajax/libs/dojo/1.8.0/dojo/dojo.js.uncompressed.js"></script>
+<link rel="stylesheet" type="text/css" title="Style" href="//ajax.googleapis.com/ajax/libs/dojo/1.8.0/dojox/grid/enhanced/resources/claro/EnhancedGrid.css">
+<link rel="stylesheet" type="text/css" title="Style" href="//ajax.googleapis.com/ajax/libs/dojo/1.8.0/dojox/grid/enhanced/resources/EnhancedGrid_rtl.css">
+
+<script>
+	if (typeof _sbt == "undefined" || window._sbt_bridge_compat) {
+		_sbt = 0;
+		require({
+			paths: {
+				"sbt": "<?php echo $agnostic_deploy_url; ?>/js/sdk/sbt"
+			}
+		});
+		require({
+			paths: {
+				"sbt/_bridge": "<?php echo $agnostic_deploy_url; ?>/js/sdk/_bridges/dojo-amd"
+			}
+		});
+		require({
+			paths: {
+				"sbt/widget": "<?php echo $agnostic_deploy_url; ?>/js/sdk/dojo2"
+			}
+		});
+
+		define("sbt/config", ["sbt/Proxy", "sbt/_bridge/Transport", "sbt/authenticator/Basic", "sbt/Endpoint", "sbt/ErrorTransport", "sbt/authenticator/OAuth"], 
+			function (Proxy, Transport, Basic, Endpoint, ErrorTransport, OAuth) {
+				var sbt = {};
+				sbt.Properties = {
+					"libraryUrl": "",
+					"serviceUrl": "<?php echo plugins_url(); ?>/index.php?classpath=services&class=Proxy&method=fileOperations&_redirectUrl=",
+					"sbtUrl": "<?php echo $agnostic_deploy_url; ?>/js/sdk"
+				};
+				sbt.Endpoints = {
+						<?php echo generateEndpoint($authentication_method, $url, $agnostic_deploy_url, $name); ?>
+				};
+				sbt.findEndpoint = function (endpointName) {
+					return this.Endpoints[endpointName];
+				};
+			return sbt;
+		});
+	};
+</script>
+</head>
+<?php 
+
+
+/**
+ * Decides on an endpoint to use and generates the JavaScript for it.
+ *
+ * @return string			The JavaScript representing an endpoint.
+ * @author Benjamin Jakobus
+*/
+function generateEndpoint($authentication_method, $url, $deploy_url, $name) {
+	//TODO support list of endpoint and aliases
+	return generateConnectionsEndpoint($authentication_method, $url, $deploy_url);
+}
+
+/**
+ * Generates the JavaScript for an IBM Connections endpoint.
+ *
+ * @return string		The JavaScript representing an IBM Connections endpoint.
+ * @author Benjamin Jakobus
+ */
+
+function generateConnectionsEndpoint($authentication_method, $url, $deploy_url) {
+	global $USER;
+	$endpoint_js = '"connections": new Endpoint({';
+	$endpoint_js .= '"authType": "' . $authentication_method . '",';
+	$endpoint_js .= '"platform": "connections",';
+
+	if ($authentication_method == 'oauth1') {
+		$endpoint_js .= '"authenticator": new OAuth({"loginUi": null,
+				"url": "' . $deploy_url . '"}),';
+	} else if ($authentication_method == 'basic') {
+		$endpoint_js .= '"authenticator": new Basic({';
+		$endpoint_js .= '"url": "' . $deploy_url . '", "actionUrl": "' . plugins_url() . '/index.php?classpath=services&class=Proxy&method=route&basicAuthRequest=true&_redirectUrl=' . getCurrentPage() . '"}),';
+	}
+
+	$endpoint_js .= '"proxyPath": "connections",';
+	$endpoint_js .= '"isAuthenticated": "false",';
+	$endpoint_js .= '"transport": new Transport({}),';
+	$endpoint_js .= '"serviceMappings": {},';
+	$endpoint_js .= '"name": "connections",';
+	$endpoint_js .= '"authenticationErrorCode": "401",';
+	$endpoint_js .= '"baseUrl": "' . $url . '",';
+	$endpoint_js .= '"apiVersion": "4.0",';
+	$endpoint_js .=	'"proxy": new Proxy({';
+	$endpoint_js .=	'"proxyUrl": "' . plugins_url() . '/index.php?classpath=services&class=Proxy&method=route&uid=' . $USER->id . '&_redirectUrl="})}),';
+
+	return $endpoint_js;
+}
+
+/**
+ * Returns the URL of the page that the user is currently on.
+ *
+ * @return string	The URL of the page that the user is currently on.
+ * @author Benjamin Jakobus
+ */
+function getCurrentPage() {
+	$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+	$currentPage = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+	return $currentPage;
+}
+
---
+++ php/moodle-block/src/edit_form.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * © Copyright IBM Corp. 2013
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * SBTK edit form
+ *
+ * @author Benjamin Jakobus
+ */
+if (!defined('BASE_LOCATION')) {
+	$autoload = __DIR__ . '/core/autoload.php';
+	include $autoload;
+}
+class block_ibmsbt_edit_form extends block_edit_form {
+ 
+    protected function specific_definition($mform) {
+ 
+        // Section header title according to language file.
+        $mform->addElement('header', 'configheader', get_string('blocksettings', 'block')); 
+
+        // List of available samples. Note: keys must reflect relative path from
+        // core/samples and must omit the .php file extension
+        $plugins = array();
+        $path = str_replace('core', '', BASE_PATH) . '/user_widgets/';
+        if ($handle = opendir($path)) {
+        	while (false !== ($file = readdir($handle))) {
+        		if ($file != "." && $file != "..") {
+        			
+        			$widgetName = str_replace('.php', '', $file);
+        			$widgetName = str_replace('-', ' ', $widgetName);
+        			
+        			$plugins[$path . $file] = $widgetName;
+        		}
+        	}
+        	closedir($handle);
+        }
+        
+        $mform->addElement('html', '<p>For documentation, tutorials and guides on how to use the Social Business SDK, go to 
+        		<a href="https://www.ibmdw.net/social/">https://www.ibmdw.net/social/</a> or visit <a href="https://greenhousestage.lotus.com/sbt/sbtplayground.nsf">our playground</a>
+        		 directly if you need JavaScript snippets.</p>');
+        
+        // Type dropdown
+        $mform->addElement('select', 'config_plugin', 'Plugin:', $plugins);
+        $mform->setDefault('config_plugin', 'social/files/files-grid');
+        
+        // Block title
+        $mform->addElement('text', 'config_title', 'Title:');
+        $mform->setDefault('config_title', 'default value');
+        $mform->setType('config_title', PARAM_MULTILANG);
+ 
+    }
+}

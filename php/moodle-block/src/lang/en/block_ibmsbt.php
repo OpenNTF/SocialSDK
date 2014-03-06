64cd2493633cd11b718063c6f20ceb5de7c6e7b3
benjamij <benjamij@ie.ibm.com>
2014-03-06 12:23:31 +0000

    Moodle work in progress

---
+++ php/moodle-block/src/lang/en/block_ibmsbt.php
@@ -0,0 +1,27 @@
+***REMOVED***
+/*
+ * © Copyright IBM Corp. 2013
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at:
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+* implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+/**
+ * @author Benjamin Jakobus
+ */
+$string['pluginname'] = 'IBM Connections Toolkit';
+$string['ibmsbt'] = 'IBM Connections Toolki';
+$string['ibmsbt:addinstance'] = 'Add a new simple HTML block';
+$string['ibmsbt:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
+$string['config_title'] = 'Title:';
+$string['type'] = 'Type:';
+$string['new_window'] = 'Open in new window';

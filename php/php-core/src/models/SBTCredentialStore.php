ccd1d6043beaeb7abb922cf5428528a2b5496d78
benjamij <benjamij@ie.ibm.com>
2014-03-11 16:19:50 +0000

    Core model stubs

--- php/php-core/src/models/SBTCredentialStore.php
+++ php/php-core/src/models/SBTCredentialStore.php
@@ -15,207 +15,326 @@
 * permissions and limitations under the License.
 */
 
+if (!defined('SESSION_NAME')) {
+	define('SESSION_NAME', 'ibm_sbt_session');
+}
+
+if (!defined('BASIC_AUTH_USERNAME')) {
+	define('BASIC_AUTH_USERNAME', 'basicauthusername');
+}
+
+if (!defined('BASIC_AUTH_PASSWORD')) {
+	define('BASIC_AUTH_PASSWORD', 'basicauthpassword');
+}
+
+if (!defined('TOKEN')) {
+	define('TOKEN', 'token');
+}
+
+if (!defined('REQUEST_TOKEN')) {
+	define('REQUEST_TOKEN', 'requesttoken');
+}
+
+if (!defined('TOKEN_TYPE')) {
+	define('TOKEN_TYPE', 'tokentype');
+}
+
+if (!defined('OAUTH_TOKEN')) {
+	define('OAUTH_TOKEN', 'oauthtoken');
+}
+
+if (!defined('OAUTH_TOKEN_SECRET')) {
+	define('OAUTH_TOKEN_SECRET', 'oauthtokensecret');
+}
+
+if (!defined('OAUTH_VERIFIER_TOKEN')) {
+	define('OAUTH_VERIFIER_TOKEN', 'oauthverifiertoken');
+}
+
+if (!defined('OAUTH_REQUEST_TOKEN')) {
+	define('OAUTH_REQUEST_TOKEN', 'oauthrequesttoken');
+}
+
+if (!defined('OAUTH_REQUEST_TOKEN_SECRET')) {
+	define('OAUTH_REQUEST_TOKEN_SECRET', 'oauthrequesttokensecret');
+}
+
 /**
- * Credential Store for authorization tokens
+ * Credential Store for authorization tokens.
  *
- * @author Lorenzo Boccaccia
  * @author Benjamin Jakobus
  */
 class SBTCredentialStore {
 	
-	// TODO: Encrypt session & cookies
-	
-	// Storage type
-	private $storeType;
-	
 	// Session name
 	private $sessionName;
 	
+	// Encryption key
+	private $key;
+	
+	// Initialization vector
+	private $iv;
+	
+	private static $uid;
+	
+	private static $instance = null;
+	
+	public static function getInstance() {
+		if (self::$instance == null) {
+			self::$instance = new SBTCredentialStore();
+		}
+		return self::$instance;
+	}
+	
+	// TODO: Implement this class for standalone samples
+	
 	/**
-	 * Load variables from config.php and make them private members of
-	 * this class.
-	 *
-	 * @return
+	 * Constructor.
 	 */
-	function __construct() {
-		$config = BASE_PATH . '/config.php';
-		if (file_exists($config)) {
-			require $config;
-		} else {
-			$config = BASE_PATH . '/core/config.php';
-			if (file_exists($config)) {
-				require $config;
-			} else {
-				exit('Could not load configuration file: config.php');
-			}
-		}
-		
-		$this->storeType = $config['credentialStoreType'];
-		$this->sessionName = $config['session_name'];
-		
-		session_name($this->sessionName);
-		
-		if (!session_id()) {
-			session_start();
-		}
+	private function __construct() {
 	}
 	
 	/**
 	 * Stores a value in the credential store.
 	 * 
-	 * @param string $key
-	 * @param unknown $value
+	 * @param string $skey
+	 * @param string $value
 	 */
-	private function _store($key, $value) {
-		$store = null;
-	
-		if ($this->storeType == 'session') {
-			
-			if (isset($_SESSION['__IBM_SBTK_CRED_STORE'])) {
-				$store = unserialize($_SESSION['__IBM_SBTK_CRED_STORE']);
-				
-			} else {
-				$store = array();
-			}
-			
-			$store[$key] = $value;
-			
-			$_SESSION['__IBM_SBTK_CRED_STORE'] = serialize($store);
-		} else if ($this->storeType == 'cookie') {
-			if (isset($_COOKIE['__IBM_SBTK_CRED_STORE'])) {
-				$store = unserialize($_COOKIE['__IBM_SBTK_CRED_STORE']);
-			} else {
-				$store = array();
-			}
-			$store[$key] = $value;
-
-			// Expiry time is next week
-			$nextWeek = time() + (7 * 24 * 60 * 60);
-			$ret = setcookie( "__IBM_SBTK_CRED_STORE", serialize($store), $nextWeek);
-		}
+	private function _store($skey, $endpoint, $value) {
+		// TODO
 	}
 	
 	/**
 	 * Returns a value from the credentials store
 	 * 
-	 * @param string $key
+	 * @param string $skey
+	 * @param string $endpoint
+	 * 
 	 * @return 
 	 */
-	private function _get($key) {
-		if ($this->storeType == 'session') {
-			
-			if (isset($_SESSION['__IBM_SBTK_CRED_STORE'])) {
-				$store = unserialize($_SESSION['__IBM_SBTK_CRED_STORE']);
-				return (isset($store[$key]) ? $store[$key] : null);
-			} 
-		} else if ($this->storeType == 'cookie') {
-			if (isset($_COOKIE['__IBM_SBTK_CRED_STORE'])) {
-				$store = unserialize($_COOKIE['__IBM_SBTK_CRED_STORE']);
-				$this->_get($key);
-				return (isset($store[$key]) ? $store[$key] : null);
-			} 
-		} 
-		return null;
+	private function _get($skey, $endpoint) {
+		// TODO
 	}
-	
 	/**
 	 * Deletes an entry from the credentials store.
 	 *
 	 * @param string $key
 	 */
-	private function _delete($key) {
-		$store = null;
-		if ($this->storeType == 'session') {
-				
-			if (isset($_SESSION['__IBM_SBTK_CRED_STORE'])) {
-				$store = unserialize($_SESSION['__IBM_SBTK_CRED_STORE']);
-		
-			} else {
-				$store = array();
-			}
-				
-			if (isset($store[$key])) {
-				unset($store[$key]);
-			}
-				
-			$_SESSION['__IBM_SBTK_CRED_STORE'] = serialize($store);
-				
-		} else if ($this->storeType == 'cookie') {
-			if (isset($_COOKIE['__IBM_SBTK_CRED_STORE'])) {
-				$store = unserialize($_COOKIE['__IBM_SBTK_CRED_STORE']);
-			} else {
-				$store = array();
-			}
-			if (isset($store[$key])) {
-				unset($store[$key]);
-			}
-				
-			// Expiry time is next week
-			$nextWeek = time() + (7 * 24 * 60 * 60);
-			setcookie( "__IBM_SBTK_CRED_STORE", serialize($store), $nextWeek);
-		} 
+	private function _delete($skey, $endpoint) {
+		// TODO
 	}
 	
-	public function storeToken($token) {
-		$this->_store('__SBTK_TOKEN', $token);
+	/**
+	 * Destroys the credential store (i.e. removes the session from the database).
+	 *
+	 * @param string $key
+	 */
+	public function destroyStore($endpoint = "connections") {
+		// TODO
 	}
 	
-	public function storeTokenType($tokenType) {
-		$this->_store('__SBTK_TOKEN_TYPE', $tokenType);
+	public function storeToken($token, $endpoint = "connections") {
+		$this->_store(TOKEN, $endpoint, $token);
 	}
 	
-	public function storeRequestToken($token) {
-		$this->_store('__REQUEST_SBTK_TOKEN', $token);
+	/**
+	 * Stores the OAuth request token in the database.
+	 *
+	 * @param string $token			The OAuth request token.
+	 */
+	public function storeRequestToken($token, $endpoint = "connections") {
+		$this->_store(REQUEST_TOKEN, $endpoint, $token);
 	}
 	
-	public function getRequestToken() {
-		$token = $this->_get('__REQUEST_SBTK_TOKEN');
+	/**
+	 * Returns a request token.
+	 *
+	 * @return string 		A request token.
+	 */
+	public function getRequestToken($endpoint = "connections") {
+		$token = $this->_get(REQUEST_TOKEN, $endpoint);
 		return $token;
 	}
 
-	public function getToken() {
-		return $this->_get('__SBTK_TOKEN');
+	/**
+	 * Returns a token.
+	 *
+	 * @param string $endpoint The endpoint associated with this token.
+	 * @return string 		A token.
+	 */
+	public function getToken($endpoint = "connections") {
+		return $this->_get(TOKEN, $endpoint);
 	}
 	
-	public function getTokenType() {
-		return $this->_get('__SBTK_TOKEN_TYPE');
+	/**
+	 * Deletes the OAuth tokens.
+	 * 
+	 * @param string $endpoint The endpoint associated with the tokens.
+	 */
+	public function deleteTokens($endpoint = "connections") {
+		$this->_delete(TOKEN, $endpoint);
+		$this->_delete(TOKEN_TYPE, $endpoint);
+		$this->_delete(OAUTH_TOKEN, $endpoint);
+		$this->_delete(OAUTH_TOKEN_SECRET, $endpoint);
+		$this->_delete(REQUEST_TOKEN, $endpoint);
+		$this->_delete(OAUTH_VERIFIER_TOKEN, $endpoint);
+		$this->_delete(OAUTH_REQUEST_TOKEN_SECRET, $endpoint);
 	}
 	
-	public function deleteTokens() {
-		$this->_delete('__SBTK_TOKEN');
-		$this->_delete('__OAUTH_SBTK_TOKEN');
-		$this->_delete('__REQUEST_SBTK_TOKEN');
-	}
-	
-	public function storeOAuthAccessToken($token) {
-		$this->_store('__OAUTH_SBTK_TOKEN', $token);
-	}
-	
-	
-	public function getOAuthAccessToken() {
-		return $this->_get('__OAUTH_SBTK_TOKEN');
-	}
-	
-	public function storeBasicAuthUsername($token) {
-		$this->_store('__BASIC_AUTH_SBTK_USERNAME', $token);
-	}
-	
-	
-	public function getBasicAuthUsername() {
-		return $this->_get('__BASIC_AUTH_SBTK_USERNAME');
-	}
-	
-	public function storeBasicAuthPassword($token) {
-		$this->_store('__BASIC_AUTH_SBTK_PASSWORD', $token);
+	/**
+	 * Stores the OAuth access token in the database.
+	 * 
+	 * @param string $token			The OAuth access token.
+	 * @param string $endpoint The endpoint associated with this token.
+	 */
+	public function storeOAuthAccessToken($token, $endpoint = "connections") {
+		$this->_store(OAUTH_TOKEN, $endpoint, $token);
 	}
 	
 	
-	public function getBasicAuthPassword() {
-		return $this->_get('__BASIC_AUTH_SBTK_PASSWORD');
+	public function storeTokenSecret($token, $endpoint = "connections") {
+		$this->_store(OAUTH_TOKEN_SECRET, $endpoint, $token);
 	}
 	
-	public function deleteBasicAuthCredentials() {
-		$this->_delete('__BASIC_AUTH_SBTK_PASSWORD');
-		$this->_delete('__BASIC_AUTH_SBTK_USERNAME');
+	public function storeVerifierToken($token, $endpoint = "connections") {
+		$this->_store(OAUTH_VERIFIER_TOKEN, $endpoint, $token);
 	}
+	
+	public function getTokenSecret($endpoint = "connections") {
+		return $this->_get(OAUTH_TOKEN_SECRET, $endpoint);
+	}
+	
+	/**
+	 * Returns OAuth access token.
+	 *
+	 * @param string $endpoint The endpoint associated with this token.
+	 * @return string 		OAuth access token.
+	 */
+	public function getOAuthAccessToken($endpoint = "connections") {
+		return $this->_get(OAUTH_TOKEN, $endpoint);
+	}
+	
+	/**
+	 * Returns the verifier token.
+	 *
+	 * @return string 		OAuth verifier token.
+	 * @param string $endpoint The endpoint associated with this token.
+	 */
+	public function getVerifierToken($endpoint = "connections") {
+		return $this->_get(OAUTH_VERIFIER_TOKEN, $endpoint);
+	}
+	
+	/**
+	 * Stores the basic authentication username in the database.
+	 *
+	 * @param string $username		The username used to perform basic authentication.
+	 * @param string $endpoint 
+	 */
+	public function storeBasicAuthUsername($username, $endpoint = "connections") {
+		$this->_store(BASIC_AUTH_USERNAME, $endpoint, $username);
+	}
+	
+	/**
+	 * Returns basic auth username.
+	 * 
+	 * @param string $endpoint 
+	 * @return string 		Basic auth username.
+	 */
+	public function getBasicAuthUsername($endpoint = "connections") {
+		return $this->_get(BASIC_AUTH_USERNAME, $endpoint);
+	}
+	
+	/**
+	 * Stores the OAuth 1.0 request token secret
+	 *
+	 * @param string $token		The OAuth 1.0 request token secret
+	 */
+	public function storeRequestTokenSecret($token, $endpoint = "connections") {
+		$this->_store(OAUTH_REQUEST_TOKEN_SECRET, $endpoint, $token);
+	}
+	
+	/**
+	 * Returns basic auth username.
+	 * 
+	 * @param string $endpoint The endpoint associated with this token.
+	 * @return string 		
+	 */
+	public function getRequestTokenSecret($endpoint = "connections") {
+		return $this->_get(OAUTH_REQUEST_TOKEN_SECRET, $endpoint);
+	}
+	
+	/**
+	 * Stores the basic authentication password in the database.
+	 * 
+	 * @param string $password		The password used to perform basic authentication.
+	 * @param string $endpoint 
+	 */
+	public function storeBasicAuthPassword($password, $endpoint = "connections") {
+		$this->_store(BASIC_AUTH_PASSWORD, $endpoint, $password);
+	}
+	
+	/**
+	 * Returns basic auth password.
+	 *
+	 * @param string $endpoint The endpoint associated with this token.
+	 * @return string 		Basic auth password.
+	 */
+	public function getBasicAuthPassword($endpoint = "connections") {
+		return $this->_get(BASIC_AUTH_PASSWORD, $endpoint);
+	}
+	
+	/**
+	 * Deletes stored basic authentication credentials.
+	 * 
+	 * @param string $endpoint The endpoint associated with the credentials to delete.
+	 */
+	public function deleteBasicAuthCredentials($endpoint = "connections") {
+		$this->_delete(BASIC_AUTH_PASSWORD, $endpoint);
+		$this->_delete(BASIC_AUTH_USERNAME, $endpoint);
+	}
+	
+	/**
+	 * Deletes stored OAuth credentials.
+	 * @param string $endpoint The endpoint associated with the credentials to delete.
+	 */
+	public function deleteOAuthCredentials($endpoint = "connections") {
+		$this->_delete(TOKEN_TYPE, $endpoint);
+		$this->_delete(OAUTH_TOKEN, $endpoint);
+		$this->_delete(OAUTH_TOKEN_SECRET, $endpoint);
+		$this->_delete(OAUTH_REQUEST_TOKEN, $endpoint);
+		$this->_delete(TOKEN, $endpoint);
+		$this->_delete(OAUTH_VERIFIER_TOKEN, $endpoint);
+		$this->_delete(OAUTH_REQUEST_TOKEN_SECRET, $endpoint);
+		$this->_delete(REQUEST_TOKEN, $endpoint);
+	}
+	
+	public function storeTokenType($tokenType, $endpoint = "connections") {
+		$this->_store(TOKEN_TYPE, $endpoint, $tokenType);
+	}
+	
+	public function getTokenType($endpoint = "connections") {
+		return $this->_get(TOKEN_TYPE, $endpoint);
+	}
+	
+	
+	/**
+	 * Generates a random string of given length.
+	 *
+	 * @param int $length		Desired length of the string.
+	 *
+	 * @return string			Random string.
+	 */
+	private function gen_string($length) {
+		return null;
+	}
+	
+	private function _encrypt($key, $data, $iv){
+		return null;
+	}
+	
+	private function _decrypt($key, $encryptedData, $iv) {
+		return null;
+	}
+	
+
 }

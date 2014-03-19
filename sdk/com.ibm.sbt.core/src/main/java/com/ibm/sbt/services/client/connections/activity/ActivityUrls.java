8c496b015edb9d973438b63924f28a9cefcbc2c3
Carlos Manias <carlmani@ie.ibm.com>
2014-03-19 12:11:17 +0000

    New URLBuilder pattern on ActivityService
    New URLBuilder pattern on ActivityService

---
+++ sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/services/client/connections/activity/ActivityUrls.java
@@ -0,0 +1,44 @@
+/*
+ * � Copyright IBM Corp. 2014
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+package com.ibm.sbt.services.client.connections.activity;
+
+import com.ibm.sbt.services.client.base.ConnectionsConstants;
+import com.ibm.sbt.services.client.base.URLBuilder;
+import com.ibm.sbt.services.client.base.URLContainer;
+import com.ibm.sbt.services.client.base.Version;
+import com.ibm.sbt.services.client.base.VersionedUrl;
+
+/**
+ * @author Carlos Manias
+ */
+public enum ActivityUrls implements URLContainer {
+	ACTIVITY_URL(new VersionedUrl(ConnectionsConstants.v4_0, "/activities/service/atom2/{activityAction}"));
+
+	private URLBuilder builder;
+	
+	private ActivityUrls(VersionedUrl... urlVersions) {
+		builder = new URLBuilder(urlVersions);
+	}
+	
+	public String format(Version version, String... args) {
+		return builder.format(version, args);
+	}
+
+	public String getPattern(Version version){
+		return builder.getPattern(version).getValue();
+	}
+
+}

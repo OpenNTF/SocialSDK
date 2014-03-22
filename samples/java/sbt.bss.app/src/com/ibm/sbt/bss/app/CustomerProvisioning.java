46ad6741dc569c19e40f343b9a624b94f14d75de
Mark Wallace <mark_wallace@ie.ibm.com>
2014-03-22 14:04:36 +0000

    BSS Sample and Service Mapping Test New BSS sample to provision a customer for SmartCloud Connections Test support for Map properties in managed-beans.xml

--- samples/java/sbt.bss.app/src/com/ibm/sbt/bss/app/CustomerProvisioning.java
+++ samples/java/sbt.bss.app/src/com/ibm/sbt/bss/app/CustomerProvisioning.java
@@ -15,14 +15,23 @@
  */
 package com.ibm.sbt.bss.app;
 
-import com.ibm.sbt.services.client.smartcloud.bss.CustomerManagementService;
-import com.ibm.sbt.services.endpoints.BasicEndpoint;
+import com.ibm.sbt.services.client.base.JsonEntity;
+import com.ibm.sbt.services.client.smartcloud.bss.SubscriberManagementService;
 
 /**
  * @author mwallace
  *
  */
-public class CustomerProvisioning {
+public class CustomerProvisioning extends BaseBssApp {
+
+	/**
+	 * @param url
+	 * @param user
+	 * @param password
+	 */
+	public CustomerProvisioning(String url, String user, String password) {
+		super(url, user, password);
+	}
 
 	/**
 	 * Demonstrate a Customer Provisioning workflow.
@@ -31,7 +40,7 @@ public class CustomerProvisioning {
 	 */
 	public static void main(String[] args) {
 		if (args.length < 3) {
-			System.out.println("Usage: java com.ibm.sbt.bss.app.CustomerProvisioning <url> <user> <password> <verb> <output>");
+			System.out.println("Usage: java com.ibm.sbt.bss.app.CustomerProvisioning <url> <user> <password>");
 			return;
 		}
 		
@@ -39,17 +48,65 @@ public class CustomerProvisioning {
 		String user = args[1];
 		String password = args[2];
 		
+		CustomerProvisioning cp = null;
 		try {
-			BasicEndpoint basicEndpoint = new BasicEndpoint();
-			basicEndpoint.setUrl(url);
-			basicEndpoint.setForceTrustSSLCertificate(true);
-			basicEndpoint.setUser(user);
-			basicEndpoint.setPassword(password);
+			cp = new CustomerProvisioning(url, user, password);
 
-			CustomerManagementService customerManagement = new CustomerManagementService(basicEndpoint);
-
-			// Step 1. 
+			String email = cp.getUniqueEmail("mailinator.com");
+			System.out.println("Email: " + email);
+			String customerId = cp.registerCustomer("Some Company", email);
+			System.out.println("Customer Id: " + customerId);
 			
+			/*
+			EntityList<JsonEntity> subscriptions = cp.getSubscriptionsById(customerId);
+			System.out.println("Subscriptions: "+subscriptions.size());
+			String subscriptionId = null;
+			for (JsonEntity subscription : subscriptions) {
+				long oid = subscription.getAsLong("Oid");
+				String partNumber = subscription.getAsString("PartNumber");
+				System.out.println(partNumber);
+				// part number for IBM SmartCloud Connections
+				if ("D0NWLLL".equalsIgnoreCase(partNumber)) {
+					subscriptionId = "" + oid;
+					break;
+				}
+			}
+			if (StringUtil.isEmpty(subscriptionId)) {
+	    		subscriptionId = cp.createSubscription(customerId, 3, "D0NWLLL", 5);
+			}
+			*/
+			String subscriptionId = cp.createSubscription(customerId, 3, "D0NWLLL", 5);
+			System.out.println("Subscription Id: " + subscriptionId);
+			
+			SubscriberManagementService subscriberManagement = cp.getSubscriberManagementService();
+			//EntityList<JsonEntity> subscribers = subscriberManagement.getSubscribersByEmail(email);
+			//JsonEntity subscriber = subscribers.get(0);
+			//String subscriberId = String.valueOf(subscriber.getAsLong("Oid"));
+			String subscriberId = cp.addSubscriber(customerId, cp.getUniqueEmail("mailinator.com"), SubscriberManagementService.Role.CustomerAdministrator);
+			System.out.println("Subscriber Id: " + subscriberId);
+			JsonEntity subscriber = cp.getSubscriberById(subscriberId);
+			System.out.println("Subscriber: " + subscriber.toJsonString(false));
+	   		String loginName = subscriber.getAsString("Subscriber/Person/EmailAddress");
+	   		System.out.println("Login Name: " + loginName);
+			String subscriberState = subscriber.getAsString("Subscriber/SubscriberState");
+			System.out.println("Subscriber State: " + subscriberState);
+			
+			if (!"ACTIVE".equals(subscriberState)) {
+				subscriberManagement.activateSubscriber(subscriberId);
+				System.out.println("Activated subscriber: " + subscriberId);
+			}
+			
+			cp.setOneTimePassword(loginName, "one-time-123");
+			cp.changePassword(loginName, "one-time-123", "password1");
+			System.out.println("Changed password: " + loginName);
+			
+			cp.entitleSubscriber(subscriberId, subscriptionId, true);
+			System.out.println("Entitled subscriber: " + subscriptionId);
+			
+			subscriber = subscriberManagement.getSubscriberById(subscriberId);
+			System.out.println(subscriber.toJsonString(false));	
+			
+			System.out.println("Customer provisioning successful");
 			
 		} catch (Exception e) {
 			e.printStackTrace();

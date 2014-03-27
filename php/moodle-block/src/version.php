e5d50fa635ef90a5bc79192eb78d05a951a223da
benjamij <benjamij@ie.ibm.com>
2014-03-27 15:02:21 +0000

    Updated version info

--- php/moodle-block/src/version.php
+++ php/moodle-block/src/version.php
@@ -17,5 +17,5 @@
 /**
  * @author Benjamin Jakobus
  */
-$plugin->version = 2014010210;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2014032715;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)

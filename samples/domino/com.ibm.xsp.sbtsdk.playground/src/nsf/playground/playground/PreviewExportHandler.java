bef68f669b067a2a7a5f9c374171ffc8ac986874
priand <priand@us.ibm.com>
2014-03-31 16:23:02 -0400

    Started some global import/export in the playground

---
+++ samples/domino/com.ibm.xsp.sbtsdk.playground/src/nsf/playground/playground/PreviewExportHandler.java
@@ -0,0 +1,58 @@
+package nsf.playground.playground;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import lotus.domino.Item;
+import lotus.domino.NotesException;
+import lotus.domino.View;
+
+import com.ibm.xsp.model.domino.DominoUtils;
+
+import nsf.playground.impexp.JsonExport;
+import nsf.playground.impexp.JsonExport.ExportTarget;
+import nsf.playground.impexp.JsonImportExport;
+
+
+public class PreviewExportHandler extends PreviewHandler {
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String key = req.getParameter("type");
+		
+		try {
+			resp.setStatus(HttpServletResponse.SC_OK);
+			resp.setContentType("application/zip");
+			OutputStream os = resp.getOutputStream();
+			try {
+				ExportTarget tgt = new JsonExport.ZipExportTarget(os,false);
+				JsonExport exp = new JsonExport(tgt);
+				exp.setItemFilter(new JsonImportExport.ItemFilter() {
+					@Override
+					public boolean accept(Item item) throws NotesException {
+						String name = item.getName();
+						if(name.equalsIgnoreCase("$UpdatedBy")) {
+							return false;
+						}
+						return true;
+					}
+				});
+				View view = DominoUtils.getCurrentDatabase().getView("AllSnippetsById");
+				try {
+					exp.exportDocuments(view,key);
+				} finally {
+					view.recycle();
+				}
+			} finally {
+				os.flush();
+			}
+		} catch(NotesException ex) {
+			throw new IOException(ex);
+		}
+	}
+
+}

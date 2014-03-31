555f73b0a439a1c0ec01946cc53aa57f08c6b027
priand <priand@us.ibm.com>
2014-03-31 16:32:14 -0400

    Moved the sbt specific classes from core playground to the sbt fragment

--- samples/domino/com.ibm.xsp.sbtsdk.playground/src/nsf/playground/environments/PlaygroundOAuthStore.java
+++ samples/domino/com.ibm.xsp.sbtsdk.playground/src/nsf/playground/environments/PlaygroundOAuthStore.java
@@ -9,7 +9,6 @@ import com.ibm.sbt.opensocial.domino.oauth.DominoOAuthClient;
 import com.ibm.sbt.opensocial.domino.oauth.DominoOAuthStore;
 import com.ibm.sbt.opensocial.domino.oauth.clients.DropBoxOAuthClient;
 import com.ibm.sbt.opensocial.domino.oauth.clients.SmartCloudOAuthClient;
-import com.ibm.xsp.sbtsdk.playground.sbt.extension.SbtEndpoints;
 
 /**
  * An OpenSocial OAuth 1.0a store.
@@ -28,21 +27,21 @@ public class PlaygroundOAuthStore implements DominoOAuthStore {
 	}
 
 	private void populateOAuthClients(Map<String, String> fieldMap) {
-		clients.put(StringUtils.defaultIfBlank(fieldMap.get(SbtEndpoints.SMA_OA_GADGET_SERVICE), SbtEndpoints.DEFAULT_SC_SERVICE_NAME), 
+		clients.put(StringUtils.defaultIfBlank(fieldMap.get(SbtConstants.SMA_OA_GADGET_SERVICE), SbtConstants.DEFAULT_SC_SERVICE_NAME), 
 				createSmartCloudClient(fieldMap));
-		clients.put(StringUtils.defaultIfBlank(fieldMap.get(SbtEndpoints.DROPBOX_GADGET_OA_SERVICE_NAME), SbtEndpoints.DEFAULT_DROPBOX_SERVICE_NAME), 
+		clients.put(StringUtils.defaultIfBlank(fieldMap.get(SbtConstants.DROPBOX_GADGET_OA_SERVICE_NAME), SbtConstants.DEFAULT_DROPBOX_SERVICE_NAME), 
 				createDropBoxClient(fieldMap));
 	}
 
 	private DominoOAuthClient createDropBoxClient(Map<String, String> fieldMap) {
-		DominoOAuthClient client = new DropBoxOAuthClient(StringUtils.trim(fieldMap.get(SbtEndpoints.DROPBOX_OA_CONSUMERKEY)),
-				StringUtils.trim(fieldMap.get(SbtEndpoints.DROPBOX_OA_CONSUMERSECRET)));
+		DominoOAuthClient client = new DropBoxOAuthClient(StringUtils.trim(fieldMap.get(SbtConstants.DROPBOX_OA_CONSUMERKEY)),
+				StringUtils.trim(fieldMap.get(SbtConstants.DROPBOX_OA_CONSUMERSECRET)));
 		return client;
 	}
 	
 	private DominoOAuthClient createSmartCloudClient(Map<String, String> fieldMap) {
-		DominoOAuthClient client = new SmartCloudOAuthClient(StringUtils.trim(fieldMap.get(SbtEndpoints.SMA_OA_COUNSUMERKEY)),
-				StringUtils.trim(fieldMap.get(SbtEndpoints.SMA_OA_CONSUMERSECRET)));
+		DominoOAuthClient client = new SmartCloudOAuthClient(StringUtils.trim(fieldMap.get(SbtConstants.SMA_OA_COUNSUMERKEY)),
+				StringUtils.trim(fieldMap.get(SbtConstants.SMA_OA_CONSUMERSECRET)));
 		return client;
 	}
 
---
+++ samples/domino/com.ibm.xsp.sbtsdk.playground/src/nsf/playground/environments/SbtConstants.java
@@ -0,0 +1,48 @@
+/*
+ * © Copyright IBM Corp. 2013
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
+package nsf.playground.environments;
+
+
+
+/**
+ * Some constants.
+ */
+public class SbtConstants {
+	
+	public static final String CON_URL = "Con_URL";
+	public static final String CON_OA2_CONSUMERKEY = "Con_OA2_ConsumerKey";
+	public static final String CON_OA2_CONSUMERSECRET = "Con_OA2_ConsumerSecret";
+	public static final String DEFAULT_CONNECTIONS_SERVICE_NAME = "connections";
+	public static final String CON_GADGET_OA2_SERVICE_NAME = "Con_Gadget_OA2_Service_Name";
+	public static final String CON_GADGET_OA2_CONSUMERKEY = "Con_Gadget_OA2_ConsumerKey";
+	public static final String CON_GADGET_OA2_CONSUMERSECRET = "Con_Gadget_OA2_ConsumerSecret";
+	public static final String CON_OA2_AUTHORIZATIONURL = "Con_OA2_AuthorizationURL";
+	public static final String CON_OA2_ACCESSTOKENURL = "Con_OA2_AccessTokenURL";
+	public static final String DEFAULT_GOOGLE_SERVICE_NAME = "googleAPI";
+	public static final String GOOGLE_GADGET_OA_SERVICE_NAME = "Google_Gadget_OA_Service_Name";
+	public static final String GOOGLE_GADGET_OA_CONSUMERKEY = "Google_Gadget_OA_ConsumerKey";
+	public static final String GOOGLE_GADGET_OA_CONSUMERSECRET = "Google_Gadget_OA_ConsumerSecret";
+	public static final String SMA_URL = "Sma_URL";
+	public static final String DEFAULT_SC_SERVICE_NAME = "SmartCloud";
+	public static final String SMA_OA_GADGET_SERVICE = "Sma_OA_Gadget_Service";
+	public static final String SMA_OA_COUNSUMERKEY = "Sma_OA_ConsumerKey";
+	public static final String SMA_OA_CONSUMERSECRET = "Sma_OA_ConsumerSecret";
+	public static final String DROPBOX_GADGET_OA_SERVICE_NAME = "DropBox_Gadget_OA_Service_Name";
+	public static final String DROPBOX_OA_CONSUMERKEY = "Dropbox_OA_ConsumerKey";
+	public static final String DROPBOX_OA_CONSUMERSECRET = "Dropbox_OA_ConsumerSecret";
+	public static final String DEFAULT_DROPBOX_SERVICE_NAME = "DropBox";
+}

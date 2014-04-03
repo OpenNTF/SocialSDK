c1e123d3512f2540da68a4fed24240d8eec24c47
Mark Wallace <mark_wallace@ie.ibm.com>
2014-04-03 20:47:16 +0100

    BSS Unit Test Unit test to demonstrate using SubscriberManagement.getSubscribersById

--- sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/services/client/smartcloud/bss/SubscriberManagementService.java
+++ sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/services/client/smartcloud/bss/SubscriberManagementService.java
@@ -298,7 +298,7 @@ public class SubscriberManagementService extends BssService {
     		HashMap<String, String> params = new HashMap<String, String>();
     		params.put("_namedQuery", "getSubscriberByCustomer");
     		params.put("customer", customerId);
-			return (EntityList<JsonEntity>)getEntities(API_RESOURCE_SUBSCRIBER, null, getJsonFeedHandler());
+			return (EntityList<JsonEntity>)getEntities(API_RESOURCE_SUBSCRIBER, params, getJsonFeedHandler());
 		} catch (Exception e) {
 			throw new BssException(e, "Error retrieving subscriber list by customer id {0} caused by {1}", customerId, e.getMessage());
 		}

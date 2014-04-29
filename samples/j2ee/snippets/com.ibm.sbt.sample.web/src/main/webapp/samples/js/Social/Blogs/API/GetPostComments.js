284f1420c6ad53e855d44a348228e66d835c726a
Francis Moloney <franmolo@ie.ibm.com>
2014-04-29 17:49:02 +0100

    Add sample for getting comments from a blog post.

---
+++ samples/j2ee/snippets/com.ibm.sbt.sample.web/src/main/webapp/samples/js/Social/Blogs/API/GetPostComments.js
@@ -0,0 +1,35 @@
+require(["sbt/connections/BlogService", "sbt/dom", "sbt/json"],     
+    function(BlogService, dom, json) {
+        var blogService = new BlogService(); 
+        var now = new Date();
+        var post = blogService.newBlogPost();
+        post.setTitle("BlogPost at " + now.getTime());
+        post.setContent("BlogPost Content on " + now.toDateString());
+        var comment = blogService.newComment();
+        comment.setContent("Comment Content on " + now.toDateString());
+        
+        blogService.getAllBlogs({ ps: 1 }).then(
+            function(blogs){
+                post.setBlogHandle(blogs[0].getHandle());
+                return blogService.createPost(post); // returning newly created blog post
+            }
+        ).then(
+            function(createdPost){
+                comment.setBlogHandle(createdPost.getBlogHandle());
+                comment.setPostUuid(createdPost.getPostUuid());
+                return blogService.createComment(comment); // returning newly created comment
+            }
+        ).then(
+            function(createdComment){
+                return post.getComments();
+            }
+        ).then(
+            function(comments) {
+                dom.setText("json", json.jsonBeanStringify(comments));
+            },
+            function(error) {
+                dom.setText("json", json.jsonBeanStringify(error));
+            }
+        );
+    }
+);

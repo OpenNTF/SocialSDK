7f792c488e13f380b96610ba81bc9e2a55d3c327
David Ryan <daviryan@ie.ibm.com>
2014-04-29 16:28:23 +0100

    Refactor URL Enum

--- sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/services/client/connections/bookmarks/BookmarkService.java
+++ sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/services/client/connections/bookmarks/BookmarkService.java
@@ -119,7 +119,7 @@ public class BookmarkService extends BaseService {
 	 * @throws ClientServicesException
 	 */
 	public EntityList<Bookmark> getBookmarks(String id, Map<String, String> parameters) throws ClientServicesException {
-		String url = BookmarkUrls.APP.format(this,BookmarkUrls.userId.get(id));
+		String url = BookmarkUrls.APP.format(this,BookmarkUrls.getUserId(id));
 		return getBookmarkEntityList(url, parameters);
 		
 	}

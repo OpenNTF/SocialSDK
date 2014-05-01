e888520bae655373eca4b5f6e8dee3fef3adbbc3
benjamij <benjamij@ie.ibm.com>
2014-05-01 10:23:16 +0100

    Users can now configure endpoints to require sign-on into Wordpress or to allow anonymous access

--- php/wordpress-plugin/src/views/wp-settings/hiddenEndpointFields.php
+++ php/wordpress-plugin/src/views/wp-settings/hiddenEndpointFields.php
@@ -7,6 +7,7 @@
 <input type="hidden" id="consumer_secret" name="consumer_secret" value="" />
 <input type="hidden" id="endpoint_name" name="endpoint_name" value="<?php echo $viewData['endpointName']; ?>" />
 <input type="hidden" id="endpoint_url" name="endpoint_url" value="" />
+<input style="display: none;" type="checkbox" id="require_sign_on" name="require_sign_on" value="require_sign_on" <?php echo (isset($viewData['require_sign_on']) && $viewData['require_sign_on'] == true ? 'checked="checked"' : "")?>/>
 <input type="hidden" id="request_token_url" name="request_token_url" value="" />
 <input type="hidden" id="server_type" name="server_type" value="<?php echo $viewData['server_type'];?>" />
 <input type="hidden" id="callback_url" name="callback_url" value="<?php echo $viewData['callback_url'];?>" />

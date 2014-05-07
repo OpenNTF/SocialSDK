12a8ac8e687f76d8aad62ba474575b6b5c66718d
Carlos Manias <carlmani@ie.ibm.com>
2014-05-07 09:27:39 +0100

    Fixed uses of Author, Contributor and Modifier classes

---
+++ sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/services/client/connections/common/Bookmark.java
@@ -0,0 +1,34 @@
+/*
+ * © Copyright IBM Corp. 2014
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.ibm.sbt.services.client.connections.common;
+
+import com.ibm.sbt.services.client.base.AtomEntity;
+
+/**
+ *  Lists the elements in a bookmark Atom entry. <br>
+ *  You can create bookmark entries in the Communities and Bookmarks services. 
+ *  Most elements of a bookmark are the same for both services. The first table lists the common elements; the additional tables define extensions used by the specific services.
+ *  
+ * @see
+ *		<a href="http://www-10.lotus.com/ldd/appdevwiki.nsf/xpDocViewer.xsp?lookupName=IBM+Connections+4.5+API+Documentation#action=openDocument&res_title=Bookmark_entry_content_ic45&content=pdcontent">
+ *			Bookmark entry content</a>
+ * 
+ * @author Carlos Manias
+ *
+ */
+public class Bookmark extends AtomEntity {
+
+}
--- sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/services/client/connections/common/Tag.java
+++ sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/services/client/connections/common/Tag.java
@@ -1,5 +1,5 @@
 /*
- * © Copyright IBM Corp. 2014
+ * Â© Copyright IBM Corp. 2014
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +24,16 @@ import com.ibm.sbt.services.client.base.BaseService;
 import com.ibm.sbt.services.client.base.datahandlers.XmlDataHandler;
 
 /**
+ * The category document identifies the tags that have been assigned to particular items,
+ * such as blog posts or community entries.
+ * Tags are single-word keywords that categorize a posting or entry.
+ * A tag classifies the information in the posting or entry to make it easier to find the content later.
+ * The format of the tags document is an Atom publishing protocol (APP) categories document. 
+ * 
+ * @see
+ *		<a href="http://www-10.lotus.com/ldd/appdevwiki.nsf/xpDocViewer.xsp?lookupName=IBM+Connections+4.5+API+Documentation#action=openDocument&res_title=Tags_category_document_ic45&content=pdcontent">
+ *			Tags category content</a>
+ * 
  * @author mwallace
  *
  */

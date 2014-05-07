f946ac73a2c8ed1b753071a0f6dffcbf66492fc5
Carlos Manias <carlmani@ie.ibm.com>
2014-05-07 14:21:07 +0100

    Fix some copyright notices
    Fix some copyright notices

--- samples/j2ee/snippets/com.ibm.sbt.automation.test/src/main/java/com/ibm/sbt/test/controls/activitystream/UpdateswithTag.java
+++ samples/j2ee/snippets/com.ibm.sbt.automation.test/src/main/java/com/ibm/sbt/test/controls/activitystream/UpdateswithTag.java
@@ -1,10 +1,5 @@
-package com.ibm.sbt.test.controls.activitystream;
-
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
-import com.ibm.sbt.automation.core.test.BaseActivityStreamTest;
 /*
- * � Copyright IBM Corp. 2012
+ * © Copyright IBM Corp. 2012
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
@@ -18,6 +13,13 @@ import com.ibm.sbt.automation.core.test.BaseActivityStreamTest;
  * implied. See the License for the specific language governing 
  * permissions and limitations under the License.
  */
+package com.ibm.sbt.test.controls.activitystream;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.ibm.sbt.automation.core.test.BaseActivityStreamTest;
 
 /**
  * @author Francis 

e82efe987b4ea37a5c1582a0a37f8aa39b3f2d3a
benjamij <benjamij@ie.ibm.com>
2014-05-16 15:41:26 +0100

    Fixed proxy helper; GET parameters sometimes missed a '?'

--- php/php-core/src/controllers/services/Proxy.php
+++ php/php-core/src/controllers/services/Proxy.php
@@ -119,9 +119,7 @@ class Proxy extends BaseController
 		}
 		
 		$url = $proxyHelper->cleanURL($url, $server);
-		syslog(LOG_INFO, $url);
-		syslog(LOG_INFO, $method);
-		syslog(LOG_INFO, $body);
+
 		// Make request
  		$response = $endpoint->makeRequest($server, $url, $method, $options, $body, $forwardHeader, $endpointName);
 

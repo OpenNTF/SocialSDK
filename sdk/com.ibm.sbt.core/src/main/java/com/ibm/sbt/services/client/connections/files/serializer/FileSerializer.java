ad934c853a54cb5c96d7ca191522f35deac2ce7b
Carlos Manias <carlmani@ie.ibm.com>
2014-05-19 12:19:15 +0100

    FileService cleanup
    FileService cleanup

--- sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/services/client/connections/files/serializer/FileSerializer.java
+++ sdk/com.ibm.sbt.core/src/main/java/com/ibm/sbt/services/client/connections/files/serializer/FileSerializer.java
@@ -1,16 +1,5 @@
-package com.ibm.sbt.services.client.connections.files.serializer;
-
-import static com.ibm.sbt.services.client.base.ConnectionsConstants.CONTRIBUTOR;
-import javax.xml.bind.DatatypeConverter;
-import org.apache.http.client.utils.DateUtils;
-import org.w3c.dom.Node;
-import com.ibm.commons.util.StringUtil;
-import com.ibm.sbt.services.client.base.ConnectionsConstants;
-import com.ibm.sbt.services.client.base.serializers.AtomEntitySerializer;
-import com.ibm.sbt.services.client.base.serializers.BaseEntitySerializer.DateSerializer;
-import com.ibm.sbt.services.client.base.serializers.PersonSerializer;
 /*
- * © Copyright IBM Corp. 2012
+ * © Copyright IBM Corp. 2014
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
@@ -24,8 +13,27 @@ import com.ibm.sbt.services.client.base.serializers.PersonSerializer;
  * implied. See the License for the specific language governing 
  * permissions and limitations under the License.
  */
+package com.ibm.sbt.services.client.connections.files.serializer;
+
+import static com.ibm.sbt.services.client.base.ConnectionsConstants.CATEGORY;
+import static com.ibm.sbt.services.client.base.ConnectionsConstants.LABEL;
+import static com.ibm.sbt.services.client.base.ConnectionsConstants.SCHEME;
+import static com.ibm.sbt.services.client.base.ConnectionsConstants.TERM;
+import static com.ibm.sbt.services.client.base.ConnectionsConstants.UUID;
+import static com.ibm.sbt.services.client.connections.files.FileConstants.CATEGORY_TAG;
+import static com.ibm.sbt.services.client.connections.files.FileConstants.CREATED;
+import static com.ibm.sbt.services.client.connections.files.FileConstants.MODIFIED;
+import static com.ibm.sbt.services.client.connections.files.FileConstants.MODIFIER;
+import static com.ibm.sbt.services.client.connections.files.FileConstants.NOTIFICATION;
+import static com.ibm.sbt.services.client.connections.files.FileConstants.VISIBILITY;
+
+import org.w3c.dom.Node;
+
+import com.ibm.commons.util.StringUtil;
+import com.ibm.sbt.services.client.base.ConnectionsConstants.Namespace;
+import com.ibm.sbt.services.client.base.serializers.AtomEntitySerializer;
+import com.ibm.sbt.services.client.base.serializers.PersonSerializer;
 import com.ibm.sbt.services.client.connections.files.File;
-import com.ibm.sbt.services.client.connections.files.FileConstants;
 
 /**
  * @author Lorenzo Boccaccia 
@@ -41,44 +49,42 @@ public class FileSerializer extends AtomEntitySerializer<File> {
     public Node getFileEntry() {
         Node n =genericAtomEntry();
         if (entity.getFileId()!=null)
-            appendChildren(n, textElement(ConnectionsConstants.Namespace.TD.getUrl(), ConnectionsConstants.UUID, entity.getFileId()));
+            appendChildren(n, textElement(Namespace.TD.getUrl(), UUID, entity.getFileId()));
         if (entity.getLabel() != null)
-            appendChildren(n, textElement(ConnectionsConstants.Namespace.TD.getUrl(), ConnectionsConstants.LABEL, entity.getLabel()));
-        appendChildren(n, element(ConnectionsConstants.Namespace.TD.getUrl(), ConnectionsConstants.CATEGORY, 
-                attribute(ConnectionsConstants.SCHEME, ConnectionsConstants.Namespace.TAG.getUrl()),
-                attribute(ConnectionsConstants.TERM, entity.getCategory()),
-                attribute(ConnectionsConstants.LABEL, entity.getCategory())
+            appendChildren(n, textElement(Namespace.TD.getUrl(), LABEL, entity.getLabel()));
+        appendChildren(n, element(Namespace.TD.getUrl(), CATEGORY, 
+                attribute(SCHEME, Namespace.TAG.getUrl()),
+                attribute(TERM, entity.getCategory()),
+                attribute(LABEL, entity.getCategory())
                 )
                 );
         if (entity.getTags()!=null)
         for (String tag : entity.getTags()) {
-            appendChildren(n, textElement(ConnectionsConstants.CATEGORY, tag,                
-                    attribute(ConnectionsConstants.TERM, FileConstants.CATEGORY_TAG)
+            appendChildren(n, textElement(CATEGORY, tag,                
+                    attribute(TERM, CATEGORY_TAG)
                     ));
             
         }
         if (StringUtil.isNotEmpty( entity.getVisibility()))      
-        appendChildren(n, textElement(ConnectionsConstants.Namespace.TD.getUrl(), FileConstants.VISIBILITY, entity.getVisibility()));
+        appendChildren(n, textElement(Namespace.TD.getUrl(), VISIBILITY, entity.getVisibility()));
         
         if (StringUtil.isNotEmpty( entity.getNotification()))      
-            appendChildren(n, textElement(ConnectionsConstants.Namespace.TD.getUrl(), FileConstants.NOTIFICATION, entity.getNotification()));
+            appendChildren(n, textElement(Namespace.TD.getUrl(), NOTIFICATION, entity.getNotification()));
             
         
         if (entity.getModified()!=null)     
-            appendChildren(n, textElement(ConnectionsConstants.Namespace.TD.getUrl(), FileConstants.MODIFIED, DateSerializer.toString(entity.getModified())));
+            appendChildren(n, textElement(Namespace.TD.getUrl(), MODIFIED, DateSerializer.toString(entity.getModified())));
         if (entity.getCreated()!=null)     
-            appendChildren(n, textElement(ConnectionsConstants.Namespace.TD.getUrl(), FileConstants.CREATED, DateSerializer.toString(entity.getCreated())));
+            appendChildren(n, textElement(Namespace.TD.getUrl(), CREATED, DateSerializer.toString(entity.getCreated())));
             
         if (entity.getModifier()!=null)   
-            appendChildren(n,new PersonSerializer(entity.getModifier()).xmlNode(FileConstants.MODIFIER, ConnectionsConstants.Namespace.TD.getUrl() ));
+            appendChildren(n,new PersonSerializer(entity.getModifier()).xmlNode(MODIFIER, Namespace.TD.getUrl() ));
         
         return n;
     }
 
-
     public String generateFileUpdatePayload() {
        getFileEntry();
-       
         return serializeToString();
     }
 }

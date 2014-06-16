4e7ca6afffcc90f84647960662e21e25cf0e6a57
Francis Moloney <franmolo@ie.ibm.com>
2014-06-16 12:01:24 +0100

    Mock endpoint should return true for isAuthenticated.

--- sdk/com.ibm.sbt.core.test/src/main/java/com/ibm/sbt/test/lib/MockEndpoint.java
+++ sdk/com.ibm.sbt.core.test/src/main/java/com/ibm/sbt/test/lib/MockEndpoint.java
@@ -1,6 +1,6 @@
 package com.ibm.sbt.test.lib;
 /*
- * © Copyright IBM Corp. 2013
+ * ï¿½ Copyright IBM Corp. 2013
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,6 +43,11 @@ public class MockEndpoint extends BasicEndpoint {
 
 	public MockEndpoint(Endpoint endpoint) {
 		this.endpoint = endpoint;
+	}
+	
+	@Override
+	public boolean isAuthenticated(){
+		return true;
 	}
 
 	@Override
